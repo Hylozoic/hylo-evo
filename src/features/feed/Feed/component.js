a5c71718e7686dc1e114ed4e4630a8360340bfd6
Robbie Carlton <robbie.carlton@gmail.com>
2017-03-06 17:55:26 -0700

    make content div scrollable

---
+++ src/features/feed/Feed/component.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Link } from 'react-router'
+
+export default function Feed () {
+  return <div styleName='feed'>
+    <div>Make this thing</div>
+    <div>Really long</div>
+    <div>With a lot of</div>
+    <div>text</div>
+    <div>so we have to scroll</div>
+    <br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
+    <br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
+    <br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
+    <div>to see down here</div>
+    <div>and then just some more text</div>
+  </div>
+}
--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,9 @@
 import React from 'react'
-import { Router, Route, hashHistory } from 'react-router'
+import { Router, Route, IndexRoute, hashHistory } from 'react-router'
 import GeneralLayout from 'features/layout/GeneralLayout'
 import EventDetail from 'features/events/EventDetail'
 import EventList from 'features/events/EventList'
+import Feed from 'features/feed/Feed'
 
 // LEJ: The following throws and error looking for Typography.js
 //    import Typography from './components/Typography'
@@ -12,6 +13,7 @@ import EventList from 'features/events/EventList'
 export default
   <Router history={hashHistory}>
     <Route path='/' component={GeneralLayout}>
+      <IndexRoute components={{content: Feed}}/>
       <Route path='events' components={{content: EventList}} />
       <Route path='events/:eventId' components={{content: EventList, detail: EventDetail}} />
     </Route>
