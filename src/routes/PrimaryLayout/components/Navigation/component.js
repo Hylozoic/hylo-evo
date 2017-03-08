5d3113e4fed4bdd29880365bb5b586e4d90ac378
Robbie Carlton <robbie.carlton@gmail.com>
2017-03-08 15:59:25 -0700

    add icon font and Icon component

--- src/routes/PrimaryLayout/components/Navigation/component.js
+++ src/routes/PrimaryLayout/components/Navigation/component.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router'
+import Icon from 'components/Icon'
 
 export default function Navigation (
   { collapsed }
@@ -9,6 +10,22 @@ export default function Navigation (
       <li><Link to='/'>Home</Link></li>
       <li><Link to='/events'>Events</Link></li>
       <li><Link to='/ui-kit'>UI Kit</Link></li>
+      <li>
+        <Icon name='Home' />
+        Home
+      </li>
+      <li>
+        <Icon name='Topics' />
+        Topics
+      </li>
+      <li>
+        <Icon name='Events' />
+        Events
+      </li>
+      <li>
+        <Icon name='Projects' />
+        Projects
+      </li>
     </ul>
   </div>
 }
