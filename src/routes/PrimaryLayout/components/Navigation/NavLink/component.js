d1831a7a193e92df5b7c420cc4ac4962126adc6f
Robbie Carlton <robbie.carlton@gmail.com>
2017-03-09 14:58:49 -0700

    nav links track router

---
+++ src/routes/PrimaryLayout/components/Navigation/NavLink/component.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Link } from 'react-router'
+import Icon from 'components/Icon'
+import cx from 'classnames'
+
+export default function NavLink (
+  { to, label, icon, badge }, { router }
+) {
+  const active = router.isActive(to, true)
+
+  return <li styleName={cx('item', {active})}>
+    <Link to={to} styleName='link'>
+      <Icon name={icon} green={active} styleName='icon' />
+      {label}
+      {badge && <span styleName='badge'><span styleName='badgeNumber'>{badge}</span></span>}
+    </Link>
+  </li>
+}
+NavLink.contextTypes = {
+  router: React.PropTypes.object
+}
--- src/routes/PrimaryLayout/components/Navigation/component.js
+++ src/routes/PrimaryLayout/components/Navigation/component.js
@@ -1,14 +1,9 @@
 import React from 'react'
-import { Link } from 'react-router'
-import Icon from 'components/Icon'
-import cx from 'classnames'
+import NavLink from './NavLink'
 
 export default function Navigation (
   { collapsed, styles }
 ) {
-
-  console.log('props', {collapsed, styles})
-
   const links = [
     {label: 'Home', icon: 'Home', to: '/', badge: 2},
     {label: 'Events', icon: 'Events', to: '/events', active: true},
@@ -17,13 +12,7 @@ export default function Navigation (
 
   return <div styleName={collapsed ? 'navigation--collapsed' : 'navigation'}>
     <ul styleName='links'>
-      {links.map(link => <li key={link.label} styleName={cx('item', {active: link.active})}>
-        <Link to={link.to} styleName='link'>
-          <Icon name={link.icon} green={link.active} styleName='icon' />
-          {link.label}
-          {link.badge && <span styleName='badge'><span styleName='badgeNumber'>{link.badge}</span></span>}
-        </Link>
-      </li>)}
+      {links.map(link => <NavLink key={link.label} {...link} />)}
     </ul>
   </div>
 }
