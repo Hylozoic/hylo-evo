1faa329f459b55fd6dfa94a39b87120d3f741aa0
Robbie Carlton <robbie.carlton@gmail.com>
2017-03-09 14:39:50 -0700

    top half nav bar styled

--- src/routes/PrimaryLayout/components/Navigation/component.js
+++ src/routes/PrimaryLayout/components/Navigation/component.js
@@ -1,30 +1,29 @@
 import React from 'react'
 import { Link } from 'react-router'
 import Icon from 'components/Icon'
+import cx from 'classnames'
 
 export default function Navigation (
-  { collapsed }
+  { collapsed, styles }
 ) {
-  return <div styleName={collapsed ? 'navigation-collapsed' : 'navigation'}>
-    <ul>
-      <li>
-        <Link to='/'>
-          <Icon name='Home' />
-          Home
+
+  console.log('props', {collapsed, styles})
+
+  const links = [
+    {label: 'Home', icon: 'Home', to: '/', badge: 2},
+    {label: 'Events', icon: 'Events', to: '/events', active: true},
+    {label: 'UI Kit', icon: 'Projects', to: '/ui-kit'}
+  ]
+
+  return <div styleName={collapsed ? 'navigation--collapsed' : 'navigation'}>
+    <ul styleName='links'>
+      {links.map(link => <li key={link.label} styleName={cx('item', {active: link.active})}>
+        <Link to={link.to} styleName='link'>
+          <Icon name={link.icon} green={link.active} styleName='icon' />
+          {link.label}
+          {link.badge && <span styleName='badge'><span styleName='badgeNumber'>{link.badge}</span></span>}
         </Link>
-      </li>
-      <li>
-        <Link to='/events'>
-          <Icon name='Events' green />
-          Events
-        </Link>
-      </li>
-      <li>
-        <Link to='/ui-kit'>
-          <Icon name='Projects' />
-          UI Kit
-        </Link>
-      </li>
+      </li>)}
     </ul>
   </div>
 }
