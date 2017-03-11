cf4290d355e5661de4a6612a825af38bc27b77aa
Robbie Carlton <robbie.carlton@gmail.com>
2017-03-10 17:32:27 -0700

    update icons and add "collapsed" to context

--- src/routes/PrimaryLayout/components/Navigation/NavLink/component.js
+++ src/routes/PrimaryLayout/components/Navigation/NavLink/component.js
@@ -5,12 +5,12 @@ import Badge from 'components/Badge'
 import cx from 'classnames'
 
 export default function NavLink (
-  { to, label, icon, badge, indexOnly = false }, { router }
+  { to, label, icon, badge, indexOnly = false }, { router, collapsed }
 ) {
   const active = router.isActive(to, indexOnly)
 
   return <li styleName={cx('item', {active})}>
-    <Link to={to} styleName='link'>
+    <Link to={to} styleName={cx('link', {collapsed})}>
       <Icon name={icon} green={active} styleName='icon' />
       {label}
       {badge && <Badge number={badge} expanded={badge % 2 === 0} styleName='badge' />}
@@ -18,5 +18,6 @@ export default function NavLink (
   </li>
 }
 NavLink.contextTypes = {
-  router: React.PropTypes.object
+  router: React.PropTypes.object,
+  collapsed: React.PropTypes.bool
 }
--- src/routes/PrimaryLayout/components/Navigation/component.js
+++ src/routes/PrimaryLayout/components/Navigation/component.js
@@ -1,22 +1,34 @@
 import React from 'react'
 import NavLink from './NavLink'
 import TopicNavigation from './TopicNavigation'
+const { bool } = React.PropTypes
 
-export default function Navigation (
-  { collapsed }
-) {
-  const links = [
-    {label: 'Home', icon: 'Home', to: '/', badge: 2, indexOnly: true},
-    {label: 'Events', icon: 'Events', to: '/events', active: true, badge: 3},
-    {label: 'UI Kit', icon: 'Projects', to: '/ui-kit'}
-  ]
+export default class Navigation extends React.Component {
+  static childContextTypes = {
+    collapsed: bool
+  }
 
-  return <div styleName={collapsed ? 'collapser-collapsed' : 'collapser'}>
-    <div styleName='navigation'>
-      <ul styleName='links'>
-        {links.map(link => <NavLink key={link.label} {...link} />)}
-      </ul>
-      <TopicNavigation />
+  getChildContext () {
+    return {collapsed: this.props.collapsed}
+  }
+
+  render () {
+    const { collapsed } = this.props
+
+    const links = [
+      {label: 'Home', icon: 'Home', to: '/', indexOnly: true},
+      {label: 'Events', icon: 'Events', to: '/events', badge: 3},
+      {label: 'Members', icon: 'Members', to: '/members'},
+      {label: 'UI Kit', icon: 'Projects', to: '/ui-kit'}
+    ]
+
+    return <div styleName={collapsed ? 'collapser-collapsed' : 'collapser'}>
+      <div styleName='navigation'>
+        <ul styleName='links'>
+          {links.map(link => <NavLink key={link.label} {...link} />)}
+        </ul>
+        <TopicNavigation />
+      </div>
     </div>
-  </div>
+  }
 }
