464f2b3b1e8c620edbc9f611bba3c05b8bdc7bcd
Loren Johnson <loren@hellovenado.com>
2017-03-14 10:57:42 -0700

    Event Details working

--- src/routes/Events/component.js
+++ src/routes/Events/component.js
@@ -3,12 +3,12 @@ import { Link } from 'react-router-dom'
 
 const { string, bool } = React.PropTypes
 
-function Events () {
+export default function Events () {
   return <div styleName='event-list'>
-    <div><Link to='events/1'>Event 1</Link></div>
-    <div><Link to='events/2'>Event 2</Link></div>
-    <div><Link to='events/3'>Event 3</Link></div>
-    <div><Link to='events/4'>Event 4</Link></div>
+    <div><Link to='/events/1'>Event 1</Link></div>
+    <div><Link to='/events/2'>Event 2</Link></div>
+    <div><Link to='/events/3'>Event 3</Link></div>
+    <div><Link to='/events/4'>Event 4</Link></div>
   </div>
 }
 Events.propTypes = {
--- src/routes/PrimaryLayout/component.js
+++ src/routes/PrimaryLayout/component.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-unused-vars */
 import React, { Component } from 'react'
-import { Route, Link } from 'react-router-dom'
+import { matchPath, Route, Link } from 'react-router-dom'
 import SampleCard from 'components/SampleCard'
 import Navigation from './components/Navigation'
 import Events from 'routes/Events'
@@ -9,9 +9,9 @@ import EventDetail from 'routes/Events/EventDetail'
 // Global styles
 import 'css/global/index.scss'
 
-export default function PrimaryLayout ({ match }) {
-  console.log(match)
-  const hasDetail = !!match.params.eventId
+export default function PrimaryLayout ({ match, location }) {
+  // TODO: Replace with something more sensible 
+  const hasDetail = location.pathname.match(/\/events\//)
   return <div styleName='container'>
     <div styleName='row'>
       <div styleName='header' className='hdr-display'>
@@ -22,11 +22,11 @@ export default function PrimaryLayout ({ match }) {
       <Navigation collapsed={hasDetail} />
       <div styleName='content'>
         <Route path='/' exact component={SampleCard} />
-        <Route path='/events' exact component={Events} />
+        <Route path='/events' component={Events} />
       </div>
       <div styleName={hasDetail ? 'detail-expanded' : 'detail'}>
         <Route path='/' exact component={SampleCard} />
-        <Route path='/events/:eventId' component={EventDetail} />
+        <Route path='/events/:eventId' exact component={EventDetail} />
       </div>
     </div>
   </div>
