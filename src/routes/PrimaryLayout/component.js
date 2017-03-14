3ddd542284e7df8b800e31cd4500d9fc7a7c28e4
Robbie Carlton <robbie.carlton@gmail.com>
2017-03-13 18:34:53 -0600

    right detail panel now slides in

--- src/routes/PrimaryLayout/component.js
+++ src/routes/PrimaryLayout/component.js
@@ -5,32 +5,43 @@ import { Link } from 'react-router'
 import SampleCard from 'components/SampleCard'
 import Navigation from './components/Navigation'
 import TopNav from './components/TopNav'
+import cx from 'classnames'
+import { get } from 'lodash/fp'
 
 // Global styles
 import 'css/global/index.scss'
 
-export default function PrimaryLayout (
-  { header, content, navigation }
-) {
-  const detail = (content && content.props && content.props.detail)
-  const expanded = !!detail
-  return <div styleName='container'>
-    {header || <TopNav />}
-    <div styleName='row'>
-      {navigation || <Navigation collapsed={expanded} />}
-      <div styleName='content'>
-        {content || <SampleCard />}
-      </div>
-      {!expanded && <div styleName={'detail'}>
-        {detail || <SampleCard />}
-      </div>}
-      {expanded && <div styleName={'detail-expanded'}>
-        {detail || <SampleCard />}
-      </div>}
-    </div>
-  </div>
-}
+export default class PrimaryLayout extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {lastDetail: null}
+  }
 
-export function Header () {
-  return <div className='hdr-display'>Top Bar</div>
+  componentDidUpdate (prevProps) {
+    if (get('content.props.detail', prevProps) && !get('content.props.detail', this.props)) {
+      this.setState({lastDetail: get('content.props.detail', prevProps)})
+    }
+  }
+
+  render () {
+    const { header, content, navigation, sidebar } = this.props
+    const detail = (content && content.props && content.props.detail)
+    const { lastDetail } = this.state
+    const expanded = !!detail
+    return <div styleName='container'>
+      {header || <TopNav />}
+      <div styleName='row'>
+        {navigation || <Navigation collapsed={expanded} />}
+        <div styleName='content'>
+          {content || <SampleCard />}
+        </div>
+        <div styleName={cx('sidebar', {hidden: expanded})}>
+          {sidebar || <SampleCard />}
+        </div>
+        <div styleName={cx('detail', {hidden: !expanded})}>
+          {detail || lastDetail || <SampleCard />}
+        </div>
+      </div>
+    </div>
+  }
 }
