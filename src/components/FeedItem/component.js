79ab636efc4b9ac1f9ce21ccad32e44994c45e0a
Robbie Carlton <robbie.carlton@gmail.com>
2017-03-20 15:53:48 -0700

    style feed items

--- src/components/FeedItem/component.js
+++ src/components/FeedItem/component.js
@@ -5,9 +5,9 @@ import { includes } from 'lodash/fp'
 
 const POST_TYPES = ['offer', 'request', 'discussion']
 
-export default function FeedItem ({ feedItem }) {
+export default function FeedItem ({ feedItem, className }) {
   if (includes(feedItem.type, POST_TYPES)) {
-    return <PostCard post={feedItem} />
+    return <PostCard post={feedItem} className={className} />
   }
 
   return null
--- src/routes/Feed/component.js
+++ src/routes/Feed/component.js
@@ -4,6 +4,7 @@ import FeedItem from 'components/FeedItem'
 
 export default function Feed () {
   return <div styleName='feed'>
-    {SAMPLE_FEED_ITEMS.map(feedItem => <FeedItem feedItem={feedItem} key={feedItem.id} />)}
+    {SAMPLE_FEED_ITEMS.map(feedItem =>
+      <FeedItem feedItem={feedItem} styleName='feedItem' key={feedItem.id} />)}
   </div>
 }
