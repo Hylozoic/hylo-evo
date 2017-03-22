41a8915a4ca8a5a3451ebeaf0eba35e3a933090d
Rich Churcher <rich.churcher@gmail.com>
2017-03-22 21:53:06 +1300

    Refactor, one attempt at a pattern

--- src/store/models/Post.js
+++ src/store/models/Post.js
@@ -1,38 +1,59 @@
-import { omit } from 'lodash'
-import { attr, fk, Model } from 'redux-orm'
+import { attr, fk, many, Model } from 'redux-orm'
 
-export default class Post extends Model {
-  toString () {
-    return `Post: ${this.name}`
-  }
-
-  static processRelatedData (postData) {
-    const { Person, Community } = this.session
-    const { people, communities } = postData
-    people.forEach(person => Person.parse(person))
-    communities.forEach(community => Community.parse(community))
-    return omit(postData, ['people', 'communities'])
-  }
-
-  static parse (postData) {
-    const { Comment } = this.session
-    let clonedData = this.processRelatedData(postData)
-    clonedData = {
-      ...clonedData,
-      author: postData.user_id,
-      communities: postData.community_ids,
-      comments: postData.comments.map(commentData => Comment.parse(commentData))
-    }
-    clonedData = omit(clonedData, ['user_id', 'community_id'])
-    return this.create(clonedData)
-  }
-}
-
-Post.modelName = 'Post'
-
-Post.fields = {
+const fields = {
   id: attr(),
   name: attr(),
   author: fk('Person'),
   communities: many('Community')
 }
+
+export default class Post extends Model {
+  static parse (postData) {
+    const {
+      id,
+      community_ids,
+      description,
+      name,
+      user_id
+    } = postData
+
+    this.processRelations(postData)
+
+    return this.create({
+      id,
+      name,
+      description,
+      author: user_id,
+      communities: community_ids,
+      comments: postData.comments.map(c => c.id)
+    })
+  }
+
+  static processRelations ({ comments, communities, people }) {
+    const { Comment, Community, Person } = this.session
+    comments.forEach(c => Comment.parse(c))
+    communities.forEach(c => Community.parse(c))
+    people.forEach(p => Person.parse(p))
+  }
+
+  // TODO: Might need to get much more fine-grained than this, depending on what
+  // the API expects (including re-hydrating arrays of relations).
+  toJSON () {
+    const { id, author, comments, communities, description, name } = this.ref
+    return JSON.stringify({
+      id,
+      user_id: author,
+      comments,
+      community_ids: communities,
+      description,
+      name
+    })
+  }
+
+  toString () {
+    return `Post: ${this.name}`
+  }
+}
+
+Post.modelName = 'Post'
+Post.fields = fields
