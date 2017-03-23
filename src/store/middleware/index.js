4c7fedf2bcea00947fbd8c9c6adb9a22c02f1977
Lawrence Wang <l@lw.io>
2017-03-22 18:09:33 -0700

    add redux-logger

--- src/store/middleware/index.js
+++ src/store/middleware/index.js
@@ -1,13 +1,16 @@
 import { applyMiddleware, compose } from 'redux'
 import promiseMiddleware from 'redux-promise'
 import apiMiddleware from './apiMiddleware'
+import createLogger from 'redux-logger'
+import { compact } from 'lodash'
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 
-const middleware = [
+const middleware = compact([
   apiMiddleware(),
-  promiseMiddleware
-]
+  promiseMiddleware,
+  process.env.NODE_ENV === 'development' && createLogger({collapsed: true})
+])
 
 export default composeEnhancers(
   applyMiddleware(...middleware)
