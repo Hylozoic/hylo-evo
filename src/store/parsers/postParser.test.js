798c8b5c7bfcead43dc0f7735d8bebb362d35e39
Rich Churcher <rich.churcher@gmail.com>
2017-03-24 22:33:43 +1300

    Filters out unknown properties

--- src/store/parsers/postParser.test.js
+++ src/store/parsers/postParser.test.js
@@ -1,6 +1,6 @@
-import postParser from './postParser'
+import postParser, { normalize } from './postParser'
 
-it('Post is invalid if missing any of the core properties', () => {
+it('is invalid if missing any of the core properties', () => {
   const id = '1'
   const title = 'Wombat'
   const type = null
@@ -9,4 +9,16 @@ it('Post is invalid if missing any of the core properties', () => {
   expect(postParser.isValid({ title, type, details })).toBe(false)
   expect(postParser.isValid({ id, type, details })).toBe(false)
   expect(postParser.isValid({ id, title, details })).toBe(false)
+})
+
+it('does not allow unspecified properties onto resulting object', () => {
+  const post = {
+    id: '1',
+    title: 'Wombat',
+    type: null,
+    details: '<p>Wombats poop square.</p>',
+    wombatRatio: '1:1'
+  }
+  const actual = normalize(post)
+  expect(actual.hasOwnProperty('wombatRatio')).toBe(false)
 })
