0f92305db22186e7de258c49c7e8a8008fd21661
Rich Churcher <rich.churcher@gmail.com>
2017-03-24 22:39:17 +1300

    Normalises ids from followers

--- src/store/parsers/postParser.test.js
+++ src/store/parsers/postParser.test.js
@@ -11,14 +11,24 @@ it('is invalid if missing any of the core properties', () => {
   expect(postParser.isValid({ id, title, details })).toBe(false)
 })
 
-it('does not allow unspecified properties onto resulting object', () => {
+it('does not allow unknown properties onto resulting object', () => {
   const post = {
     id: '1',
-    title: 'Wombat',
-    type: null,
-    details: '<p>Wombats poop square.</p>',
     wombatRatio: '1:1'
   }
   const actual = normalize(post)
   expect(actual.hasOwnProperty('wombatRatio')).toBe(false)
+})
+
+it('normalizes ids from followers', () => {
+  const post = {
+    followers: [
+      { id: '111', name: 'Hortense', avatarUrl: 'https://foobar.com/foo.png' },
+      { id: '222', name: 'Catalina', avatarUrl: 'https://foobar.com/foo.png' },
+      { id: '333', name: 'Raphael', avatarUrl: 'https://foobar.com/foo.png' },
+    ]
+  }
+  const expected = { followers: [ '111', '222', '333' ] }
+  const actual = normalize(post)
+  expect(actual).toEqual(expected)
 })
