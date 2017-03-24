cc9eecf176cf1dc0ce73b20729fa1c68bee5f8cf
Rich Churcher <rich.churcher@gmail.com>
2017-03-25 10:29:34 +1300

    Transform communities

--- src/store/transformers/communityTransformer.js
+++ src/store/transformers/communityTransformer.js
@@ -1,12 +1,7 @@
 export default function transform (entity) {
   // Functions taking the value to modify as a single argument.
-  // Note that most of these need to happen _after_ relations have already
-  // been processed in `transformMiddleware`.
   const transforms = {
-    creator: creator => creator.id,
-    followers: followers => followers.map(f => f.id),
-    communities: communities => communities.map(c => c.id),
-    comments: comments => comments.map(c => c.id)
+    members: members => members.map(m => m.id)
   }
 
   const transformed = {
