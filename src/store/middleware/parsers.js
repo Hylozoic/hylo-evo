3c8b42065964bd8af6e6621f29e0823ca32f9bb3
Rich Churcher <rich.churcher@gmail.com>
2017-03-24 22:11:57 +1300

    Broad outline: parses posts, needs more testing

--- src/store/middleware/parsers.js
+++ src/store/middleware/parsers.js
@@ -1,16 +1,73 @@
-export const postParser = {
-  isValid (post) {
-    const requiredProperties = [
-      'id',
-      'title',
-      'details',
-      'type'
-    ]
-    return requiredProperties.find(p => !post.hasOwnProperty(p)) ? false : true
-  },
+// Must be present for a valid model
+const required = [
+  'id',
+  'title',
+  'details',
+  'type'
+]
 
-  parse (post) {
-    return post
+// Add unaltered. If a property name isn't listed in either `passThrough` or
+// `transform`, it won't be in the store.
+const passThrough = [
+  'id',
+  'title',
+  'details',
+  'type',
+  'creator',
+  'followersTotal',
+  'communitiesTotal',
+  'commentsTotal',
+  'createdAt',
+  'startsAt',
+  'endsAt',
+  'fulfilledAt'
+]
+
+// Functions taking the value to modify as a single argument.
+// Note that most of these need to happen _after_ relations have already
+// been processed in `parserMiddleware`.
+const transform = {
+  creator: creator => creator.id,
+  followers: followers => followers.map(f => f.id),
+  communities: communities => communities.map(c => c.id),
+  comments: comments => comments.map(c => c.id)
+}
+
+// All validators will be run with the post to check as a single argument.
+const validate = {
+  hasRequiredProperties (post) {
+    return required.find(p => !post.hasOwnProperty(p)) ? false : true
   }
 }
 
+export function normalize (post) {
+  const parsedPost = {}
+
+  passThrough.forEach(key => {
+    if (post[key] !== undefined) {
+      parsedPost[key] = post[key]
+    }
+  })
+  Object.keys(transform).forEach(key => {
+    if (post[key] !== undefined) {
+      parsedPost[key] = transform[key](post[key])
+    }
+  })
+
+  return parsedPost
+}
+
+const parser = {
+  isValid (post) {
+    const validatorResults = Object.keys(validate)
+      .map(key => validate[key](post))
+
+    return !validatorResults.includes(false)
+  },
+
+  parse (post) {
+    return normalize(post)
+  }
+}
+
+export default parser
