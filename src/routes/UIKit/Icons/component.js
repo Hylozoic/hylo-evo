e92c971d1c47733770cbbbf58c19bb7e062d3c50
Lawrence Wang <l@lw.io>
2017-03-24 16:18:09 -0700

    remove accidental/unnecessary changes

--- src/routes/UIKit/Icons/component.js
+++ src/routes/UIKit/Icons/component.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Icon from 'components/Icon'
-import './component.scss' // eslint-disable-line no-unused-vars
+import './component.scss'
 
 const ICON_NAMES = [
   'Messages',
