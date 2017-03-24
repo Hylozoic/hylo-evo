1c7f2fa52e75bb898a3990afb626f0876273083e
Lawrence Wang <l@lw.io>
2017-03-24 16:02:09 -0700

    no more need for cssHeader

--- scripts/serveUniversal/setup.js
+++ scripts/serveUniversal/setup.js
@@ -5,7 +5,6 @@ import { join } from 'path'
 import { addPath } from 'app-module-path'
 import scss from 'postcss-scss'
 import cssHook from 'css-modules-require-hook'
-import { once } from 'lodash/fp'
 import root from 'root-path'
 
 const startTime = new Date().getTime()
@@ -17,17 +16,9 @@ addPath(join(__dirname, '../../src'))
 
 const sharedConfig = require(root('config/webpack.config.shared'))
 
-// add a header to each css file that gets imported, to mimic the behavior of
-// sass-resources-loader
-const cssHeader = once(() => {
-  const { resources } = sharedConfig.sassResourcesLoader.options
-  return resources.map(path => `@import "${path}";`).join('\n') + `\n`
-})
-
 // handle CSS imports and generate class names the same way that webpack does
 cssHook({
   extensions: ['.css', '.scss'],
   generateScopedName: sharedConfig.cssLoader.options.localIdentName,
-  preprocessCss: css => cssHeader() + css,
   processorOpts: {parser: scss.parse}
 })
