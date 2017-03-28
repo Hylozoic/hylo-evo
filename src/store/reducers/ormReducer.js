205d307c1ba99d7b1aec9baeebdf164f1982d106
Rich Churcher <rich.churcher@gmail.com>
2017-03-28 15:26:07 +1300

    Rearrange

--- src/store/reducers/ormReducer.js
+++ src/store/reducers/ormReducer.js
@@ -14,16 +14,19 @@ export default function ormReducer (state = {}, action) {
 
   switch (type) {
     case a.ADD_COMMENTS: add(Comment); break
-    case a.ADD_COMMUNITIES: add(Community); break
-    case a.ADD_PEOPLE: add(Person); break
-    case a.ADD_POSTS: add(Post); break
     case a.UPDATE_COMMENT: update(Comment); break
-    case a.UPDATE_COMMUNITY: update(Community); break
-    case a.UPDATE_PERSON: update(Person); break
-    case a.UPDATE_POST: update(Post); break
     case a.DELETE_COMMENT: del(Comment); break
+
+    case a.ADD_COMMUNITIES: add(Community); break
+    case a.UPDATE_COMMUNITY: update(Community); break
     case a.DELETE_COMMUNITY: del(Community); break
+
+    case a.ADD_PEOPLE: add(Person); break
+    case a.UPDATE_PERSON: update(Person); break
     case a.DELETE_PERSON: del(Person); break
+
+    case a.ADD_POSTS: add(Post); break
+    case a.UPDATE_POST: update(Post); break
     case a.DELETE_POST: del(Post); break
   }
 
