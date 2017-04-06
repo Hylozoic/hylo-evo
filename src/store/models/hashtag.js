7b6a57e5750149fe5acc35d3024867a0e7fde737
Robbie Carlton <robbie.carlton@gmail.com>
2017-04-06 17:01:36 -0600

    move text utils to hylo-utils

--- src/store/models/hashtag.js
+++
@@ -1,4 +0,0 @@
-export const invalidCharacterRegex = /[^\w_-]/
-export const hashtagCharacterRegex = /^[\w_-]$/
-export const hashtagWordRegex = /^[A-Za-z][\w_-]+$/
-export const hashtagFullRegex = /^#([A-Za-z][\w_-]+)$/
