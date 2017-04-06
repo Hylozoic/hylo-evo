308e8bde78d784295df5033fcfee7cfd144fb91d
Robbie Carlton <robbie.carlton@gmail.com>
2017-04-06 11:44:38 -0600

    present post details as html

--- src/store/models/hashtag.js
+++ src/store/models/hashtag.js
@@ -1,1 +1,4 @@
+export const invalidCharacterRegex = /[^\w_-]/
 export const hashtagCharacterRegex = /^[\w_-]$/
+export const hashtagWordRegex = /^[A-Za-z][\w_-]+$/
+export const hashtagFullRegex = /^#([A-Za-z][\w_-]+)$/
