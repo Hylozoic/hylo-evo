c673b0ba1f9112059cc32e07e30fe4f1c37a2f42
Connor Turland <connorturland@gmail.com>
2017-04-07 09:23:01 -0400

    make the websocket connection

---
+++ src/client/websockets.js
@@ -0,0 +1,25 @@
+const environment = process.env.NODE_ENV || 'development'
+const socketHost = process.env.SOCKET_HOST
+const isClient = typeof window !== 'undefined' && !window.isMock
+
+let socket // client-side singleton
+
+if (isClient) {
+  const socketIOClient = require('socket.io-client')
+  const sailsIOClient = require('sails.io.js')
+  const io = sailsIOClient(socketIOClient)
+  io.sails.url = socketHost
+  io.sails.environment = environment
+  io.sails.reconnection = true
+  socket = io.socket
+}
+
+export const socketUrl = path => `${socketHost}/${path.replace(/^\//, '')}`
+
+export const getSocket = () => {
+  if (!isClient) {
+    const noop = () => {}
+    return {get: noop, post: noop, on: noop, off: noop}
+  }
+  return socket
+}
