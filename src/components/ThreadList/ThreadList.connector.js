6882cb3d011a65ccf2054a332aaa6b14041b6bed
Connor Turland <connorturland@gmail.com>
2017-04-07 17:15:14 -0400

    avatar lists styled

--- src/components/ThreadList/ThreadList.connector.js
+++ src/components/ThreadList/ThreadList.connector.js
@@ -1,11 +1,16 @@
 import { connect } from 'react-redux'
 // import { someAction } from 'some/path/to/actions'
+import getCurrentUser from 'store/selectors/getCurrentUser'
 import { rndPerson } from 'routes/Feed/sampleData'
 const SAMPLE_PERSON_1 = rndPerson()
 const SAMPLE_PERSON_2 = rndPerson()
+const SAMPLE_PERSON_3 = rndPerson()
+const SAMPLE_PERSON_4 = rndPerson()
+const SAMPLE_PERSON_5 = rndPerson()
 
 export function mapStateToProps (state, props) {
   return {
+    currentUser: getCurrentUser(state),
     threads: [
       {
         id: '1',
@@ -14,7 +19,22 @@ export function mapStateToProps (state, props) {
       },
       {
         id: '2',
+        participants: [SAMPLE_PERSON_1, SAMPLE_PERSON_2, SAMPLE_PERSON_3],
+        messages: [{id: '1', createdAt: new Date(), message: 'Hey you', person: SAMPLE_PERSON_1}]
+      },
+      {
+        id: '3',
         participants: [SAMPLE_PERSON_1, SAMPLE_PERSON_2],
+        messages: [{id: '1', createdAt: new Date(), message: 'Hey you', person: SAMPLE_PERSON_1}]
+      },
+      {
+        id: '4',
+        participants: [SAMPLE_PERSON_1, SAMPLE_PERSON_2, SAMPLE_PERSON_3, SAMPLE_PERSON_4],
+        messages: [{id: '1', createdAt: new Date(), message: 'Hey you', person: SAMPLE_PERSON_1}]
+      },
+      {
+        id: '5',
+        participants: [SAMPLE_PERSON_1, SAMPLE_PERSON_2, SAMPLE_PERSON_3, SAMPLE_PERSON_4, SAMPLE_PERSON_5],
         messages: [{id: '1', createdAt: new Date(), message: 'Hey you', person: SAMPLE_PERSON_1}]
       }
     ]
--- src/components/ThreadList/ThreadList.js
+++ src/components/ThreadList/ThreadList.js
@@ -1,5 +1,5 @@
 import React, { PropTypes, Component } from 'react'
-import { map } from 'lodash/fp'
+import { filter, map } from 'lodash/fp'
 import cx from 'classnames'
 import { Link } from 'react-router-dom'
 import RoundImage from 'components/RoundImage'
@@ -7,16 +7,21 @@ import Badge from 'components/Badge'
 import Button from 'components/Button'
 import TextInput from 'components/TextInput'
 import './ThreadList.scss'
-const { array, string } = PropTypes
+const { array, object, string } = PropTypes
 
 export default class ThreadList extends Component {
   static propTypes = {
+    currentUser: object,
     threads: array,
     activeId: string
   }
 
+  componentDidMount () {
+    // fetchThreads
+  }
+
   render () {
-    const { threads, activeId } = this.props
+    const { currentUser, threads, activeId } = this.props
     console.log(threads)
     return <div styleName='thread-list'>
       <div styleName='header'>
@@ -29,6 +34,7 @@ export default class ThreadList extends Component {
       <ul styleName='list'>
         {threads.map(t => {
           return <ThreadListItem id={t.id}
+            currentUser={currentUser}
             active={t.id === activeId}
             participants={t.participants}
             latestMessage={t.messages[0]}
@@ -39,13 +45,14 @@ export default class ThreadList extends Component {
   }
 }
 
-function ThreadListItem ({active, id, participants, latestMessage, unreadCount}) {
+function ThreadListItem ({currentUser, active, id, participants, latestMessage, unreadCount}) {
+  const otherParticipants = filter(p => p.id !== currentUser.id, participants)
   return <li styleName='list-item'>
     <Link to={`/t/${id}`}>
       {active && <div styleName='active-thread' />}
-      <ThreadAvatars avatarUrls={map('avatarUrl', participants)} />
+      <ThreadAvatars avatarUrls={map('avatarUrl', otherParticipants)} />
       <div styleName='li-center-content'>
-        <ThreadNames names={map('name', participants)} />
+        <ThreadNames names={map('name', otherParticipants)} />
         <div styleName='thread-message-text'>{latestMessage.message}</div>
       </div>
       <div styleName='li-right-content'>
@@ -57,14 +64,32 @@ function ThreadListItem ({active, id, participants, latestMessage, unreadCount})
 }
 
 function ThreadAvatars ({avatarUrls}) {
-  // {avatarUrls[0]}
+  const count = avatarUrls.length
+  const style = `avatar-${count < 4 ? count : 'more'}`
+  const plusStyle = `avatar-${count < 4 ? count : 'more'} ${count > 4 ? 'plus-count' : ''}`
   return <div styleName='thread-avatars'>
-    <RoundImage url={avatarUrls[0]} medium />
+    {(count === 1 || count === 2) && <RoundImage url={avatarUrls[0]} />}
+    {count === 2 && <RoundImage url={avatarUrls[1]} medium styleName={style} />}
+    {count > 2 && <RoundImage url={avatarUrls[0]} medium styleName={style} />}
+    {count > 2 && <RoundImage url={avatarUrls[1]} medium styleName={style} />}
+    {count > 2 && <RoundImage url={avatarUrls[2]} medium styleName={style} />}
+    {count === 4 && <RoundImage url={avatarUrls[3]} medium styleName={style} />}
+    {count > 4 && <div styleName={`${plusStyle}`}>+{count - 4}</div>}
   </div>
 }
 
 function ThreadNames ({names}) {
+  let nameString = ''
+  switch (names.length) {
+    case 1:
+    case 2:
+      nameString = names.join(', ')
+      break
+    default:
+      nameString = `${names.slice(0, 1).join(', ')} and ${names.length - 1} other${names.length > 2 ? 's' : ''}`
+      break
+  }
   return <div styleName='thread-names'>
-    {names[0]}
+    {nameString}
   </div>
 }
