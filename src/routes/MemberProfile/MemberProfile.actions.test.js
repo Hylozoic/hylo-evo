b3594d063591237a1eaeb802f509e2ff1716148e
Rich Churcher <rich.churcher@gmail.com>
2017-04-08 16:48:36 +1200

    Accidentally deleted

---
+++ src/routes/MemberProfile/MemberProfile.actions.test.js
@@ -0,0 +1,54 @@
+import configureStore from 'redux-mock-store'
+import { get } from 'lodash/fp'
+import promiseMiddleware from 'redux-promise'
+
+import graphqlMiddleware from 'store/middleware/graphql'
+import normalizingMiddleware from 'store/middleware/normalizingMiddleware'
+import { ADD_PERSON, FETCH_PERSON } from 'store/constants'
+import payload from './MemberProfile.test.json'
+import { fetchPerson } from './MemberProfile.actions'
+
+const apiMiddleware = store => next => action => {
+  const id = get('payload.api.params.variables.id', action)
+  return id === '46816' ? next({...action, payload }) : next(action)
+}
+
+it('Returns the correct action', () => {
+  const expected = {
+    type: FETCH_PERSON,
+    graphql: {
+      query: 'A very wombaty query.',
+      variables: {
+        id: '12345'
+      }
+    }
+  }
+  const { query, variables } = expected.graphql
+  const actual = fetchPerson(variables.id, query)
+  expect(actual).toEqual(expected)
+})
+
+describe('ACTIONS', () => {
+  let store = null
+  let mockStore = configureStore([
+    graphqlMiddleware,
+    apiMiddleware,
+    normalizingMiddleware
+  ])
+
+  beforeEach(() => {
+    store = mockStore({})
+  })
+
+  it('Does not dispatch ADD_PERSON when no person in payload', () => {
+    store.dispatch({ type: 'FLARGLE_ARGLE', payload: {} })
+    const actual = store.getActions().find(a => a.type === ADD_PERSON)
+    expect(actual).toBeFalsy()
+  })
+
+  it('Dispatches ADD_PERSON when person is requested', () => {
+    store.dispatch(fetchPerson('46816'))
+    const actual = store.getActions().find(a => a.type === ADD_PERSON)
+    expect(actual).toBeTruthy()
+  })
+})
