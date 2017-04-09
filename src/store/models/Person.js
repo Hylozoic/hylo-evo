67eff2fcdb42c7df542a647d89b76949bfe74b82
Rich Churcher <rich.churcher@gmail.com>
2017-04-09 14:01:58 +1200

    Person details

--- src/store/models/Person.js
+++ src/store/models/Person.js
@@ -13,7 +13,14 @@ Person.modelName = 'Person'
 Person.fields = {
   id: attr(),
   name: attr(),
+  bio: attr(),
   avatarUrl: attr(),
   bannerUrl: attr(),
+  twitterName: attr(),
+  facebookUrl: attr(),
+  linkedinUrl: attr(),
+  url: attr(),
+  location: attr(),
+  membershipsTotal: attr(),
   postsTotal: attr()
 }
