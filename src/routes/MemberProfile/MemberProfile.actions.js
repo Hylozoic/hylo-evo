d9e09ef5eb39dda4d9fc9dbc9e4b7aa9cc72c7e7
Rich Churcher <rich.churcher@gmail.com>
2017-04-11 08:24:21 +1200

    Cleanup

--- src/routes/MemberProfile/MemberProfile.actions.js
+++ src/routes/MemberProfile/MemberProfile.actions.js
@@ -1,48 +1,42 @@
 import { FETCH_PERSON } from 'store/constants'
 
 const fetchPersonQuery =
-`query PersonWithPosts ($id: ID) {
-  person (id: $id) {
-    id
-    name
+`query PersonDetails ($slug: String, $id: ID) {
+  community (slug: $slug) {
+    id,
+    name,
     avatarUrl
-    bannerUrl
-    bio
-    twitterName
-    linkedinUrl
-    facebookUrl
-    url
-    memberships {
+    members (id: $id) {
       id
-      role
-      community {
+      name
+      avatarUrl
+      bannerUrl
+      bio
+      twitter_name
+      linkedin_url
+      facebook_url
+      url
+      location
+      posts {
         id
-        name
+        title
+        details
+        type
+        creator {
+          id
+        }
       }
+      postsTotal
     }
-    membershipsTotal
-    location
-    posts {
-      id
-      title
-      details
-      type
-      creator {
-        id
-        name
-        avatarUrl
-      }
-    }
-    postsTotal
   }
 }`
 
-export function fetchPerson (id, query = fetchPersonQuery) {
+export function fetchPerson (id, slug, query = fetchPersonQuery) {
   return {
     type: FETCH_PERSON,
     graphql: {
       query,
-      variables: { id }
+      variables: { id, slug }
     }
   }
 }
