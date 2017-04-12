2d96a3ef2242dfb14f6a7a5a0a1fac987b75af21
Rich Churcher <rich.churcher@gmail.com>
2017-04-13 09:15:46 +1200

    Simplify props for now

--- src/components/SimpleTabBar/SimpleTabBar.js
+++ src/components/SimpleTabBar/SimpleTabBar.js
@@ -1,17 +1,15 @@
 import { capitalize } from 'lodash/fp'
-import Dropdown from 'components/Dropdown'
 import React from 'react'
 
 import './SimpleTabBar.scss'
 
 export default class TabBar extends React.Component {
   static defaultProps = {
-    sortOption: 'latest',
     onChange: settings => console.log('Tab Bar changed: ', settings)
   }
 
   render () {
-    const { currentTab, tabNames, sortOption, onChange } = this.props
+    const { currentTab, tabNames, onChange } = this.props
 
     return <ul styleName='tab-bar'>
       {tabNames.map(name => <li
