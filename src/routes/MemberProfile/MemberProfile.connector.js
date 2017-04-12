267623cf101d32f599462bba22d985f6846f97e3
Rich Churcher <rich.churcher@gmail.com>
2017-04-13 08:21:04 +1200

    Move selector to store file

--- src/routes/MemberProfile/MemberProfile.connector.js
+++ src/routes/MemberProfile/MemberProfile.connector.js
@@ -1,7 +1,6 @@
-import { createSelector } from 'redux-orm'
 import { connect } from 'react-redux'
 
-import { fetchPerson } from './MemberProfile.store'
+import { fetchPerson, personSelector } from './MemberProfile.store'
 import orm from 'store/models'
 
 const defaultPerson = {
@@ -10,42 +9,6 @@ const defaultPerson = {
   bannerUrl: ''
 }
 
-// TODO: this sort of thing belongs in an i18n module
-const messages = {
-  invalid: "That doesn't seem to be a valid person ID."
-}
-
-export function getRole (slug, memberships = []) {
-  return memberships.find(m => m.community.slug === slug && m.hasModeratorRole)
-    ? 'Community Manager'
-    : null
-}
-
-const getPerson = createSelector(
-  orm,
-  state => state.orm,
-  (_, params) => params,
-  (session, params) => {
-    const { id, slug } = params
-    if (session.Person.hasId(id)) {
-      const person = session.Person.withId(id)
-      let result = {
-        ...person.ref,
-        memberships: person.memberships.toModelArray().map(membership => ({
-          ...membership.ref,
-          community: membership.community.ref
-        })),
-        posts: person.postsCreated.toModelArray().map(post => ({
-          ...post.ref,
-          communities: post.communities.toRefArray()
-        }))
-      }
-      return { ...result, role: getRole(slug, result.memberships) }
-    }
-    return defaultPerson
-  }
-)
-
 export function mapStateToProps (state, { match }) {
   const { id } = match.params
   const error = Number.isSafeInteger(Number(id)) ? null : messages.invalid
@@ -53,7 +16,7 @@ export function mapStateToProps (state, { match }) {
   return {
     id,
     error,
-    person: getPerson(state, match.params)
+    person: personSelector(state, match.params)
   }
 }
 
--- src/routes/MemberProfile/MemberProfile.store.js
+++ src/routes/MemberProfile/MemberProfile.store.js
@@ -1,3 +1,5 @@
+import { createSelector } from 'redux-orm'
+
 import { FETCH_PERSON } from 'store/constants'
 
 const fetchPersonQuery =
@@ -45,3 +47,39 @@ export function fetchPerson (id, query = fetchPersonQuery) {
     }
   }
 }
+
+// TODO: this sort of thing belongs in an i18n module
+const messages = {
+  invalid: "That doesn't seem to be a valid person ID."
+}
+
+export function getRole (slug, memberships = []) {
+  return memberships.find(m => m.community.slug === slug && m.hasModeratorRole)
+    ? 'Community Manager'
+    : null
+}
+
+export const personSelector = createSelector(
+  orm,
+  state => state.orm,
+  (_, params) => params,
+  (session, params) => {
+    const { id, slug } = params
+    if (session.Person.hasId(id)) {
+      const person = session.Person.withId(id)
+      let result = {
+        ...person.ref,
+        memberships: person.memberships.toModelArray().map(membership => ({
+          ...membership.ref,
+          community: membership.community.ref
+        })),
+        posts: person.postsCreated.toModelArray().map(post => ({
+          ...post.ref,
+          communities: post.communities.toRefArray()
+        }))
+      }
+      return { ...result, role: getRole(slug, result.memberships) }
+    }
+    return defaultPerson
+  }
+)
