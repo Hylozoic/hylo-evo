7e7797eede580b98170f45a18147e9ec9fc7352b
Lawrence Wang <l@lw.io>
2017-04-11 21:21:12 -0700

    wire up real community data in top nav

--- src/store/selectors/getCommunityForCurrentRoute.js
+++ src/store/selectors/getCommunityForCurrentRoute.js
@@ -4,7 +4,7 @@ import getParam from './getParam'
 
 const getCommunityForCurrentRoute = createSelector(
   state => orm.session(state.orm),
-  getParam('slug'),
+  (state, props) => getParam('slug', state, props) || tryLocation(props),
   (session, slug) => {
     try {
       return session.Community.get({slug})
@@ -15,3 +15,11 @@ const getCommunityForCurrentRoute = createSelector(
 )
 
 export default getCommunityForCurrentRoute
+
+// this is a workaround for fetching the slug from the current path when you are
+// in a component, like PrimaryLayout, that isn't matching a route where the
+// slug is a parameter
+function tryLocation (props) {
+  if (!props.location) return null
+  return props.location.pathname.match(/\/c\/([^/]+)/)[1]
+}
