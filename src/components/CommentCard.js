eb7e0ec1e3670c039a4ed220a0588dad4d2fd410
Rich Churcher <rich.churcher@gmail.com>
2017-04-14 08:37:32 +1200

    Styling

--- src/components/CommentCard.js
+++ src/components/CommentCard.js
@@ -5,7 +5,7 @@ import './CommentCard.scss'
 
 export default function CommentCard ({ comment }) {
   const { creator } = comment
-  return <div>
+  return <div styleName='comment-card'>
     <RoundImage url={creator.avatarUrl} large />
     {creator.name} commented on ... postname ...
   </div>
