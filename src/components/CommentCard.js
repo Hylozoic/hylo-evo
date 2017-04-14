cddb8cfbcd0bdc911f0571bae7fc51cd96e8497e
Rich Churcher <rich.churcher@gmail.com>
2017-04-14 14:27:11 +1200

    Finish CommentCard

--- src/components/CommentCard.js
+++ src/components/CommentCard.js
@@ -1,23 +1,24 @@
 import React from 'react'
 
+import Icon from 'components/Icon'
 import RoundImage from 'components/RoundImage'
+import { humanDate } from 'hylo-utils/text'
 import './CommentCard.scss'
 
 export default function CommentCard ({ comment }) {
   const { creator, post } = comment
   return <div styleName='comment-card'>
-    <CommentHeader
-      avatarUrl={creator.avatarUrl}
-      personName={creator.name}
-      postId={post.id}
-      postTitle={post.title} />
-  </div>
-}
-
-export function CommentHeader ({ avatarUrl, personName, postId, postTitle }) {
-  return <div styleName='comment-header'>
-    <RoundImage url={avatarUrl} large />
-    <span styleName='person-name'>{personName}</span> commented on
-    <span styleName='post-title'>{postTitle}</span>
+    <div styleName='comment-header'>
+      <RoundImage url={creator.avatarUrl} large />
+      <div styleName='comment-meta'>
+        <span styleName='person-name'>{creator.name}</span> commented on&nbsp;
+        <span styleName='post-title'>{post.title}</span>
+      </div>
+      <span styleName='date'>{humanDate(comment.createdAt)}</span>
+    </div>
+    <div styleName='comment-body' dangerouslySetInnerHTML={{__html: comment.text}} />
+    <div styleName='comment-footer'>
+      <a href='' ><Icon styleName='reply-button' name='Reply' green /> Reply</a>
+    </div>
   </div>
 }
