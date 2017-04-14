96cf3031c359e0534878641c800b9bb1fc34e5ca
Connor Turland <connorturland@gmail.com>
2017-04-13 20:37:43 -0400

    getMe now takes (state) instead of (state.orm)

--- src/store/selectors/getMe.js
+++ src/store/selectors/getMe.js
@@ -1,6 +1,10 @@
 import orm from '../models'
 import { createSelector as ormCreateSelector } from 'redux-orm'
 
-export const getMe = ormCreateSelector(orm, session => {
-  return session.Me.first()
-})
+export const getMe = ormCreateSelector(
+  orm,
+  state => state.orm,
+  session => {
+    return session.Me.first()
+  }
+)
