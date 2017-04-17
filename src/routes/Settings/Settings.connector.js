009ad3206ed0f9e72e8aed473132a87689566c34
Robbie Carlton <robbie.carlton@gmail.com>
2017-04-17 16:14:15 -0700

    settings basic layout

--- src/routes/Settings/Settings.connector.js
+++ src/routes/Settings/Settings.connector.js
@@ -1,9 +1,18 @@
 import { connect } from 'react-redux'
-// import { someAction } from 'some/path/to/actions'
+import { fakePerson } from 'components/PostCard/samplePost'
+
+const SAMPLE_USER = {
+  ...fakePerson(),
+  bio: 'Subtly charming problem solver. Friendly travel guru. Avid entrepreneur. Activist. Creator.',
+  location: 'Vancouver, BC',
+  website: 'www.soniajohnson.com',
+  facebookUrl: 'facebook.com/soniaj',
+  twitterUrl: 'twitter.com/soniaj'
+}
 
 export function mapStateToProps (state, props) {
   return {
-    exampleProp: 'example'
+    currentUser: SAMPLE_USER
   }
 }
 
