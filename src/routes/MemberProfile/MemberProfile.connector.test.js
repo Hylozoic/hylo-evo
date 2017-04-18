db9c291786eb645818719232b9258965f8fbffe8
Rich Churcher <rich.churcher@gmail.com>
2017-04-18 21:59:58 +1200

    Start updating tests

--- src/routes/MemberProfile/MemberProfile.connector.test.js
+++ src/routes/MemberProfile/MemberProfile.connector.test.js
@@ -1,20 +1,7 @@
-import orm from 'store/models'
-import { getPerson, mapStateToProps } from './MemberProfile.connector'
+import { mapStateToProps } from './MemberProfile.connector'
 import payload from './MemberProfile.normalized.test.json'
-import denormalized from './MemberProfile.test.json'
 
 describe('MemberProfile.connector', () => {
-  let session = null
-
-  beforeEach(() => {
-    session = orm.mutableSession(orm.getEmptyState())
-
-    const { person, posts, communities } = payload
-    session.Person.create(person)
-    session.Community.create(communities[0])
-    session.Post.create(posts[0])
-  })
-
   describe('mapStateToProps', () => {
 
     it('Sets an error when id missing from route params', () => {
@@ -31,20 +18,4 @@ describe('MemberProfile.connector', () => {
     })
   })
 
-  describe('getPerson', () => {
-    it('Returns null for a non-existent entity', () => {
-      const actual = getPerson('1')(session.state)
-        expect(actual).toBe(null)
-    })
-
-    it('Populates relations correctly', () => {
-      const { person } = denormalized.data
-        const expected = {
-          ...person,
-          posts: person.posts.map(post => ({ ...post, creator: post.creator.id }))
-        }
-      const actual = getPerson(payload.person.id)(session.state)
-        expect(actual).toEqual(expected)
-    })
-  })
 })
--- src/routes/MemberProfile/MemberProfile.store.test.js
+++ src/routes/MemberProfile/MemberProfile.store.test.js
@@ -2,15 +2,16 @@ import configureStore from 'redux-mock-store'
 import { get } from 'lodash/fp'
 import promiseMiddleware from 'redux-promise'
 
+import orm from 'store/models'
 import graphqlMiddleware from 'store/middleware/graphql'
-import normalizingMiddleware from 'store/middleware/normalizingMiddleware'
 import { ADD_PERSON, FETCH_PERSON } from 'store/constants'
 import payload from './MemberProfile.test.json'
-import { fetchPerson } from './MemberProfile.store'
+import normalized from './MemberProfile.normalized.test.json'
+import { fetchPerson, personSelector } from './MemberProfile.store'
 
 const apiMiddleware = store => next => action => {
   const { id, slug } = get('payload.api.params.variables', action) || {}
-  return id === '46816' ? next({ ...action, payload }) : next(action)
+  return id === '12345' ? next({ ...action, payload }) : next(action)
 }
 
 it('Returns the correct action', () => {
@@ -19,21 +20,44 @@ it('Returns the correct action', () => {
     graphql: {
       query: 'A very wombaty query.',
       variables: {
-        id: '12345'
+        id: '12345',
+        limit: 10,
+        order: 'desc'
       }
     }
   }
   const { query, variables } = expected.graphql
-  const actual = fetchPerson(variables.id, query)
+  const actual = fetchPerson(variables.id, 'desc', 10, query)
   expect(actual).toEqual(expected)
+})
+
+describe('personSelector', () => {
+  let session = null
+  let state = null
+  let props = null
+
+  beforeEach(() => {
+    session = orm.mutableSession(orm.getEmptyState())
+
+    const { person } = payload.data
+    session.Person.create(person)
+    state = { orm: session.state }
+    props = { match: { params: { id: '46816', slug: 'wombats' } } }
+  })
+
+  it('returns a default object for a non-existent person', () => {
+    const expected = { avatarUrl: '', bannerUrl: '', name: '' }
+    props.match.params.id = '1'
+    const actual = personSelector(state, props)
+    expect(actual).toEqual(expected)
+  })
 })
 
 describe('ACTIONS', () => {
   let store = null
   let mockStore = configureStore([
     graphqlMiddleware,
-    apiMiddleware,
-    normalizingMiddleware
+    apiMiddleware
   ])
 
   beforeEach(() => {
