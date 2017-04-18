ea1ecba326cbd087aea17a56e3a05533dd37d4d2
Robbie Carlton <robbie.carlton@gmail.com>
2017-04-18 12:51:58 -0700

    add controls

--- src/routes/Settings/Settings.connector.js
+++ src/routes/Settings/Settings.connector.js
@@ -2,13 +2,14 @@ import { connect } from 'react-redux'
 import { fakePerson, SAMPLE_IMAGE_URL } from 'components/PostCard/samplePost'
 import faker from 'faker'
 
-faker.seed(995)
+faker.seed(993)
 
 const SAMPLE_USER = {
   ...fakePerson(),
   bio: 'Subtly charming problem solver. Friendly travel guru. Avid entrepreneur. Activist. Creator.',
   location: 'Vancouver, BC',
-  website: 'www.soniajohnson.com',
+  url: 'www.soniajohnson.com',
+  email: 'sonia@soniajohnson.com',
   facebookUrl: 'facebook.com/soniaj',
   twitterUrl: 'twitter.com/soniaj',
   avatarUrl: faker.image.avatar(),
--- src/routes/Settings/Settings.js
+++ src/routes/Settings/Settings.js
@@ -2,6 +2,7 @@ import React, { PropTypes, Component } from 'react'
 import './Settings.scss'
 import { Link } from 'react-router-dom'
 import Icon from 'components/Icon'
+import Button from 'components/Button'
 import { bgImageStyle } from 'util/index'
 const { object } = PropTypes
 
@@ -20,19 +21,68 @@ export default class Settings extends Component {
         </div>
         <SettingsControls currentUser={currentUser} />
         <div styleName='right-sidebar'>
-          <CloseButton onClose={() => console.log('closed')} />
+          <CloseButton onClose={() => console.log('What do we do here?')} />
         </div>
       </div>
     </div>
   }
 }
 
-export function SettingsControls ({ currentUser }) {
-  const { name, avatarUrl, bannerUrl } = currentUser
-  return <div styleName='center'>
-    <div styleName='name'>{name}</div>
-    <div style={bgImageStyle(bannerUrl)} styleName='banner' />
-    <div style={bgImageStyle(avatarUrl)} styleName='avatar' />
+export class SettingsControls extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {edits: {}, changed: false}
+  }
+
+  componentDidMount () {
+    const { name, avatarUrl, bannerUrl, tagline, bio, location, email, url } = this.props.currentUser
+    this.setState({
+      edits: {
+        name, avatarUrl, bannerUrl, tagline, bio, location, email, url
+      }
+    })
+  }
+
+  render () {
+    const { edits, changed } = this.state
+    const { name, avatarUrl, bannerUrl, tagline, bio, location, email, url } = edits
+
+    console.log({changed})
+
+    const updateSetting = key => event => {
+      const { edits } = this.state
+      this.setState({
+        changed: true,
+        edits: {
+          ...edits,
+          [key]: event.target.value
+        }
+      })
+    }
+
+    const save = () => {
+      this.setState({changed: false})
+      console.log('saving changes', edits)
+    }
+
+    return <div styleName='center'>
+      <input styleName='name' onChange={updateSetting('name')} value={name} />
+      <div style={bgImageStyle(bannerUrl)} styleName='banner' />
+      <div style={bgImageStyle(avatarUrl)} styleName='avatar' />
+      <Control label='Tagline' onChange={updateSetting('tagline')} value={tagline} />
+      <Control label='About Me' onChange={updateSetting('bio')} value={bio} />
+      <Control label='Location' onChange={updateSetting('location')} value={location} />
+      <Control label='Email' onChange={updateSetting('email')} value={email} />
+      <Control label='Website' onChange={updateSetting('url')} value={url} />
+      <Button label='Save Changes' color={changed ? 'green' : 'gray'} onClick={changed ? save : null} />
+    </div>
+  }
+}
+
+export function Control ({ label, value, onChange }) {
+  return <div>
+    <label>{label}</label>
+    <input type='text' value={value} onChange={onChange} />
   </div>
 }
 
