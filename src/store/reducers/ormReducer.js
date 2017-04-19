1179f879d8f804075224fa05692c6f150de077ef
Lawrence Wang <l@lw.io>
2017-04-19 15:29:09 -0700

    cleanup

--- src/store/reducers/ormReducer.js
+++ src/store/reducers/ormReducer.js
@@ -14,29 +14,6 @@ export default function ormReducer (state = {}, action) {
   const del = deleteEntity(payload)
 
   switch (type) {
-    case a.ADD_COMMENT: add(Comment); break
-    case a.UPDATE_COMMENT: update(Comment); break
-    case a.DELETE_COMMENT: del(Comment); break
-
-    case a.ADD_COMMUNITY: add(Community); break
-    case a.UPDATE_COMMUNITY: update(Community); break
-    case a.DELETE_COMMUNITY: del(Community); break
-
-    case a.ADD_MEMBERSHIP: add(Membership); break
-    case a.UPDATE_MEMBERSHIP: update(Membership); break
-    case a.DELETE_MEMBERSHIP: del(Membership); break
-
-    case a.ADD_PERSON: add(Person); break
-    case a.UPDATE_PERSON: update(Person); break
-    case a.DELETE_PERSON: del(Person); break
-
-    case a.ADD_POST: add(Post); break
-    case a.UPDATE_POST: update(Post); break
-    case a.DELETE_POST: del(Post); break
-
-    case a.ADD_FEED_ITEM: add(FeedItem); break
-    case a.UPDATE_FEED_ITEM: update(FeedItem); break
-    case a.DELETE_FEED_ITEM: del(FeedItem); break
 
     case a.FETCH_CURRENT_USER:
       ModelExtractor.addAll({
