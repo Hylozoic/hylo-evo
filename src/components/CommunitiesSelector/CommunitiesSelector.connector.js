5b99e84bd63bc4b05d35379f9e37b99f59ae7935
Loren Johnson <loren@hellovenado.com>
2017-04-19 16:22:08 -0700

    Remove connector from CommunitiesSelector

--- src/components/CommunitiesSelector/CommunitiesSelector.connector.js
+++
@@ -1,9 +0,0 @@
-import { connect } from 'react-redux'
-
-export function mapStateToProps (state, props) {
-  return {}
-}
-
-export const mapDispatchToProps = {}
-
-export default connect(mapStateToProps, mapDispatchToProps)
