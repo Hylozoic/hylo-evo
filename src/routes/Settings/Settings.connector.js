2a2996b8bab7adda42cd648d460d2c0feda6672c
Robbie Carlton <robbie.carlton@gmail.com>
2017-04-19 17:17:15 -0700

    cleanup

--- src/routes/Settings/Settings.connector.js
+++ src/routes/Settings/Settings.connector.js
@@ -1,23 +1,7 @@
 import { connect } from 'react-redux'
-import { fakePerson, SAMPLE_IMAGE_URL } from 'components/PostCard/samplePost'
-import faker from 'faker'
 import { goBack } from 'react-router-redux'
 import { getMe } from 'store/selectors/getMe'
 import { fetchUserSettings, updateUserSettings } from './Settings.store'
-
-faker.seed(993)
-
-const SAMPLE_USER = {
-  ...fakePerson(),
-  bio: 'Subtly charming problem solver. Friendly travel guru. Avid entrepreneur. Activist. Creator.',
-  location: 'Vancouver, BC',
-  url: 'www.soniajohnson.com',
-  email: 'sonia@soniajohnson.com',
-  facebookUrl: 'facebook.com/soniaj',
-  twitterUrl: 'twitter.com/soniaj',
-  avatarUrl: faker.image.avatar(),
-  bannerUrl: SAMPLE_IMAGE_URL
-}
 
 export function mapStateToProps (state, props) {
   return {
