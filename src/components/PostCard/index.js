88b01608e414837c4d2c48e0e9a08a9464326298
Lawrence Wang <l@lw.io>
2017-04-20 15:37:41 -0700

    cleanup and rename PostCard files

--- src/components/PostCard/index.js
+++ src/components/PostCard/index.js
@@ -1,3 +1,4 @@
-import component from './component'
-import connector from './connector'
+import component from './PostCard'
+import connector from './PostCard.connector'
 export default connector(component)
+export { PostHeader, PostImage, PostBody, PostFooter } from './PostCard'
