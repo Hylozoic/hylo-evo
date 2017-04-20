dc47ac0632821a206fa6b9adc37db731ae75c13f
Lawrence Wang <l@lw.io>
2017-04-20 15:58:02 -0700

    rename Feed to CommunityFeed

---
+++ src/routes/CommunityFeed/index.js
@@ -0,0 +1,3 @@
+import component from './CommunityFeed'
+import connector from './CommunityFeed.connector'
+export default connector(component)
--- src/routes/PrimaryLayout/components/CommunitiesDrawer/CommunitiesDrawer.connector.js
+++ src/routes/PrimaryLayout/components/CommunitiesDrawer/CommunitiesDrawer.connector.js
@@ -1,7 +1,7 @@
 import { connect } from 'react-redux'
 import { createSelector as ormCreateSelector } from 'redux-orm'
 import orm from 'store/models'
-import { SAMPLE_COMMUNITY } from 'routes/Feed/sampleData'
+import { SAMPLE_COMMUNITY } from 'routes/CommunityFeed/sampleData'
 import { toggleCommunitiesDrawer } from 'routes/PrimaryLayout/PrimaryLayout.store'
 
 export const getCommunities = ormCreateSelector(
