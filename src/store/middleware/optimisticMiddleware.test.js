e9aa02aa11364e3c9ff6b73bf02e99fc72642752
Robbie Carlton <robbie.carlton@gmail.com>
2017-04-21 16:16:00 -0700

    move middleware tests to individual files

---
+++ src/store/middleware/optimisticMiddleware.test.js
@@ -0,0 +1,36 @@
+import optimisticMiddleware from 'store/middleware/optimisticMiddleware'
+import { SET_STATE } from 'store/constants'
+
+describe('optimisticMiddleware', () => {
+  let next, initialState, store, middleware
+
+  beforeEach(() => {
+    initialState = {
+      aReducer: {a: 1, b: 2}
+    }
+    store = {dispatch: jest.fn(), getState: () => initialState}
+    middleware = optimisticMiddleware(store)
+    next = jest.fn(val => Promise.resolve(val))
+  })
+
+  it('continues the chain for a non optimistic action', () => {
+    let action = {type: 'FOO', payload: {id: 3}}
+    middleware(next)(action)
+    expect(next).toHaveBeenCalled()
+  })
+
+  describe('with a promise payload and meta.optimistic', () => {
+    it('dispatches SET_STATE on error', () => {
+      const action = {
+        type: 'FOO',
+        payload: Promise.reject(new Error('promise failed')),
+        meta: {optimistic: true}
+      }
+      return middleware(next)(action)
+      .then(() => {
+        expect(next).toHaveBeenCalled()
+        expect(store.dispatch).toBeCalledWith({type: SET_STATE, payload: initialState})
+      })
+    })
+  })
+})
