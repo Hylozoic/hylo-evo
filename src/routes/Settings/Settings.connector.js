17517c85bb856658afa2d206849f140fd38a9f0e
Robbie Carlton <robbie.carlton@gmail.com>
2017-04-21 14:43:06 -0700

    rearrange

--- src/routes/Settings/Settings.connector.js
+++ src/routes/Settings/Settings.connector.js
@@ -1,7 +1,19 @@
 import { connect } from 'react-redux'
 import { goBack } from 'react-router-redux'
 import { getMe } from 'store/selectors/getMe'
-import { fetchUserSettings, updateUserSettings, leaveCommunity, getCurrentUserCommunities } from './Settings.store'
+import { fetchUserSettings, updateUserSettings, leaveCommunity } from './Settings.store'
+import { createSelector as ormCreateSelector } from 'redux-orm'
+import orm from 'store/models'
+
+// this selector assumes that all Memberships belong to the currentUser
+// using this instead of currentUser.memberships to avoid a memoization issue
+// see https://github.com/tommikaikkonen/redux-orm/issues/117
+export const getCurrentUserCommunities = ormCreateSelector(
+  orm,
+  state => state.orm,
+  (session) =>
+    session.Membership.all().toModelArray().map(m => m.community)
+)
 
 export function mapStateToProps (state, props) {
   const currentUser = getMe(state, props)
