c0ba2400e1bcd3be3be2eeb246b7340f4834edbf
Rich Churcher <rich.churcher@gmail.com>
2017-04-27 19:20:58 +1200

    Update MemberVotes with showDetails

--- src/routes/MemberProfile/MemberVotes/MemberVotes.connector.js
+++ src/routes/MemberProfile/MemberVotes/MemberVotes.connector.js
@@ -4,6 +4,7 @@ import { memberVotesSelector, fetchMemberVotes } from './MemberVotes.store'
 
 export function mapStateToProps (state, props) {
   return {
+    showDetails: (id, slug) => props.navigate(`/c/${slug}/p/${id}`),
     votes: memberVotesSelector(state, props)
   }
 }
