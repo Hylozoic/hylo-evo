14a203c48f326cecac7ad1cb0f87bbe3e6ead479
Rich Churcher <rich.churcher@gmail.com>
2017-04-28 13:25:45 +1200

    Test fetchPeople supression on backspace

--- src/components/PeopleSelector/PeopleSelector.test.js
+++ src/components/PeopleSelector/PeopleSelector.test.js
@@ -1,6 +1,7 @@
-import { shallow } from 'enzyme'
+import { mount, shallow } from 'enzyme'
 import React from 'react'
 
+import { keyMap } from 'util/textInput'
 import PeopleSelector from './PeopleSelector'
 
 it('matches the last snapshot', () => {
@@ -15,4 +16,22 @@ it('matches the last snapshot', () => {
       matches={matches} />
   )
   expect(wrapper).toMatchSnapshot()
+})
+
+it('does not hit server when backspace is pressed', () => {
+  const fetchPeople = jest.fn()
+  const wrapper = mount(
+    <PeopleSelector matches={[]} fetchPeople={fetchPeople} />
+  )
+  wrapper.find('input').first().simulate('keyDown', { keyCode: keyMap.BACKSPACE })
+  expect(fetchPeople).not.toHaveBeenCalled()
+})
+
+it('hits server when keys other than backspace are pressed', () => {
+  const fetchPeople = jest.fn()
+  const wrapper = mount(
+    <PeopleSelector matches={[]} fetchPeople={fetchPeople} setAutocomplete={jest.fn()} />
+  )
+  wrapper.find('input').first().simulate('keyDown', { keyCode: keyMap.SPACE })
+  expect(fetchPeople).toHaveBeenCalled()
 })
