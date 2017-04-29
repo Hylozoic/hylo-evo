dd45c99f6fefd5aefff88f15e4477831451e0b55
Rich Churcher <rich.churcher@gmail.com>
2017-04-29 15:58:31 +1200

    calls deleteMatch when close button clicked

--- src/components/SelectorMatchedItem/SelectorMatchedItem.test.js
+++ src/components/SelectorMatchedItem/SelectorMatchedItem.test.js
@@ -1,4 +1,4 @@
-import { shallow } from 'enzyme'
+import { mount, shallow } from 'enzyme'
 import React from 'react'
 
 import SelectorMatchedItem from './SelectorMatchedItem'
@@ -6,4 +6,13 @@ import SelectorMatchedItem from './SelectorMatchedItem'
 it('matches last snapshot', () => {
   const wrapper = shallow(<SelectorMatchedItem />)
   expect(wrapper).toMatchSnapshot()
+})
+
+it('calls deleteMatch when close button clicked', () => {
+  const deleteMatch = jest.fn()
+  const wrapper = mount(
+    <SelectorMatchedItem deleteMatch={deleteMatch} />
+  )
+  wrapper.find('span').last().simulate('click')
+  expect(deleteMatch).toHaveBeenCalled()
 })
