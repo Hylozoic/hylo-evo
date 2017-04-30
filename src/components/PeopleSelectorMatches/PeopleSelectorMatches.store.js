6b3f7c33dd50fddb12d2fff606fac314e5c3daf0
Rich Churcher <rich.churcher@gmail.com>
2017-05-01 09:19:25 +1200

    Selector testing

--- src/components/PeopleSelectorMatches/PeopleSelectorMatches.store.js
+++ src/components/PeopleSelectorMatches/PeopleSelectorMatches.store.js
@@ -2,8 +2,6 @@ import { createSelector } from 'redux-orm'
 
 import orm from 'store/models'
 
-export const MODULE_NAME = 'PeopleSelectorMatches'
-
 export const matchesSelector = createSelector(
   orm,
   state => state.orm,
