1c24564bc67d7ab3e9e76889a327a0d192955d5e
Connor Turland <connorturland@gmail.com>
2017-05-01 10:30:04 -0400

    remove gradient and change resize to not flicker

--- src/components/MessageForm/MessageForm.js
+++ src/components/MessageForm/MessageForm.js
@@ -65,9 +65,15 @@ export default class MessageForm extends React.Component {
     this.sendIsTyping(true)
   }, STARTED_TYPING_INTERVAL)
 
-  resize = () => {
-    this.refs.editor.style.height = 0
-    this.refs.editor.style.height = this.refs.editor.scrollHeight + 'px'
+  resize = (force) => {
+    const editor = this.refs.editor
+    const cloned = editor.cloneNode()
+    cloned.style.height = 0
+    editor.parentElement.appendChild(cloned)
+    if (editor.style.height !== cloned.scrollHeight + 'px') {
+      editor.style.height = cloned.scrollHeight + 'px'
+    }
+    editor.parentElement.removeChild(cloned)
   }
 
   render () {
