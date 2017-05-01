d666e9ad983bec58fb35f9d5cda0ca879b38d084
Rich Churcher <rich.churcher@gmail.com>
2017-05-01 12:06:12 +1200

    Update snapshots

--- src/components/PersonListItem/PersonListItem.test.js
+++ src/components/PersonListItem/PersonListItem.test.js
@@ -3,6 +3,12 @@ import { shallow } from 'enzyme'
 import React from 'react'
 
 it('matches the last snapshot', () => {
-  const wrapper = shallow(<PersonListItem />)
+  const person = {
+    active: true,
+    id: '1',
+    name: 'Wombat',
+    avatarUrl: 'https://wombat.life'
+  }
+  const wrapper = shallow(<PersonListItem addMatch={() => {}} person={person} />)
   expect(wrapper).toMatchSnapshot()
 })
