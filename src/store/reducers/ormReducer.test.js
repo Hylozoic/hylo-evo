0cd45903229f90578129afe5a6216f48c9c74d81
Robbie Carlton <robbie.carlton@gmail.com>
2017-05-02 15:55:23 -0700

    add VOTE_ON_POST_PENDING test to ormReducer

--- src/store/reducers/ormReducer.test.js
+++ src/store/reducers/ormReducer.test.js
@@ -1,6 +1,6 @@
 import orm from 'store/models' // this initializes redux-orm
 import ormReducer from './ormReducer'
-import { EXTRACT_MODEL } from 'store/constants'
+import { EXTRACT_MODEL, VOTE_ON_POST_PENDING } from 'store/constants'
 
 it('responds to EXTRACT_MODEL', () => {
   const state = orm.getEmptyState()
@@ -45,5 +45,42 @@ it('responds to EXTRACT_MODEL', () => {
       items: [0],
       itemsById: {'0': {fromPostId: '1', toCommunityId: '1', id: 0}}
     }
+  })
+})
+
+describe('on VOTE_ON_POST_PENDING', () => {
+  const session = orm.session(orm.getEmptyState())
+
+  session.Post.create({id: '1', votesTotal: 7, myVote: false})
+  session.Post.create({id: '2', votesTotal: 4, myVote: true})
+
+  const state = session.state
+
+  const action = {
+    type: VOTE_ON_POST_PENDING
+  }
+
+  describe('when myVote is false', () => {
+    it('does nothing if didVote is false', () => {
+      expect(ormReducer(state, {...action, meta: {postId: '1', didVote: false}}))
+      .toMatchSnapshot()
+    })
+
+    it('increments votesTotal and updates myVote if didVote is true', () => {
+      expect(ormReducer(state, {...action, meta: {postId: '1', didVote: true}}))
+      .toMatchSnapshot()
+    })
+  })
+
+  describe('when myVote is true', () => {
+    it('does nothing if didVote is true', () => {
+      expect(ormReducer(state, {...action, meta: {postId: '2', didVote: false}}))
+      .toMatchSnapshot()
+    })
+
+    it('decrements votesTotal and updates myVote if didVote is false', () => {
+      expect(ormReducer(state, {...action, meta: {postId: '2', didVote: true}}))
+      .toMatchSnapshot()
+    })
   })
 })
