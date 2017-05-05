635e9c0a9baa09e9665a28a77ff3721765d17b75
Rich Churcher <rich.churcher@gmail.com>
2017-05-05 16:58:59 +1200

    More complete snapshot

--- src/components/PeopleSelector/PeopleSelectorMatches/PeopleSelectorMatches.test.js
+++ src/components/PeopleSelector/PeopleSelectorMatches/PeopleSelectorMatches.test.js
@@ -4,6 +4,26 @@ import React from 'react'
 import PeopleSelectorMatches from './PeopleSelectorMatches'
 
 it('does something', () => {
-  const wrapper = shallow(<PeopleSelectorMatches />)
+  const matches = [
+    {
+      id: '1',
+      name: 'Wombat',
+      avatarUrl: 'https://flargle.com',
+      community: 'Wombats'
+    },
+    {
+      id: '2',
+      name: 'Aardvark',
+      avatarUrl: 'https://argle.com',
+      community: 'Aardvarks'
+    },
+    {
+      id: '3',
+      name: 'Ocelot',
+      avatarUrl: 'https://wargle.com',
+      community: 'Ocelots'
+    }
+  ]
+  const wrapper = shallow(<PeopleSelectorMatches matches={matches} />)
   expect(wrapper).toMatchSnapshot()
 })
