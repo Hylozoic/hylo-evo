2e988ac296221f8022164f6f88efd36760d68aaa
Lawrence Wang <l@lw.io>
2017-05-05 11:40:45 -0700

    remove events link

--- src/routes/PrimaryLayout/components/Navigation/Navigation.js
+++ src/routes/PrimaryLayout/components/Navigation/Navigation.js
@@ -11,7 +11,6 @@ export default function Navigation ({ className, collapsed, location }) {
 
   const links = [
     {label: 'Home', icon: 'Home', to: slug ? `/c/${slug}` : '/all', badge: 3, exact: true},
-    {label: 'Events', icon: 'Events', to: `/c/${slug}/events`, badge: 3},
     {label: 'Members', icon: 'Members', to: `/c/${slug}/members`},
     {label: 'UI Kit', icon: 'Projects', to: '/ui-kit'}
   ]
