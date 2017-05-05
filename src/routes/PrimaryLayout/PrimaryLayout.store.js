b9968eff107e4a69e71770a4d3dc7b352237b943
Lawrence Wang <l@lw.io>
2017-05-05 15:41:11 -0700

    move fetchCurrentUser into its own file

--- src/routes/PrimaryLayout/PrimaryLayout.store.js
+++ src/routes/PrimaryLayout/PrimaryLayout.store.js
@@ -1,35 +1,4 @@
-import {
-  FETCH_CURRENT_USER,
-  TOGGLE_COMMUNITIES_DRAWER
-} from 'store/constants'
-
-export function fetchCurrentUser () {
-  return {
-    type: FETCH_CURRENT_USER,
-    graphql: {
-      query: `{
-        me {
-          id
-          name
-          avatarUrl
-          memberships {
-            id
-            lastViewedAt
-            community {
-              id
-              name
-              slug
-              avatarUrl
-            }
-          }
-        }
-      }`
-    },
-    meta: {
-      extractModel: 'Me'
-    }
-  }
-}
+import { TOGGLE_COMMUNITIES_DRAWER } from 'store/constants'
 
 export function toggleDrawer () {
   return {
