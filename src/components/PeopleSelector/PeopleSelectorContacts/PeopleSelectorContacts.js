3c27e5395f5ba3dfa70fcaae95eb0c4e6349bb03
Rich Churcher <rich.churcher@gmail.com>
2017-05-05 15:59:22 +1200

    Simple contacts display

--- src/components/PeopleSelector/PeopleSelectorContacts/PeopleSelectorContacts.js
+++ src/components/PeopleSelector/PeopleSelectorContacts/PeopleSelectorContacts.js
@@ -1,10 +1,31 @@
 import React from 'react'
+const { any, arrayOf, func, shape, string } = React.PropTypes
 
+import PersonListItem from 'components/PersonListItem'
 import './PeopleSelectorContacts.scss'
 
-export default function PeopleSelectorContacts () {
-  return <div styleName='people-selector-contacts'>PeopleSelectorContacts</div>
+export default function PeopleSelectorContacts ({ addParticipant, contacts }) {
+  return <div styleName='people-selector-contacts'>
+    <h2>Recent</h2>
+    <h2>All Contacts</h2>
+    <ul styleName='contact-list'>
+      {contacts && contacts.map(contact =>
+        <PersonListItem
+          key={contact.id}
+          person={contact}
+          onClick={() => addParticipant(contact.id)} />)}
+    </ul>
+  </div>
 }
 
+const personType = shape({
+  id: any,
+  name: string,
+  avatarUrl: string,
+  community: string
+})
+
 PeopleSelectorContacts.propTypes = {
+  addParticipant: func,
+  contacts: arrayOf(personType)
 }
