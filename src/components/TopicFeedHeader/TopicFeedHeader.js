aaa5089a545d523a5c3de593f5357f50177380f1
Connor Turland <connorturland@gmail.com>
2017-05-08 19:25:12 -0400

    add the back icon

--- src/components/TopicFeedHeader/TopicFeedHeader.js
+++ src/components/TopicFeedHeader/TopicFeedHeader.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import Icon from 'components/Icon'
 import { communityUrl } from 'util/index'
 import './TopicFeedHeader.scss'
 
@@ -10,7 +11,7 @@ const pluralize = (count, word) => `${count} ${word}${count === 1 ? '' : 's'}`
 function TopicFeedHeader ({ communityTopic, community }) {
   const { postsTotal, followersTotal, topic } = communityTopic
   return <div styleName='topic-feed-header'>
-    <Link to={communityUrl(community.slug)} styleName='back'>&lt;- back to {community.name}</Link>
+    <Link to={communityUrl(community.slug)} styleName='back'><Icon name='Back' styleName='back-icon' /> back to {community.name}</Link>
     <div styleName='topic-name'>#{topic.name}</div>
     <div styleName='meta'>{pluralize(postsTotal, 'post')} • {pluralize(followersTotal, 'follower')}</div>
   </div>
