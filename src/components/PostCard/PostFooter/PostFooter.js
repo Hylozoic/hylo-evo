48a60ac63e90c0224c256d004c4283b710fd395d
Connor Turland <connorturland@gmail.com>
2017-05-07 21:50:16 -0400

    use the currentUser to use 'you' phrasing

--- src/components/PostCard/PostFooter/PostFooter.js
+++ src/components/PostCard/PostFooter/PostFooter.js
@@ -1,15 +1,16 @@
 import React from 'react'
+import { get, sortBy } from 'lodash/fp'
 import './PostFooter.scss'
 import Icon from 'components/Icon'
 import RoundImageRow from 'components/RoundImageRow'
 import cx from 'classnames'
 
-const { string, array, number, func } = React.PropTypes
+const { string, array, number, func, object } = React.PropTypes
 
-export default function PostFooter ({ id, commenters, commentersTotal, votesTotal, myVote, vote }) {
+export default function PostFooter ({ id, currentUser, commenters, commentersTotal, votesTotal, myVote, vote }) {
   return <div styleName='footer'>
     <RoundImageRow imageUrls={(commenters).map(c => c.avatarUrl)} styleName='people' />
-    <span styleName='caption'>{commentCaption(commenters, commentersTotal)}</span>
+    <span styleName='caption'>{commentCaption(get('id', currentUser), commenters, commentersTotal)}</span>
     <div styleName='votes'>
       <a onClick={vote} styleName={cx('votes-link', {voted: myVote})} data-on-click='true'>
         <Icon name='ArrowUp' styleName='arrowIcon' data-on-click='true' />{votesTotal}
@@ -22,20 +23,22 @@ PostFooter.propTypes = {
   commenters: array,
   commentersTotal: number,
   votesTotal: number,
-  vote: func
+  vote: func,
+  currentUser: object
 }
 
-export const commentCaption = (commenters, commentersTotal) => {
+export const commentCaption = (meId, commenters, commentersTotal) => {
+  const commentersSorted = sortBy(c => c.id !== meId, commenters)
   var names = ''
-  const firstName = person => person.name.split(' ')[0]
+  const firstName = person => person.id === meId ? 'You' : person.name.split(' ')[0]
   if (commentersTotal === 0) {
     return 'Be the first to comment'
   } else if (commentersTotal === 1) {
-    names = firstName(commenters[0])
+    names = firstName(commentersSorted[0])
   } else if (commentersTotal === 2) {
-    names = `${firstName(commenters[0])} and ${firstName(commenters[1])}`
+    names = `${firstName(commentersSorted[0])} and ${firstName(commentersSorted[1])}`
   } else {
-    names = `${firstName(commenters[0])}, ${firstName(commenters[1])} and ${commentersTotal - 2} other${commentersTotal - 2 > 1 ? 's' : ''}`
+    names = `${firstName(commentersSorted[0])}, ${firstName(commentersSorted[1])} and ${commentersTotal - 2} other${commentersTotal - 2 > 1 ? 's' : ''}`
   }
   return `${names} commented`
 }
--- src/components/PostCard/PostFooter/PostFooter.test.js
+++ src/components/PostCard/PostFooter/PostFooter.test.js
@@ -3,14 +3,19 @@ import { shallow } from 'enzyme'
 import React from 'react'
 
 const commenters = [
-  {name: 'Joe Smith'},
-  {name: 'Sue Jones'},
-  {name: 'Scary Terry'},
-  {name: 'John Larkin'}
+  {name: 'Joe Smith', id: '1'},
+  {name: 'Sue Jones', id: '2'},
+  {name: 'Scary Terry', id: '3'},
+  {name: 'John Larkin', id: '4'}
+]
+
+const commentersUnsorted = [
+  {name: 'Sue Jones', id: '2'},
+  {name: 'Joe Smith', id: '1'}
 ]
 
 describe('PostFooter', () => {
-  it('does something', () => {
+  it('matches the latest snapshot', () => {
     const wrapper = shallow(<PostFooter
       commenters={commenters}
       commentersTotal={4}
@@ -22,10 +27,17 @@ describe('PostFooter', () => {
 
 describe('commentCaption', () => {
   it('returns the correct text', () => {
-    expect(commentCaption([], 0)).toEqual('Be the first to comment')
-    expect(commentCaption(commenters.slice(0, 1), 1)).toEqual('Joe commented')
-    expect(commentCaption(commenters.slice(0, 2), 2)).toEqual('Joe and Sue commented')
-    expect(commentCaption(commenters.slice(0, 3), 3)).toEqual('Joe, Sue and 1 other commented')
-    expect(commentCaption(commenters.slice(0, 4), 4)).toEqual('Joe, Sue and 2 others commented')
+    const currentUserId = '1'
+    expect(commentCaption(undefined, [], 0)).toEqual('Be the first to comment')
+    expect(commentCaption(undefined, commenters.slice(0, 1), 1)).toEqual('Joe commented')
+    expect(commentCaption(currentUserId, commenters.slice(0, 1), 1)).toEqual('You commented')
+    expect(commentCaption(undefined, commenters.slice(0, 2), 2)).toEqual('Joe and Sue commented')
+    expect(commentCaption(undefined, commenters.slice(0, 3), 3)).toEqual('Joe, Sue and 1 other commented')
+    expect(commentCaption(undefined, commenters.slice(0, 4), 4)).toEqual('Joe, Sue and 2 others commented')
+  })
+
+  it('will sort them so that the currentUser is listed first', () => {
+    const currentUserId = '1'
+    expect(commentCaption(currentUserId, commentersUnsorted, 2)).toEqual('You and Sue commented')
   })
 })
