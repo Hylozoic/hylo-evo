f32eeead898ed07a20bdbabf99deaae526e73e43
Robbie Carlton <robbie.carlton@gmail.com>
2017-05-08 14:49:50 -0700

    RoundImageRow can display vertically, use in Messages Dropdown

--- src/components/RoundImageRow/RoundImageRow.js
+++ src/components/RoundImageRow/RoundImageRow.js
@@ -2,14 +2,26 @@ import React from 'react'
 import RoundImage from 'components/RoundImage'
 import './RoundImageRow.scss'
 
-const { array, string } = React.PropTypes
+const { array, string, bool } = React.PropTypes
 
-export default function RoundImageRow ({ imageUrls = [], className }) {
+export default function RoundImageRow ({ imageUrls = [], className, vertical, cap }) {
+  var capped
+  var extra
+  if (cap && cap < imageUrls.length) {
+    capped = true
+    extra = imageUrls.length - cap
+    imageUrls = imageUrls.slice(0, cap)
+  }
   const images = imageUrls.map((url, i) =>
-    <RoundImage url={url} key={i} medium overlaps />)
-  return <div className={className}>{images}</div>
+    <RoundImage url={url} key={i} medium overlaps={!vertical} overlapsVertical={vertical} />)
+
+  const plus = <div styleName='plus' key='plus'>+{extra}</div>
+  return <div className={className}>
+    {capped ? images.concat([plus]) : images}
+  </div>
 }
 RoundImageRow.propTypes = {
   imageUrls: array,
-  className: string
+  className: string,
+  vertical: bool
 }
--- src/routes/PrimaryLayout/components/TopNav/MessagesDropdown/MessagesDropdown.js
+++ src/routes/PrimaryLayout/components/TopNav/MessagesDropdown/MessagesDropdown.js
@@ -1,12 +1,11 @@
 import React, { PropTypes, Component } from 'react'
 import './MessagesDropdown.scss'
 const { object, array, string, func } = PropTypes
-import RoundImage from 'components/RoundImage'
 import { Link } from 'react-router-dom'
 import { humanDate } from 'hylo-utils/text'
 import cx from 'classnames'
 import { newMessageUrl } from 'util/index'
-import Loading from 'components/Loading'
+import RoundImageRow from 'components/RoundImageRow'
 
 export default class MessagesDropdown extends Component {
   static propTypes = {
@@ -58,17 +57,29 @@ export default class MessagesDropdown extends Component {
   }
 }
 
+const participantNames = participants => {
+  const length = participants.length
+  if (length === 1) {
+    return participants[0].name
+  } else if (length === 2) {
+    return `${participants[0].name} and ${participants[1].name}`
+  } else if (length > 2) {
+    const n = length - 2
+    return `${participants[0].name}, ${participants[1].name} and ${n} other${n > 1 ? 's' : ''}`
+  }
+}
+
 export function Thread ({ thread, goToThread, currentUserId }) {
   const message = thread.messages[0]
   if (!message || !message.text) return null
-  const participant = thread.participants.filter(p => p.id !== currentUserId)[0]
+  const participants = thread.participants.filter(p => p.id !== currentUserId)
   return <li styleName='thread'
     onClick={goToThread(thread.id)}>
     <div styleName='image-wraper'>
-      <RoundImage url={participant.avatarUrl} styleName='image' />
+      <RoundImageRow imageUrls={participants.map(p => p.avatarUrl)} vertical cap='2' />
     </div>
     <div styleName='message-content'>
-      <div styleName='name'>{participant.name}</div>
+      <div styleName='name'>{participantNames(participants)}</div>
       <div styleName='body'>{thread.messages[0].text}</div>
       <div styleName='date'>{humanDate(thread.updatedAt)}</div>
     </div>
