90b271556fa496bba8a158813aba529786ada446
Loren Johnson <loren@hellovenado.com>
2017-05-10 11:31:45 -0700

    Move shared actions and selector to src/store

--- src/routes/PostDetail/PostDetail.connector.js
+++ src/routes/PostDetail/PostDetail.connector.js
@@ -1,28 +1,8 @@
 import { connect } from 'react-redux'
-import { fetchPost } from './PostDetail.store'
-import { createSelector } from 'reselect'
-import getParam from 'store/selectors/getParam'
-import orm from 'store/models'
 import { push } from 'react-router-redux'
-
-export const getPost = createSelector(
-  state => state,
-  state => orm.session(state.orm),
-  (state, props) => getParam('postId', state, props),
-  (state, session, id) => {
-    try {
-      const post = session.Post.get({id})
-      return {
-        ...post.ref,
-        creator: post.creator,
-        commenters: post.commenters.toModelArray(),
-        communities: post.communities.toModelArray()
-      }
-    } catch (e) {
-      return null
-    }
-  }
-)
+import { fetchPost } from 'store/actions/fetchPost'
+import getParam from 'store/selectors/getParam'
+import getPost from 'store/selectors/getPost'
 
 export function mapStateToProps (state, props) {
   const slug = getParam('slug', state, props)
