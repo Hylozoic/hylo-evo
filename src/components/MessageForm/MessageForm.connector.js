f17b7510139f93751b36b845cddc6bcf6e1f0972
Lawrence Wang <l@lw.io>
2017-05-11 22:12:20 -0700

    move socket reference from MessageForm component to its connector

--- src/components/MessageForm/MessageForm.connector.js
+++ src/components/MessageForm/MessageForm.connector.js
@@ -1,3 +1,4 @@
+import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
 import { push } from 'react-router-redux'
 import { getMe } from 'store/selectors/getMe'
@@ -7,20 +8,31 @@ import {
   updateMessageText
 } from './MessageForm.store'
 import { CREATE_MESSAGE, FIND_OR_CREATE_THREAD } from 'store/constants'
-import { getSocket } from 'client/websockets'
+import { getSocket, socketUrl } from 'client/websockets'
 
 export function mapStateToProps (state, props) {
   return {
     text: getTextForMessageThread(state, props),
     pending: !!state.pending[CREATE_MESSAGE] || (props.forNewThread && !!state.pending[FIND_OR_CREATE_THREAD]),
-    currentUser: getMe(state),
-    socket: getSocket()
+    currentUser: getMe(state)
   }
 }
 
-export const mapDispatchToProps = {
-  createMessage,
-  updateMessageText,
-  goToThread: messageThreadId => push(`/t/${messageThreadId}`)
+export function mapDispatchToProps (dispatch, props) {
+  const socket = getSocket()
+
+  return {
+    ...bindActionCreators({
+      createMessage,
+      updateMessageText,
+      goToThread: messageThreadId => push(`/t/${messageThreadId}`)
+    }, dispatch),
+
+    sendIsTyping: isTyping => {
+      const url = socketUrl(`/noo/post/${props.messageThreadId}/typing`)
+      socket.post(url, {isTyping})
+    }
+  }
 }
+
 export default connect(mapStateToProps, mapDispatchToProps, null, {withRef: true})
