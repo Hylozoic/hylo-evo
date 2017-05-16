366aa66317429aa8ad7f42aebc502f539133fe31
Connor Turland <connorturland@gmail.com>
2017-05-16 12:18:30 -0400

    write tests for TopicFeedHeader

--- src/components/TopicFeedHeader/TopicFeedHeader.js
+++ src/components/TopicFeedHeader/TopicFeedHeader.js
@@ -9,14 +9,15 @@ const { string, number, object, shape, func } = React.PropTypes
 
 const pluralize = (count, word) => `${count} ${word}${count === 1 ? '' : 's'}`
 
-function TopicFeedHeader ({ subscription, topicName, postsTotal, followersTotal, topic, community, toggleTopicSubscribe }) {
+function TopicFeedHeader ({ subscription, topicName, postsTotal, followersTotal, community, toggleTopicSubscribe }) {
   const url = community ? communityUrl(community.slug) : '/all'
   const name = community ? community.name : 'All Communities'
-  const metaDefined = postsTotal && followersTotal ? true : null
+  postsTotal = postsTotal || 0
+  followersTotal = followersTotal || 0
   return <div styleName='topic-feed-header'>
     <Link to={url} styleName='back'><Icon name='Back' styleName='back-icon' /> back to {name}</Link>
     <div styleName='topic-name'>#{topicName}</div>
-    {metaDefined && <div styleName='meta'>{pluralize(postsTotal, 'post')} &nbsp;•&nbsp; {pluralize(followersTotal, 'follower')}</div>}
+    <div styleName='meta'>{pluralize(postsTotal, 'post')} • {pluralize(followersTotal, 'follower')}</div>
     {community && <Button styleName='subscribe' onClick={toggleTopicSubscribe}>{subscription ? 'Unsubscribe' : 'Subscribe'}</Button>}
   </div>
 }
@@ -26,10 +27,6 @@ TopicFeedHeader.propTypes = {
   topicName: string,
   postsTotal: number,
   followersTotal: number,
-  topic: shape({
-    id: string,
-    name: string
-  }),
   community: shape({
     id: string,
     name: string,
