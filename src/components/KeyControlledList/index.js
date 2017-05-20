87e9abeb517d884fcbcdadad8abc37a0d0165850
Loren Johnson <loren@hellovenado.com>
2017-05-20 09:36:22 -0700

    CommunitiesSelector updates necessary for PostEditor

--- src/components/KeyControlledList/index.js
+++ src/components/KeyControlledList/index.js
@@ -1,4 +1,4 @@
 // LEJ: This comonent violates the boundary of a "pod" in that
 //      a helper component of the default is referenced and used
 //      outside the pod.
-export * from './component'
+export * from './KeyControlledList'
