bec2f7496a7c3439569de82f6efef91cd7a042cf
Lawrence Wang <l@lw.io>
2017-05-19 17:43:03 -0700

    timezones! XP

--- src/components/PostCard/PostCard.test.js
+++ src/components/PostCard/PostCard.test.js
@@ -3,13 +3,22 @@ import { shallow } from 'enzyme'
 import PostCard from './PostCard'
 import samplePost from './samplePost'
 import faker from 'faker'
+import timezoneMock from 'timezone-mock'
 
 faker.seed(9000)
+
+beforeEach(() => {
+  timezoneMock.register('US/Pacific')
+})
+
+afterEach(() => {
+  timezoneMock.unregister()
+})
 
 it('renders as expected', () => {
   const post = {
     ...samplePost(),
-    updatedAt: new Date('2014-01-17').toString()
+    updatedAt: new Date('2014-01-17').toISOString()
   }
   const wrapper = shallow(<PostCard post={post} />)
   expect(wrapper).toMatchSnapshot()
