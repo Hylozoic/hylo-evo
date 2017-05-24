79a01a21a915860b9fdd91dac3acb426e52b3051
Robbie Carlton <robbie.carlton@gmail.com>
2017-05-24 14:48:30 -0600

    AllTopics layout

--- src/routes/AllTopics/AllTopics.connector.js
+++ src/routes/AllTopics/AllTopics.connector.js
@@ -1,10 +1,42 @@
 import { connect } from 'react-redux'
 // import { getMe } from 'store/selectors/getMe'
 
+const sampleTopics = [
+  {
+    name: 'announcements',
+    followersTotal: 671,
+    postsTotal: 2171,
+    subscribed: false
+  },
+  {
+    name: 'sustainable',
+    followersTotal: 582,
+    postsTotal: 1928,
+    subscribed: true
+  },
+  {
+    name: 'climate',
+    followersTotal: 671,
+    postsTotal: 2171,
+    subscribed: false
+  },
+  {
+    name: 'rally2017',
+    followersTotal: 671,
+    postsTotal: 2171,
+    subscribed: true
+  },
+  {
+    name: 'organics',
+    followersTotal: 671,
+    postsTotal: 2171,
+    subscribed: true
+  }
+]
+
 export function mapStateToProps (state, props) {
   return {
-    example: 'example'
-//  currentUser: getMe(state, props)
+    topics: sampleTopics
   }
 }
 
--- src/routes/AllTopics/AllTopics.js
+++ src/routes/AllTopics/AllTopics.js
@@ -1,17 +1,46 @@
 import React, { PropTypes, Component } from 'react'
 import './AllTopics.scss'
-const { string } = PropTypes
+const { array } = PropTypes
 import FullPageModal from 'routes/FullPageModal'
+import { pluralize } from 'util/index'
 
 export default class AllTopics extends Component {
   static propTypes = {
-    example: string
+    topics: array
   }
 
   render () {
+    const { topics } = this.props
+
     return <FullPageModal
-      content={<div styleName='exampleName'>
-        Topic List goes here.
+      content={<div styleName='all-topics'>
+        <div styleName='title'>Topics</div>
+        <div styleName='subtitle'>7 Total Topics</div>
+        <SearchBar />
+        {topics.map(topic => <TopicListItem topic={topic} />)}
       </div>} />
   }
+}
+
+export function SearchBar () {
+  return <div styleName='search-bar'>
+    <input styleName='search-input' type='text' placeholder='Search here' />
+    <select styleName='search-order'>
+      <option>Popular</option>
+      <option>Recent</option>
+    </select>
+  </div>
+}
+
+export function TopicListItem ({ topic, toggleSubscribe }) {
+  const { name, postsTotal, followersTotal, subscribed } = topic
+  return <div styleName='topic'>
+    <div styleName='topic-details'>
+      <div styleName='topic-name'>#{name}</div>
+      <div styleName='topic-stats'>{pluralize(postsTotal, 'post')} • {pluralize(followersTotal, 'follower')}</div>
+    </div>
+    <a onClick={toggleSubscribe} styleName='topic-subscribe'>
+      {subscribed ? 'Unsubscribe' : 'Subscribe'}
+    </a>
+  </div>
 }
