84c6cfdc249517b4d87b67feebd9d95f82144ab6
Lawrence Wang <l@lw.io>
2017-05-25 12:44:11 -0700

    block loading of everything in community routes until community is loaded
    this way, components that expect a community can be guaranteed that it
    will be present in the ORM. this ensures that we can always pass
    community ID’s to the API

--- src/routes/PrimaryLayout/PrimaryLayout.js
+++ src/routes/PrimaryLayout/PrimaryLayout.js
@@ -46,8 +46,13 @@ export default class PrimaryLayout extends Component {
       currentUser,
       isDrawerOpen,
       location,
-      toggleDrawer
+      toggleDrawer,
+      isCommunityRoute
     } = this.props
+
+    if (isCommunityRoute && !community) {
+      return <Loading type='fullscreen' />
+    }
 
     const closeDrawer = () => isDrawerOpen && toggleDrawer()
     const hasDetail = some(
