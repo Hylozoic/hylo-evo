31d5e52e7cdb8320e3712b1ed260582f33e8d4c1
Rich Churcher <rich.churcher@gmail.com>
2017-05-29 13:41:24 +1200

    Add some testing, refactor to facilitate it

--- src/routes/PrimaryLayout/PrimaryLayout.js
+++ src/routes/PrimaryLayout/PrimaryLayout.js
@@ -110,8 +110,12 @@ const detailRoutes = [
   {path: '/c/:slug/:topicName/p/:postId', component: PostDetail}
 ]
 
-function RedirectToCommunity ({ currentUser }) {
-  return <Route path='/' exact render={() => {
+export function RedirectToCommunity ({ currentUser }) {
+  return <Route path='/' exact render={redirectIfCommunity(currentUser)} />
+}
+
+export function redirectIfCommunity (currentUser) {
+  return () => {
     if (!currentUser || currentUser.memberships.count() === 0) return <Loading type='top' />
 
     const mostRecentCommunity = currentUser.memberships
@@ -120,5 +124,5 @@ function RedirectToCommunity ({ currentUser }) {
     .community
 
     return <Redirect to={`/c/${mostRecentCommunity.slug}`} />
-  }} />
+  }
 }
