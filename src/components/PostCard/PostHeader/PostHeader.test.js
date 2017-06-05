83d31485765319ff82cef64108fbb5f91d6df3ed
Robbie Carlton <robbie.carlton@gmail.com>
2017-06-05 16:04:38 -0600

    add PostHeader test

--- src/components/PostCard/PostHeader/PostHeader.test.js
+++ src/components/PostCard/PostHeader/PostHeader.test.js
@@ -2,6 +2,30 @@ import PostHeader from './PostHeader'
 import { shallow } from 'enzyme'
 import React from 'react'
 
-it('renders correctly', () => {
-  
+it('matches snapshot', () => {
+  const creator = {
+    name: 'JJ',
+    avatarUrl: 'foo.png',
+    id: 123
+  }
+  const context = {
+    label: 'some context',
+    url: '/foo/bar'
+  }
+
+  const communities = [
+    {
+      name: 'FooC', slug: 'fooc'
+    },
+    {
+      name: 'BarC', slug: 'barc'
+    }
+  ]
+
+  const wrapper = shallow(<PostHeader communities={communities} creator={creator} />)
+  expect(wrapper).toMatchSnapshot()
+  wrapper.setProps({context, type: 'request', communities, showCommunity: true})
+  expect(wrapper).toMatchSnapshot()
+  wrapper.setProps({deletePost: () => {}, editPost: () => {}})
+  expect(wrapper).toMatchSnapshot()
 })
