c9d1a3b28c980ee74eed47126064b67ba3bdc99e
Lawrence Wang <l@lw.io>
2017-06-06 17:15:31 -0700

    highlight topic in left nav when on its feed

--- src/routes/PrimaryLayout/components/Navigation/TopicNavigation/TopicNavigation.js
+++ src/routes/PrimaryLayout/components/Navigation/TopicNavigation/TopicNavigation.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 const { array, func, object } = React.PropTypes
 import Icon from 'components/Icon'
 import Badge from 'components/Badge'
-import { Link } from 'react-router-dom'
+import { Link, NavLink } from 'react-router-dom'
 import { tagUrl, topicsUrl } from 'util/index'
 import s from './TopicNavigation.scss' // eslint-disable-line no-unused-vars
 import badgeHoverStyles from '../../../../../components/Badge/component.scss'
@@ -26,13 +26,14 @@ export default class TopicNavigation extends Component {
       <ul styleName='s.topics'>
         {communityTopics.map(({ id, topic, newPostCount }) =>
           <li key={topic.name}>
-            <Link styleName='s.topic' className={badgeHoverStyles.parent}
+            <NavLink styleName='s.topic' className={badgeHoverStyles.parent}
               to={tagUrl(topic.name, slug)}
-              onClick={() => id && newPostCount > 0 && clearBadge(id)}>
+              onClick={() => id && newPostCount > 0 && clearBadge(id)}
+              activeClassName='active-topic-nav-link'>
               <span styleName='s.name'>#{topic.name}</span>
               {newPostCount > 0 &&
                 <Badge number={newPostCount} styleName='s.badge' />}
-            </Link>
+            </NavLink>
           </li>)}
       </ul>
       {slug && <div styleName='s.addTopic'>
--- src/routes/PrimaryLayout/components/Navigation/TopicNavigation/TopicNavigation.test.js
+++ src/routes/PrimaryLayout/components/Navigation/TopicNavigation/TopicNavigation.test.js
@@ -2,23 +2,20 @@ import React from 'react'
 import TopicNavigation from './TopicNavigation'
 import { shallow } from 'enzyme'
 
-it('renders correctly for a community', () => {
-  const communityTopics = [
-    {topic: {name: 't1'}, newPostCount: 3},
-    {topic: {name: 't2'}, newPostCount: 0},
-    {topic: {name: 't3'}},
-    {topic: {name: 't4'}, newPostCount: 2}
-  ]
+const communityTopics = [
+  {topic: {name: 't1'}, newPostCount: 3},
+  {topic: {name: 't2'}, newPostCount: 0},
+  {topic: {name: 't3'}},
+  {topic: {name: 't4'}, newPostCount: 2}
+]
 
-  const wrapper = shallow(<TopicNavigation
-    communityTopics={communityTopics}
+it('renders correctly for a community', () => {
+  const wrapper = shallow(<TopicNavigation communityTopics={communityTopics}
     community={{slug: 'foo'}} />)
-  expect(wrapper.find('li').length).toEqual(4)
-  expect(wrapper.find('Badge').length).toEqual(2)
-  expect(wrapper.find('li Link span').at(0).text()).toEqual(`#${communityTopics[0].topic.name}`)
-  expect(wrapper.find('li Link span').at(3).text()).toEqual(`#${communityTopics[3].topic.name}`)
+  expect(wrapper).toMatchSnapshot()
 })
 
 it('renders correctly for All Communities', () => {
-
+  const wrapper = shallow(<TopicNavigation communityTopics={communityTopics} />)
+  expect(wrapper).toMatchSnapshot()
 })
