716d5d21209792e180cf6da776843d5e7ef48cb2
Ray Marceau <razor2n4@gmail.com>
2017-06-14 13:18:45 -0700

    Opening notifications resets the badge https://trello.com/c/0Nzpxkfx/445-opening-the-notifications-dropdown-doesnt-remove-its-badge

--- src/routes/PrimaryLayout/components/TopNav/NotificationsDropdown/NotificationsDropdown.store.js
+++ src/routes/PrimaryLayout/components/TopNav/NotificationsDropdown/NotificationsDropdown.store.js
@@ -22,47 +22,45 @@ export function fetchNotifications () {
     type: FETCH_NOTIFICATIONS,
     graphql: {
       query: `{
-        me {
-          name
-          notifications (first: 10, order: "desc") {
-            total
-            hasMore
-            items {
+        notifications (first: 10, order: "desc", resetCount: true) {
+          total
+          hasMore
+          items {
+            id
+            createdAt
+            activity {
               id
-              createdAt
-              activity {
+              actor {
                 id
-                actor {
-                  id
-                  name
-                  avatarUrl
-                }
-                comment {
-                  id
-                  text
-                }
-                post {
-                  id
-                  title
-                }
-                community {
-                  id
-                  name
-                  slug
-                }
-                meta {
-                  reasons
-                }
-                action
-                unread
+                name
+                avatarUrl
               }
+              comment {
+                id
+                text
+              }
+              post {
+                id
+                title
+              }
+              community {
+                id
+                name
+                slug
+              }
+              meta {
+                reasons
+              }
+              action
+              unread
             }
           }
         }
       }`
     },
     meta: {
-      extractModel: 'Me'
+      extractModel: 'Notification',
+      resetCount: true
     }
   }
 }
