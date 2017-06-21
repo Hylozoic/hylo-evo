84b46ee268ce25063a487e743f9db4c4714331da
Ray Marceau <razor2n4@gmail.com>
2017-06-21 15:48:29 -0700

    adds test and alphabetize store constants

---
+++ src/store/index.test.js
@@ -0,0 +1,32 @@
+import createStore from "store"
+import { LOGOUT } from 'store/constants'
+
+describe('store', () => {
+  let store = createStore(null)
+
+  it ('resets to initial state on logout', () => {
+    let initialState = store.getState();
+    const action = {
+      type: 'FOO',
+      payload: {
+        data: {
+          post: {
+            id: '1',
+            title: 'Cat on the loose',
+          }
+        }
+      },
+      meta: {
+        extractModel: 'Post'
+      }
+    }
+
+    store.dispatch(action)
+
+    expect(store.getState()).not.toEqual(initialState)
+
+    store.dispatch({type: LOGOUT})
+
+    expect(store.getState()).toEqual(initialState)
+  })
+})
