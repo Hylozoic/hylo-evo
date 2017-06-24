9d33d458a152ef3fb4c856fc47d491dddcd801b5
Ray Marceau <razor2n4@gmail.com>
2017-06-24 16:38:26 -0700

    update test

--- src/store/index.test.js
+++ src/store/index.test.js
@@ -4,7 +4,7 @@ import { LOGOUT } from 'store/constants'
 describe('store', () => {
   it('resets to initial state on logout', () => {
     const fullInitialState = rootReducer(initialState, {})
-    const state = {type: 'FOO'}
+    const state = {foo: 'bar'}
     const action = {type: LOGOUT}
     const newState = rootReducer(state, action)
     expect(newState).toEqual(fullInitialState)
