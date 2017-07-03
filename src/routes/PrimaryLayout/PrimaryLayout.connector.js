7a8e5cdb26afef7f36bf783f22915735d7f87cf3
Robbie Carlton <robbie.carlton@gmail.com>
2017-07-03 14:10:41 -0600

    get showLogoBadge from memberships

--- src/routes/PrimaryLayout/PrimaryLayout.connector.js
+++ src/routes/PrimaryLayout/PrimaryLayout.connector.js
@@ -4,15 +4,22 @@ import {
 } from './PrimaryLayout.store'
 import getMe from 'store/selectors/getMe'
 import getCommunityForCurrentRoute from 'store/selectors/getCommunityForCurrentRoute'
+import getMemberships from 'store/selectors/getMemberships'
 import isCommunityRoute, { getSlugFromLocation } from 'store/selectors/isCommunityRoute'
+import { some } from 'lodash/fp'
 
 function mapStateToProps (state, props) {
+  const memberships = getMemberships(state, props)
+  const showLogoBadge = some(m => m.newPostCount > 0, memberships)
+
+  console.log({showLogoBadge})
+
   return {
     isCommunityRoute: isCommunityRoute(state, props),
     community: getCommunityForCurrentRoute(state, props),
     currentUser: getMe(state),
     isDrawerOpen: state.PrimaryLayout.isDrawerOpen,
-    showLogoBadge: true
+    showLogoBadge
   }
 }
 
--- src/routes/PrimaryLayout/components/Drawer/Drawer.connector.js
+++ src/routes/PrimaryLayout/components/Drawer/Drawer.connector.js
@@ -1,15 +1,6 @@
 import { connect } from 'react-redux'
-import { createSelector as ormCreateSelector } from 'redux-orm'
-import orm from 'store/models'
 import { toggleDrawer } from 'routes/PrimaryLayout/PrimaryLayout.store'
-
-export const getMemberships = ormCreateSelector(
-  orm,
-  state => state.orm,
-  session => {
-    return session.Membership.all().toModelArray()
-  }
-)
+import getMemberships from 'store/selectors/getMemberships'
 
 export function mapStateToProps (state, props) {
   return {
