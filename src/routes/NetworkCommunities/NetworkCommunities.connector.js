fdf3ee49a354b45fe03e662396906062906639ee
Robbie Carlton <robbie.carlton@gmail.com>
2017-07-14 15:50:09 -0700

    NetworkCommunities styles

--- src/routes/NetworkCommunities/NetworkCommunities.connector.js
+++ src/routes/NetworkCommunities/NetworkCommunities.connector.js
@@ -9,9 +9,37 @@ const network = {
   name: 'Lawyers For Good Government'
 }
 
+const communities = [
+  {
+    id: 1,
+    name: 'Climate Change',
+    memberCount: 4681,
+    avatarUrl: 'https://d3ngex8q79bk55.cloudfront.net/community/1944/avatar/1489438401225_face.png'
+  },
+  {
+    id: 2,
+    name: 'Save the Bees',
+    memberCount: 4681,
+    avatarUrl: 'https://d3ngex8q79bk55.cloudfront.net/community/1944/avatar/1489438401225_face.png'
+  },
+  {
+    id: 3,
+    name: 'Stop Big Oil',
+    memberCount: 4681,
+    avatarUrl: 'https://d3ngex8q79bk55.cloudfront.net/community/1944/avatar/1489438401225_face.png'
+  },
+  {
+    id: 4,
+    name: "Women's March",
+    memberCount: 4681,
+    avatarUrl: 'https://d3ngex8q79bk55.cloudfront.net/community/1944/avatar/1489438401225_face.png'
+  }
+]
+
 export function mapStateToProps (state, props) {
   return {
-    network
+    network,
+    communities
 //  currentUser: getMe(state, props)
   }
 }
--- src/routes/NetworkCommunities/NetworkCommunities.js
+++ src/routes/NetworkCommunities/NetworkCommunities.js
@@ -1,12 +1,16 @@
 import React, { PropTypes, Component } from 'react'
 import './NetworkCommunities.scss'
 const { string, array, func, object } = PropTypes
+import Dropdown from 'components/Dropdown'
 import Icon from 'components/Icon'
+import TextInput from 'components/TextInput'
+import RoundImage from 'components/RoundImage'
+import { find } from 'lodash/fp'
 
-const sortOptions = [ // eslint-disable-line
+const sortOptions = [
+  {id: 'updated_at', label: 'Newest'},
   {id: 'name', label: 'Alphabetical'},
-  {id: 'member_count', label: 'Popular'},
-  {id: 'updated_at', label: 'Newest'}
+  {id: 'member_count', label: 'Popular'}
 ]
 
 export default class NetworkCommunities extends Component {
@@ -37,20 +41,47 @@ export function Banner ({ network }) {
   return <div styleName='banner'>
     <div styleName='banner-text'>
       <div styleName='name'>Communities</div>
-      <div styleName='stats'>436 Communities • 27,106 Total Members</div>
+      <div styleName='stats'>436 Communities&nbsp;&nbsp;•&nbsp;&nbsp;27,106 Total Members</div>
     </div>
     <Icon name='More' styleName='icon' />
   </div>
 }
 
 export function SearchBar ({ search, setSearch, sortOption, setSort }) {
-  return <div />
+  var selected = find(o => o.id === sortOption, sortOptions)
+
+  if (!selected) selected = sortOptions[0]
+
+  return <div styleName='search-bar'>
+    <TextInput styleName='search-input'
+      value={search}
+      placeholder='Search by name'
+      onChange={event => setSearch(event.target.value)} />
+    <Dropdown styleName='search-order'
+      toggleChildren={<span styleName='search-sorter-label'>
+        {selected.label}
+        <Icon name='ArrowDown' />
+      </span>}
+      items={sortOptions.map(({ id, label }) => ({
+        label,
+        onClick: () => setSort(id)
+      }))}
+      alignRight />
+  </div>
 }
 
 export function CommunityList ({ communities, fetchMoreCommunities }) {
-  return <div />
+  return <div styleName='community-list'>
+    {communities.map(c => <CommunityCard community={c} />)}
+  </div>
 }
 
 export function CommunityCard ({ community }) {
-
+  return <div styleName='community-card'>
+    <RoundImage url={community.avatarUrl} styleName='community-image' size='50' square />
+    <div styleName='community-details'>
+      <span styleName='community-name'>{community.name}</span>
+      <span styleName='community-stats'>{community.memberCount} Members</span>
+    </div>
+  </div>
 }
