5c601540391846d2d35e7a83b5a158eee811138a
Robbie Carlton <robbie.carlton@gmail.com>
2017-08-07 17:35:47 -0600

    add test

--- src/routes/CommunitySettings/InviteSettingsTab/InviteSettingsTab.test.js
+++ src/routes/CommunitySettings/InviteSettingsTab/InviteSettingsTab.test.js
@@ -2,7 +2,16 @@ import InviteSettingsTab from './InviteSettingsTab'
 import { shallow } from 'enzyme'
 import React from 'react'
 
-it('does something', () => {
-  const wrapper = shallow(<InviteSettingsTab />)
+it('renders correctly', () => {
+  const community = {
+    id: 1,
+    name: 'Hylo'
+  }
+
+  const wrapper = shallow(<InviteSettingsTab
+    community={community}
+    regenerateAccessCode={() => {}}
+    inviteLink='http://www.hylo.com/c/hylo/join/lalala'
+    />)
   expect(wrapper).toMatchSnapshot()
 })
