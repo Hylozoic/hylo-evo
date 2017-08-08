6ac051c1dda72251e6d855237c2dcef921406552
Robbie Carlton <robbie.carlton@gmail.com>
2017-08-08 13:12:26 -0600

    fancy buttons

--- src/routes/CommunitySettings/InviteSettingsTab/InviteSettingsTab.js
+++ src/routes/CommunitySettings/InviteSettingsTab/InviteSettingsTab.js
@@ -1,49 +1,77 @@
-import React from 'react'
+import React, { Component } from 'react'
 import './InviteSettingsTab.scss'
 import Button from 'components/Button'
 import CopyToClipboard from 'react-copy-to-clipboard'
+const { object, func, string } = React.PropTypes
+import { isEmpty } from 'lodash/fp'
 
-const { object } = React.PropTypes
+export default class InviteSettingsTab extends Component {
+  static propTypes = {
+    community: object,
+    regenerateAccessCode: func,
+    inviteLink: string
+  }
 
-export default function InviteSettingsTab ({ community, regenerateAccessCode, inviteLink }) {
-  const { name } = community
-
-  const onReset = () => {
-    if (window.confirm("Are you sure you want to create a new join link? The current link won't work anymore if you do.")) {
-      regenerateAccessCode()
+  constructor (props) {
+    super(props)
+    this.state = {
+      copied: false,
+      reset: false
     }
   }
-  const onCopy = () => console.log('copied')
 
-  return <div>
-    <div styleName='header'>
-      <div styleName='title'>Invite People</div>
-      <div styleName='subtitle'>
-        to {name} on Hylo
+  setTemporatyState (key, value) {
+    console.log('sTs', key, value)
+    const oldValue = this.state[key]
+    console.log('this', this)
+    this.setState({[key]: value})
+    setTimeout(() => {
+      this.setState({[key]: oldValue})
+    }, 3000)
+  }
+
+  render () {
+    const { community, regenerateAccessCode, inviteLink } = this.props
+    const { name } = community
+    const { copied, reset } = this.state
+
+    const onReset = () => {
+      if (window.confirm("Are you sure you want to create a new join link? The current link won't work anymore if you do.")) {
+        regenerateAccessCode()
+        this.setTemporatyState('reset', true)
+      }
+    }
+    const onCopy = () => this.setTemporatyState('copied', true)
+
+    const buttonColor = highlight => highlight ? 'green' : 'green-white-green-border'
+
+    return <div>
+      <div styleName='header'>
+        <div styleName='title'>Invite People</div>
+        <div styleName='subtitle'>
+          to {name} on Hylo
+        </div>
       </div>
-    </div>
-    <div styleName='invite-link-settings'>
-      <div styleName='invite-link-text'>
-        <div styleName='help'>Anyone with this link can join the community</div>
-        <div styleName='invite-link'>{inviteLink}</div>
-      </div>
-      <div styleName='buttons'>
-        <Button onClick={onReset}
-          color='green-white-green-border'
-          styleName='reset-button'
-          narrow
-          small>
-          Reset Link
-        </Button>
-        <CopyToClipboard text={inviteLink} onCopy={onCopy}>
-          <Button color='green-white-green-border' styleName='button' narrow small>
-            Copy Link
+      <div styleName='invite-link-settings'>
+        <div styleName='invite-link-text'>
+          <div styleName='help'>Anyone with this link can join the community</div>
+          <div styleName='invite-link'>{inviteLink}</div>
+        </div>
+        <div styleName='buttons'>
+          <Button onClick={onReset}
+            color={buttonColor(reset)}
+            styleName='reset-button'
+            narrow
+            small>
+            {reset ? 'Reset' : 'Reset Link'}
           </Button>
-        </CopyToClipboard>
+          <CopyToClipboard text={inviteLink} onCopy={onCopy}>
+            <Button color={buttonColor(copied)} styleName='button' narrow small>
+              {copied ? 'Copied' : 'Copy Link'}
+            </Button>
+          </CopyToClipboard>
+        </div>
       </div>
     </div>
-  </div>
-}
-InviteSettingsTab.propTypes = {
-  community: object
+  }
 }
