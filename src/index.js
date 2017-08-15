af50cdba08cdd02f9f9a00db1c2c5d27b1984129
Loren Johnson <loren@hellovenado.com>
2017-08-15 05:22:29 -0700

    Move mobile redirect into src and employ isMobile library

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
+import mobileRedirect from 'util/mobileRedirect'
 import { clientRouter } from './router'
 import createHistory from 'history/createBrowserHistory'
 import createStore from './store'
@@ -9,7 +10,7 @@ import './client/websockets'
 const history = createHistory()
 const store = createStore(history)
 
-ReactDOM.render(
+mobileRedirect() || ReactDOM.render(
   <Provider store={store}>{ clientRouter(history) }</Provider>,
   document.getElementById('root')
 )
