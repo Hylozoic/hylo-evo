c641b5eb60b1bd902bcbfc514b4dd36c59b217ee
Loren Johnson <loren@hellovenado.com>
2017-08-15 15:55:50 -0700

    Redirect to App Store or Google Play entry for Hylo App for mobile phones - WEB-76

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
+import mobileRedirect from 'util/mobileRedirect'
 import { clientRouter } from './router'
 import createHistory from 'history/createBrowserHistory'
 import createStore from './store'
@@ -9,7 +10,11 @@ import './client/websockets'
 const history = createHistory()
 const store = createStore(history)
 
-ReactDOM.render(
-  <Provider store={store}>{ clientRouter(history) }</Provider>,
-  document.getElementById('root')
-)
+const redirecting = process.env.REDIRECT_TO_APP_STORE && mobileRedirect()
+
+if (!redirecting) {
+  ReactDOM.render(
+    <Provider store={store}>{ clientRouter(history) }</Provider>,
+    document.getElementById('root')
+  )
+}
