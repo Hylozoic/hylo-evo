4fb746ef0269f99369631cda3ea2f45823acdfdd
Andy Gimma <andy@hylo.com>
2017-08-17 17:37:18 -0500

    Creating layouts

--- src/components/Signup/SignupCreateCommunity/index.js
+++ src/components/Signup/SignupCreateCommunity/index.js
@@ -1,25 +1,36 @@
 import React, { Component } from 'react'
 import ReactCSSTransitionGroup from 'react-addons-css-transition-group'
-import PostEditor from 'components/PostEditor'
 import '../Signup.scss'
 
 export default class SignupCreateCommunity extends Component {
   render () {
-    const { match, hidePostEditor } = this.props
     return <ReactCSSTransitionGroup
       transitionName='post-editor'
       transitionAppear
       transitionAppearTimeout={400}
       transitionEnterTimeout={400}
       transitionLeaveTimeout={300}>
-      {match && <div
-        styleName='post-editor-modal'
-        key='post-editor-modal'>
-        <div styleName='post-editor-background' className='post-editor-background' />
-        <div styleName='post-editor-wrapper' className='post-editor-wrapper'>
-          <PostEditor onClose={hidePostEditor} {...this.props} />
+      <div
+        styleName='signup-modal'
+        key='signup-modal'>
+        <div styleName='signup-background' className='signup-background' />
+        <div styleName='signup-wrapper' className='signup-wrapper'>
+          <TestComp />
         </div>
-      </div>}
+      </div>
     </ReactCSSTransitionGroup>
   }
+}
+
+export function TestComp () {
+  return <div styleName='wrapper'>
+    <div styleName='left-sidebar'>
+      <p>First</p>
+      <p>Second</p>
+      <p>Third</p>
+      <p>Fourth</p>
+      <p>Fifth</p>
+    </div>
+    <div styleName='center'>right</div>
+  </div>
 }
--- src/routes/Signup/SignupCreateCommunity/SignupCreateCommunity.js
+++ src/routes/Signup/SignupCreateCommunity/SignupCreateCommunity.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import ReactCSSTransitionGroup from 'react-addons-css-transition-group'
 import PostEditor from 'components/PostEditor'
-import './SignupModal.scss'
+import './PostEditorModal.scss'
 
 export default class SignupCreateCommunity extends Component {
   render () {
@@ -12,14 +12,14 @@ export default class SignupCreateCommunity extends Component {
       transitionAppearTimeout={400}
       transitionEnterTimeout={400}
       transitionLeaveTimeout={300}>
-      {match && <div
+      <div
         styleName='post-editor-modal'
         key='post-editor-modal'>
         <div styleName='post-editor-background' className='post-editor-background' />
         <div styleName='post-editor-wrapper' className='post-editor-wrapper'>
-          <PostEditor onClose={hidePostEditor} {...this.props} />
+          <h1>Popup</h1>
         </div>
-      </div>}
+      </div>
     </ReactCSSTransitionGroup>
   }
 }
