fce9986c965a327d52da9fcd604b53c012a8ae37
Loren Johnson <loren@hellovenado.com>
2017-08-22 16:57:14 -0700

    Signup should login user

--- src/routes/Signup/Signup.store.js
+++ src/routes/Signup/Signup.store.js
@@ -4,7 +4,16 @@ export function signup (fullName, email, password) {
   return {
     type: SIGNUP,
     payload: {
-      api: {method: 'post', path: '/noo/user', params: {fullName, email, password}}
+      api: {
+        method: 'post',
+        path: '/noo/user',
+        params: {
+          fullName,
+          email,
+          password,
+          login: true
+        }
+      }
     }
   }
 }
