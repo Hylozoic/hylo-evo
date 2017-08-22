4c42cafe4f3bf7e9ca92461361404f75bf543dae
Andy Gimma <andy@hylo.com>
2017-08-22 10:45:32 -0500

    Separate components

---
+++ src/components/Signup/CreateCommunity.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react'
+
+import { Pill } from 'components/Pillbox/Pillbox'
+import { hyloNameWhiteBackground } from 'util/assets'
+import { bgImageStyle } from 'util/index'
+import './Signup.scss'
+
+export default class CreateCommunity extends Component {
+  handleCommunityNameChange = (event) => {
+    const communityName = event.target.value
+    this.setState({
+      communityName
+    })
+  }
+  render () {
+    const avatarUrl = hyloNameWhiteBackground
+    return <div styleName='wrapper'>
+      <div styleName='sidebar'>
+        <p styleName='gray-text'>Close</p>
+        <p styleName='sidebar-header'>Great, let's get started</p>
+        <p styleName='gray-text'>All good things start somewhere! Let's kick things off with a catchy name for your community.</p>
+      </div>
+      <div styleName='detail'>
+        <span styleName='white-text float-right'>STEP 1/4</span>
+        <br />
+        <div styleName='center'>
+          <div styleName='logo center' style={bgImageStyle(avatarUrl)} />
+        </div>
+        <div styleName='center center-vertical'>
+          <input
+            styleName='my-input'
+            onChange={this.handleCommunityNameChange}
+            autoFocus
+          />
+        </div>
+        <div>
+          <div styleName='float-right bottom'>
+            <div>
+              <Pill label='Continue' styleName='my-button' />
+            </div>
+            <div styleName='or-press-enter'>Or Press Enter</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  }
+}
--- src/components/Signup/SignupCreateCommunity/index.js
+++ src/components/Signup/SignupCreateCommunity/index.js
@@ -1,59 +1,22 @@
-import React, { Component } from 'react'
+import React from 'react'
 import ReactCSSTransitionGroup from 'react-addons-css-transition-group'
 
-import { Pill } from 'components/Pillbox/Pillbox'
-import { hyloNameWhiteBackground } from 'util/assets'
-import { bgImageStyle } from 'util/index'
+import CreateCommunity from '../CreateCommunity'
 import '../Signup.scss'
-
-export default class SignupCreateCommunity extends Component {
-  render () {
-    return <ReactCSSTransitionGroup
-      transitionName='post-editor'
-      transitionAppear
-      transitionAppearTimeout={400}
-      transitionEnterTimeout={400}
-      transitionLeaveTimeout={300}>
-      <div
-        styleName='signup-modal'
-        key='signup-modal'>
-        <div styleName='signup-background' className='signup-background' />
-        <div styleName='signup-wrapper' className='signup-wrapper'>
-          <CreateCommunity />
-        </div>
-      </div>
-    </ReactCSSTransitionGroup>
-  }
-}
-
-export function CreateCommunity () {
-  const avatarUrl = hyloNameWhiteBackground
-  return <div styleName='wrapper'>
-    <div styleName='sidebar'>
-      <p styleName='gray-text'>Close</p>
-      <p styleName='sidebar-header'>Great, let's get started</p>
-      <p styleName='gray-text'>All good things start somewhere! Let's kick things off with a catchy name for your community.</p>
-    </div>
-    <div styleName='detail'>
-      <span styleName='white-text float-right'>STEP 1/4</span>
-      <br />
-      <div styleName='center'>
-        <div styleName='logo center' style={bgImageStyle(avatarUrl)} />
-      </div>
-      <div styleName='center center-vertical'>
-        <input
-          styleName='my-input'
-          autoFocus
-        />
-      </div>
-      <div>
-        <div styleName='float-right bottom'>
-          <div>
-            <Pill label='Continue' styleName='my-button' />
-          </div>
-          <div styleName='or-press-enter'>Or Press Enter</div>
-        </div>
+export default function SignupCreateCommunity () {
+  return <ReactCSSTransitionGroup
+    transitionName='post-editor'
+    transitionAppear
+    transitionAppearTimeout={400}
+    transitionEnterTimeout={400}
+    transitionLeaveTimeout={300}>
+    <div
+      styleName='signup-modal'
+      key='signup-modal'>
+      <div styleName='signup-background' className='signup-background' />
+      <div styleName='signup-wrapper' className='signup-wrapper'>
+        <CreateCommunity />
       </div>
     </div>
-  </div>
+  </ReactCSSTransitionGroup>
 }
