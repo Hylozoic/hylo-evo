739c31b643b9b7226fe9fa2b4e7d854f3886a0cd
Andy Gimma <andy@hylo.com>
2017-08-29 13:16:16 -0500

    Fix tests

--- src/routes/Signup/LeftSidebar/LeftSidebar.connector.test.js
+++ src/routes/Signup/LeftSidebar/LeftSidebar.connector.test.js
@@ -3,6 +3,6 @@ import { mapDispatchToProps } from './LeftSidebar.connector'
 describe('mapDispatchToProps', () => {
   it('updateUserSettings should match latest snapshot', () => {
     const changes = {settings: {'signupInProgress': false}}
-    expect(mapDispatchToProps.updateUserSettings(changes).toMatchSnapshot())
+    expect(mapDispatchToProps.updateUserSettings(changes)).toMatchSnapshot()
   })
 })
