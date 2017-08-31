ed6835197fa86697bbc0ceab039420ca709130d6
Andy Gimma <andy@hylo.com>
2017-08-31 12:06:01 -0500

    Remove unused variable

--- src/routes/Signup/LeftSidebar/LeftSidebar.connector.test.js
+++ src/routes/Signup/LeftSidebar/LeftSidebar.connector.test.js
@@ -2,7 +2,6 @@ import { mapDispatchToProps } from './LeftSidebar.connector'
 
 describe('mapDispatchToProps', () => {
   it('should match latest snapshot for updateUserSettings', () => {
-    const changes = {settings: {'signupInProgress': false}}
     expect(mapDispatchToProps).toMatchSnapshot()
   })
 })
