3d304e9bf4a05c2581754e43c32a198c1d01b666
Andy Gimma <andy@hylo.com>
2017-09-04 21:59:13 -0500

    Footer button positioning

--- src/routes/Signup/SignupModalFooter/index.js
+++ src/routes/Signup/SignupModalFooter/index.js
@@ -2,10 +2,12 @@ import React from 'react'
 import '../Signup.scss'
 
 export default function SignupModalFooter ({previous, submit, continueText, showPrevious = true}) {
-  return <div styleName='signup-modal-footer'>
-    <div styleName='footer-buttons'>
-      {showPrevious && <span styleName='previous-button' onClick={previous}>Previous</span>}
-      <span styleName='continue-button' onClick={submit}>{continueText}</span>
+  return <div>
+    <div styleName='signup-modal-footer'>
+      <div styleName='footer-buttons'>
+        {showPrevious && <span styleName='previous-button' onClick={previous}>Previous</span>}
+        <span styleName='continue-button' onClick={submit}>{continueText}</span>
+      </div>
     </div>
     <div styleName='press-enter text-opacity'>or press Enter</div>
   </div>
