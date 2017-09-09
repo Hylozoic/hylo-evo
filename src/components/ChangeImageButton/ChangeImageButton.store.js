28a7bedbbfe04ad93a93f7200834ad802964258f
Lawrence Wang <l@lw.io>
2017-09-08 17:14:00 -0700

    DEV-57: use new file upload backend (#184)
    * first cut at wiring up ChangeImageButton to new uploader backend
    * wire up community and network images to new uploader
    * update ChangeImageButton in signup

--- src/components/ChangeImageButton/ChangeImageButton.store.js
+++ src/components/ChangeImageButton/ChangeImageButton.store.js
@@ -1,13 +1,28 @@
-import { upload } from 'client/filepicker'
-import {
-  UPLOAD_IMAGE
-} from 'store/constants'
+import { pick } from 'client/filepicker'
+import { UPLOAD_IMAGE } from 'store/constants'
 
 export function uploadImage (opts) {
-  let { subject, id, path, convert } = opts
+  let { type, id } = opts
   let payload = new Promise((resolve, reject) => {
-    upload({path, convert, success: resolve, failure: reject})
+    pick({
+      success: url => resolve({
+        api: {
+          method: 'post',
+          path: '/noo/upload',
+          params: {url, id, type}
+        }
+      }),
+      failure: err => {
+        // code 101 = user canceled the filepicker UI.
+        // we have to resolve or reject so that we don't stay in pending state,
+        // but we also don't want to create an action with an error, since there
+        // wasn't any real error, so we just resolve with an empty payload.
+        if (err.code === 101) return resolve({})
+
+        reject(err)
+      }
+    })
   })
 
-  return {type: UPLOAD_IMAGE, payload, meta: {subject, id}}
+  return {type: UPLOAD_IMAGE, payload, meta: {type, id}}
 }
