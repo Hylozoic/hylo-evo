29c02a5a71b0b6354b5aa1126617bd004ec4efb5
Lawrence Wang <l@lw.io>
2017-09-12 15:38:19 -0700

    fix display of long links in topic nav
    flexbox and text-overflow: ellipsis, together at last

--- src/components/Icon/component.js
+++ src/components/Icon/component.js
@@ -2,9 +2,9 @@ import React from 'react'
 import cx from 'classnames'
 import './component.scss'
 
-export default function Icon ({ name, className, green, children }) {
+export default function Icon ({ name, className, green, children, onClick }) {
   const iconClassName = `icon-${name}`
-  return <span styleName={cx('icon', {green})}
+  return <span styleName={cx('icon', {green})} onClick={onClick}
     className={cx(iconClassName, className)}>
     {children}
   </span>
