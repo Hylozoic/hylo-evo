21884a6572943ba4c5932c7420b946787aea0b86
Lawrence Wang <l@lw.io>
2017-09-12 18:56:00 -0700

    limit tagline length

--- src/components/SettingsControl/SettingsControl.js
+++ src/components/SettingsControl/SettingsControl.js
@@ -2,18 +2,31 @@ import React from 'react'
 import TextareaAutosize from 'react-textarea-autosize'
 import './SettingsControl.scss'
 
-export default function SettingsControl ({ label, value = '', onChange, type }) {
+export default function SettingsControl ({ label, value = '', onChange, type, ...otherProps }) {
   var control
 
   switch (type) {
     case 'textarea':
-      control = <TextareaAutosize minRows={1} maxRows={3} styleName='control-input' value={value} onChange={onChange} />
+      control = <TextareaAutosize minRows={1}
+        maxRows={3}
+        styleName='control-input'
+        value={value}
+        onChange={onChange}
+        {...otherProps} />
       break
     case 'password':
-      control = <input styleName='control-input' type='password' value={value} onChange={onChange} />
+      control = <input styleName='control-input'
+        type='password'
+        value={value}
+        onChange={onChange}
+        {...otherProps} />
       break
     default:
-      control = <input styleName='control-input' type='text' value={value} onChange={onChange} />
+      control = <input styleName='control-input'
+        type='text'
+        value={value}
+        onChange={onChange}
+        {...otherProps} />
       break
   }
 
