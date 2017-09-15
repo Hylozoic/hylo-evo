f1baac0ed508580ff021654c1189d85710d3150d
Robbie Carlton <robbie.carlton@gmail.com>
2017-09-15 15:54:39 -0600

    Only show link preview image when there's an image

--- src/components/PostCard/LinkPreview/index.js
+++ src/components/PostCard/LinkPreview/index.js
@@ -1,14 +1,15 @@
 import { bgImageStyle } from 'util/index'
 import { parse } from 'url'
 import React from 'react'
+import cx from 'classnames'
 import './LinkPreview.scss'
 
 export default function LinkPreview ({ title, url, imageUrl }) {
   const domain = url && parse(url).hostname.replace('www.', '')
   return <div styleName='wrapper'>
-    <div styleName='linkPreview'>
+    <div styleName={cx('linkPreview', {noImage: !imageUrl})}>
       <a href={url} target='_blank'>
-        <div style={bgImageStyle(imageUrl)} styleName='previewImage' />
+        {imageUrl && <div style={bgImageStyle(imageUrl)} styleName='previewImage' />}
         <div styleName='previewText'>
           <span styleName='previewTitle'>{title}</span>
           <div styleName='previewDomain'>{domain}</div>
