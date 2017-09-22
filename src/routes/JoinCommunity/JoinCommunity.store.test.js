a273525e5cb1fa4324df037481ec374da454d666
Loren Johnson <loren@hellovenado.com>
2017-09-21 18:24:44 -0700

    Remove unused import

--- src/routes/JoinCommunity/JoinCommunity.store.test.js
+++ src/routes/JoinCommunity/JoinCommunity.store.test.js
@@ -4,8 +4,7 @@ import {
   useInvitation,
   getNewMembership,
   getValidInvite,
-  defaultState,
-  reducer
+  defaultState
 } from './JoinCommunity.store'
 
 describe('JoinCommunity.store', () => {
