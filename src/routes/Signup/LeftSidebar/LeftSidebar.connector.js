204528814550e514ef073682f48c0c2627449352
Loren Johnson <loren@hellovenado.com>
2017-10-16 18:10:45 -0700

    Properly redirects to returnToURL on cancellation of Signup process

--- src/routes/Signup/LeftSidebar/LeftSidebar.connector.js
+++ src/routes/Signup/LeftSidebar/LeftSidebar.connector.js
@@ -2,18 +2,36 @@ import { connect } from 'react-redux'
 import { push } from 'react-router-redux'
 import getMe from 'store/selectors/getMe'
 import { updateUserSettings } from 'store/actions/updateUserSettings'
+import { getReturnToURL, resetReturnToURL } from 'router/AuthRoute/AuthRoute.store'
 
 export function mapStateToProps (state, props) {
   return {
-    currentUser: getMe(state)
+    currentUser: getMe(state),
+    returnToURL: getReturnToURL(state)
   }
 }
 
 export function mapDispatchToProps (dispatch, props) {
   return {
     updateUserSettings: (changes) => dispatch(updateUserSettings(changes)),
-    redirectHome: () => dispatch(push('/'))
+    resetReturnToURL: () => dispatch(resetReturnToURL()),
+    push: (path) => dispatch(push(path))
   }
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)
+export function mergeProps (stateProps, dispatchProps, ownProps) {
+  return {
+    ...stateProps,
+    ...dispatchProps,
+    ...ownProps,
+    handleCloseSignupModal: (defaultPath = '/') => {
+      const changes = {settings: {signupInProgress: false}}
+      dispatchProps.updateUserSettings(changes).then(() => {
+        dispatchProps.resetReturnToURL()
+        dispatchProps.push(stateProps.returnToURL || defaultPath)
+      })
+    }
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps, mergeProps)
