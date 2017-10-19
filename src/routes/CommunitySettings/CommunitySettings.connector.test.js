8d70843230813829104f007ce76264edb3f75854
Andy Gimma <andy@hylo.com>
2017-10-19 14:23:19 -0400

    Fix test

--- src/routes/CommunitySettings/CommunitySettings.connector.test.js
+++ src/routes/CommunitySettings/CommunitySettings.connector.test.js
@@ -1,10 +1,28 @@
 import { mapStateToProps, mapDispatchToProps, mergeProps } from './CommunitySettings.connector'
+import orm from 'store/models'
+
+let state
+beforeAll(() => {
+  const session = orm.session(orm.getEmptyState())
+  const community = session.Community.create({id: '99', slug: 'foo'})
+  session.Community.create({id: '88', slug: 'bar'})
+
+  session.Me.create({
+    id: '1',
+    memberships: [session.Membership.create({
+      id: '345',
+      community: community.id,
+      hasModeratorRole: true
+    })]
+  })
+
+  state = {
+    orm: session.state
+  }
+})
 
 describe('mapStateToProps', () => {
   it('returns the right keys', () => {
-    const state = {
-
-    }
     expect(mapStateToProps(state, {})).toMatchSnapshot()
   })
 })
