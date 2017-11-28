ab76a9b1f0dcd86a1b6f5625b4c04e2988554c63
Andy Gimma <andy@hylo.com>
2017-11-28 12:10:11 -0500

    Style topic support component

--- src/components/TopicSupportComingSoon/index.js
+++ src/components/TopicSupportComingSoon/index.js
@@ -1,14 +1,16 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import Button from 'components/Button'
+import { axolotlDigging } from 'util/assets'
+import './TopicSupportComingSoon.scss'
 
 export default function TopicSupportComingSoon () {
-  return <div>
-    <h1>Were working on expanding</h1>
-    <h2>#topics to more places</h2>
-    <p>In the meantime, click a topic from an individual community</p>
-    <p>to see posts from that community</p>
-    <Link to='/reset-password'>
-      <p>Forgot password?</p>
+  return <div styleName='container'>
+    <h1>Were working on expanding<br />#topics to more places</h1>
+    <p styleName='gray-text'>In the meantime, click a topic from an individual<br />community to see posts from that community</p>
+    <Link to='/all'>
+      <Button styleName='back-button'>Return to All Communities</Button>
     </Link>
+    <img styleName='axolotl-digging-image' src={axolotlDigging} />
   </div>
 }
