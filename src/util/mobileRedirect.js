251fb7696ca17adc5a2c3c42f054a7ea27ab1642
Andy Gimma <andy@hylo.com>
2017-12-05 15:56:52 -0500

    Update mobileRedirect

---
+++ src/util/mobileRedirect.js
@@ -0,0 +1,21 @@
+import isMobile from 'ismobilejs'
+
+export const APP_STORE_APP_URL = 'https://appsto.re/us/0gcV7.i'
+export const GOOGLE_PLAY_APP_URL = 'https://play.google.com/store/apps/details?id=com.hylo.hyloandroid'
+
+export default function mobileRedirect () {
+  const forceApp = (
+    isMobile.apple.phone ||
+    isMobile.apple.ipod ||
+    isMobile.android.phone ||
+    isMobile.seven_inch
+  )
+
+  if (forceApp) {
+    if (isMobile.apple.device) {
+      return APP_STORE_APP_URL
+    } else if (isMobile.android.device) {
+      return GOOGLE_PLAY_APP_URL
+    }
+  }
+}
