6c05194627a15946cc56419b078c0dcc79782d0d
Andy Gimma <andy@hylo.com>
2017-12-18 11:11:56 -0500

    Style DownloadAppModal

--- src/components/DownloadAppModal/DownloadAppModal.js
+++ src/components/DownloadAppModal/DownloadAppModal.js
@@ -1,11 +1,18 @@
 import React from 'react'
 import './DownloadAppModal.scss'
 import Modal from '../Modal'
+import Button from '../Button'
+import { axolotlPhone } from 'util/assets'
+
 export default function DownloadAppModal () {
   return <Modal>
     <div styleName='modal'>
       <div styleName='modal-container'>
-        <h1>Download the app to sign up!</h1>
+        <h1 styleName='modal-header'>Download the app to sign up!</h1>
+        <a href='www.google.com'>
+          <Button>Go to the App Store</Button>
+          <img styleName='axolotl-phone-image' src={axolotlPhone} />
+        </a>
       </div>
     </div>
   </Modal>
