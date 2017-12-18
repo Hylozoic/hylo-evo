7951ef77bf758559c799c8268a5c75a880803c0c
Andy Gimma <andy@hylo.com>
2017-12-18 12:49:57 -0500

    Remove unfinished test

--- src/components/Modal/Modal.test.js
+++
@@ -1,8 +0,0 @@
-import Modal from './Modal'
-import { shallow } from 'enzyme'
-import React from 'react'
-
-it('renders correctly', () => {
-  const wrapper = shallow(<Modal />)
-  expect(wrapper).toMatchSnapshot()
-})
