b9d8467db821f92d9949fb353841c5fdf7852523
Andy Gimma <andy@hylo.com>
2017-12-20 12:18:16 -0500

    Signup connector keys test

--- src/routes/NonAuthLayout/Signup/Signup.connector.test.js
+++ src/routes/NonAuthLayout/Signup/Signup.connector.test.js
@@ -1,7 +1,11 @@
-import { mapDispatchToProps } from './Signup.connector'
+import { mapDispatchToProps, mapStateToProps } from './Signup.connector'
 
 describe('Signup', () => {
   it('should call signup', () => {
     expect(mapDispatchToProps.signup('name', 'test@hylo.com', 'testPassword')).toMatchSnapshot()
+  })
+  it('returns the right keys', () => {
+    expect(mapStateToProps({}, {}).hasOwnProperty('downloadAppUrl')).toBeTruthy()
+    expect(mapStateToProps({}, {}).hasOwnProperty('returnToURL')).toBeTruthy()
   })
 })
