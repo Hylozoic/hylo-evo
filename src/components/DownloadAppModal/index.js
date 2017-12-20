0848cd98e2ea899aa0f72bdc44d21c3520229ff1
Andy Gimma <andy@hylo.com>
2017-12-19 20:38:20 -0500

    Use root DOM element from /util

--- src/components/DownloadAppModal/index.js
+++ src/components/DownloadAppModal/index.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import DownloadAppModal from './DownloadAppModal'
+import { rootDomId } from 'client/util'
 
 const DownloadAppPortal = function (props) {
   return ReactDOM.createPortal(
     <DownloadAppModal {...props} />,
-      document.getElementById('root')
+      document.getElementById(rootDomId)
     )
 }
 
