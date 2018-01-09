8e6eb897b32cb570098a0d5ef939166a54b93ce8
Ray Marceau <razor2n4@gmail.com>
2018-01-08 19:15:54 -0800

    abstract out the URL logic from the shared removableListItem component

--- src/components/RemovableListItem/RemovableListItem.js
+++ src/components/RemovableListItem/RemovableListItem.js
@@ -1,10 +1,9 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import RoundImage from 'components/RoundImage'
-import { personUrl } from 'util/index'
 import './RemovableListItem.scss'
 
-export default function RemovableListItem ({ item, slug, removeItem, square, size, confirmMessage, url = personUrl(item.id, slug) }) {
+export default function RemovableListItem ({ item, removeItem, square, size, confirmMessage, url }) {
   const remove = () => {
     confirmMessage = confirmMessage || `Are you sure you want to remove ${item.name}?`
     if (window.confirm(confirmMessage)) {
