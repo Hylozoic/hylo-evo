3d5c580c92d5c7a9093baf6fb1aacbc510d74870
Loren Johnson <loren@hellovenado.com>
2018-01-09 14:53:58 -0800

    Fix default banner and avatars for communities WEB-316

--- src/routes/NetworkCommunities/NetworkCommunities.js
+++ src/routes/NetworkCommunities/NetworkCommunities.js
@@ -1,5 +1,5 @@
-import PropTypes from 'prop-types'
 import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 import './NetworkCommunities.scss'
 import { DEFAULT_AVATAR } from 'store/models/Community'
 import Dropdown from 'components/Dropdown'
