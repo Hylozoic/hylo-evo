d9afc534f3aa8932083a7e8f3dac6eb04fae7310
Andy Gimma <andy@hylo.com>
2018-01-25 11:17:16 -0500

    Remove comment

--- src/components/Thread/Thread.js
+++ src/components/Thread/Thread.js
@@ -49,30 +49,3 @@ export default class Thread extends React.Component {
     </div>
   }
 }
-
-// export function Header ({ thread, currentUser, showAll }) {
-//   const participants = get('participants', thread) || []
-//   const id = get('id', currentUser)
-//   const others = map('name', filter(f => f.id !== id, participants))
-//   const maxCharacters = 60
-//   const maxShown = showAll ? undefined : calculateMaxShown(others, maxCharacters)
-//   const headerText = isEmpty(others)
-//     ? 'You'
-//     : formatNames(others, maxShown)
-//
-//   return <div styleName='header' id='thread-header'>
-//     <div styleName='header-text'>
-//       {headerText}
-//     </div>
-//     <div>show</div>
-//     <CloseMessages />
-//   </div>
-// }
-//
-// export function calculateMaxShown (names, maxCharacters) {
-//   let count = 0
-//   for (let i = 0; i < names.length; i++) {
-//     count += names[i].length
-//     if (count > maxCharacters) return i
-//   }
-// }
