609cbada64711e0faf5224939f5f48a3d5d486ce
Andy Gimma <andy@hylo.com>
2018-02-09 09:51:04 -0500

    Add current user checks

--- src/routes/PrimaryLayout/components/TopNav/NotificationsDropdown/NotificationsDropdown.js
+++ src/routes/PrimaryLayout/components/TopNav/NotificationsDropdown/NotificationsDropdown.js
@@ -40,7 +40,8 @@ export default class NotificationsDropdown extends Component {
   }
 
   componentDidMount = () => {
-    this.props.fetchNotifications()
+    const { currentUser, fetchNotifications } = this.props
+    currentUser && fetchNotifications()
   }
 
   render () {
