1c7d426742bc6a7d8d54c5c1173a274c946832b4
Ray Marceau <razor2n4@gmail.com>
2018-03-06 15:34:32 -0800

    coverage for connector

---
+++ src/routes/CommunitySettings/ModeratorsSettingsTab/ModeratorsSettingsTab.connector.test.js
@@ -0,0 +1,50 @@
+import { mapStateToProps, mapDispatchToProps, mergeProps } from './ModeratorsSettingsTab.connector'
+import orm from 'store/models'
+
+let state
+beforeAll(() => {
+  const session = orm.session(orm.getEmptyState())
+  const community = session.Community.create({id: '99', slug: 'foo'})
+  session.Community.create({id: '88', slug: 'bar'})
+
+  session.Me.create({
+    id: '1',
+    memberships: [session.Membership.create({
+      id: '345',
+      community: community.id,
+      hasModeratorRole: true
+    })]
+  })
+
+  session.Person.create({
+    id: '1'
+  })
+
+  session.Person.create({
+    id: '2'
+  })
+
+  session.Membership.create({
+    id: '355',
+    community: community.id,
+    person: '2',
+    hasModeratorRole: true
+  })
+
+  community.update({moderators: ['1', '2']})
+
+  state = {
+    orm: session.state,
+    ModeratorsSettings: []
+  }
+})
+
+describe('mapStateToProps', () => {
+  it('works', () => {
+    const props = {
+      communityId: '99'
+    }
+
+    expect(mapStateToProps(state, props)).toMatchSnapshot()
+  })
+})
