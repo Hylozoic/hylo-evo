3ad12614211126fee26cf383ac9254b025dee487
Andy Gimma <andy@hylo.com>
2018-03-12 23:17:26 -0500

    Button design

--- src/components/SendAnnouncementModal/SendAnnouncementModal.js
+++ src/components/SendAnnouncementModal/SendAnnouncementModal.js
@@ -8,8 +8,8 @@ export default function SendAnnouncementModal ({closeModal, save}) {
       <h1 styleName='modal-header'>MAKE AN ANNOUNCEMENT</h1>
       <p styleName='modal-paragraph'>Send an email and push notif to the entire community of ___ members along with this announcement.</p>
       <a>
-        <Button onClick={closeModal}>Go Back</Button>
-        <Button onClick={save}>Send It</Button>
+        <Button styleName='close-button' small onClick={closeModal}>Go Back</Button>
+        <Button styleName='send-button' small onClick={save}>Send It</Button>
       </a>
     </div>
   </div>
