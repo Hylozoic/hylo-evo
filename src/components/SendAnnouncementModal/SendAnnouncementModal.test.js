e4cbae44cdd994cd049a06b8524caf8fa1522632
Andy Gimma <andy@hylo.com>
2018-03-21 11:12:17 -0500

    SendAnnouncementTest and refactor

--- src/components/SendAnnouncementModal/SendAnnouncementModal.test.js
+++ src/components/SendAnnouncementModal/SendAnnouncementModal.test.js
@@ -6,7 +6,9 @@ it('renders correctly', () => {
   const props = {
     closeModal: jest.fn(),
     save: jest.fn(),
-    communityMembersCount: 100
+    communityMembersCount: 100,
+    myModeratedCommunities: [{id: 1}],
+    communities: [{id: 1}, {id: 2}]
   }
   const wrapper = shallow(<SendAnnouncementModal {...props} />)
   expect(wrapper).toMatchSnapshot()
