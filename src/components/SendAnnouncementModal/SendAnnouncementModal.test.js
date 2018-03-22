0e19ecd7d6c3b3ea20bfeb21761b53ef8f1b6325
Andy Gimma <andy@hylo.com>
2018-03-22 10:43:40 -0500

    Update tests

--- src/components/SendAnnouncementModal/SendAnnouncementModal.test.js
+++ src/components/SendAnnouncementModal/SendAnnouncementModal.test.js
@@ -2,7 +2,31 @@ import SendAnnouncementModal from './SendAnnouncementModal'
 import { shallow } from 'enzyme'
 import React from 'react'
 
-it('renders correctly', () => {
+it('renders correctly with one community', () => {
+  const props = {
+    closeModal: jest.fn(),
+    save: jest.fn(),
+    communityMembersCount: 100,
+    myModeratedCommunities: [{id: 1}],
+    communities: [{id: 1}]
+  }
+  const wrapper = shallow(<SendAnnouncementModal {...props} />)
+  expect(wrapper).toMatchSnapshot()
+})
+
+it('renders correctly with multiple communities', () => {
+  const props = {
+    closeModal: jest.fn(),
+    save: jest.fn(),
+    communityMembersCount: 100,
+    myModeratedCommunities: [{id: 1}, {id: 2}],
+    communities: [{id: 1}, {id: 2}]
+  }
+  const wrapper = shallow(<SendAnnouncementModal {...props} />)
+  expect(wrapper).toMatchSnapshot()
+})
+
+it('renders correctly with multiple communities, where the current user is not always a moderator', () => {
   const props = {
     closeModal: jest.fn(),
     save: jest.fn(),
