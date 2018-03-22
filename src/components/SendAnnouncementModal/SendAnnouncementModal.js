abafe34648f31dc5b44c19854570d020f679182c
Andy Gimma <andy@hylo.com>
2018-03-22 10:29:30 -0500

    SendAnnouncementModal text changes

--- src/components/SendAnnouncementModal/SendAnnouncementModal.js
+++ src/components/SendAnnouncementModal/SendAnnouncementModal.js
@@ -9,18 +9,25 @@ export default function SendAnnouncementModal ({
   myModeratedCommunities,
   communities
 }) {
-  const communityLabel = communityCount > 1 ? `${communityCount} communities` : 'this community'
   const communityIds = communities.map(c => c.id)
   const communityModIds = myModeratedCommunities.map(c => c.id)
-  const canModerateAllCommunities = communityIds.every(val => communityModIds.indexOf(val) >= 0)
+  const canModerateAllCommunities = !communityIds.every(val => communityModIds.indexOf(val) >= 0)
 
   return <div styleName='modal'>
     <div styleName='modal-container'>
       <h1 styleName='modal-header'>MAKE AN ANNOUNCEMENT</h1>
-      <p styleName='modal-paragraph'>Send an email and push notification to the each member of {communityLabel} along with this announcement.</p>
+      {communityCount === 1 && canModerateAllCommunities &&
+        <p styleName='modal-paragraph'>This means that all members of this community will receive an instant email and push notification about this Post. (Community Moderators only)</p>
+      }
+      {communityCount > 1 && canModerateAllCommunities &&
+        <p styleName='modal-paragraph'>This means that all members of the {communityCount} communities selected will receive instant email and push notifications about this Post. (Community Moderators only)</p>
+      }
 
       {!canModerateAllCommunities &&
-        <p styleName='modal-paragraph'>This will only be sent as an announcement to the communities where you are are a moderator. For other communities it will be shared as a regular post.</p>
+        <span>
+          <p styleName='modal-paragraph'>This means that all members of the {communityCount} communities selected will receive an instant email and push notification about this Post.</p>
+          <p styleName='modal-paragraph'>This will only be sent as an Announcement to the communities where you are a Moderator. For other communities it will be shared as a regular Post.</p>
+        </span>
       }
       <a>
         <Button styleName='close-button' small onClick={closeModal}>Go Back</Button>
