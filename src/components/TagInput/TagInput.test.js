89b45c562d82c9b748044cfeea8549ad12ca1bb2
Robbie Carlton <robbie.carlton@gmail.com>
2018-03-27 16:35:17 -0600

    Topic line bugs and finishing touches WEB-387 (#305)
    * add leading hashtags to TagInput
    
    * strip leading hashtag in input
    
    * add post to correct topic query results
    
    * topic suggestion dropdown improvement
    
    * fix error
    
    * fix tests
    
    * improve coverage
    
    * improve coverage
    
    * pr cleanup

--- src/components/TagInput/TagInput.test.js
+++ src/components/TagInput/TagInput.test.js
@@ -18,13 +18,32 @@ describe('TagInput', () => {
     expect(wrapper).toMatchSnapshot()
   })
 
+  it('renders correctly with tags', () => {
+    const props = {
+      tags: [{name: 'one', id: 1}, {name: 'two', id: 2}]
+    }
+    const wrapper = renderComponent(shallow, props)
+    expect(wrapper).toMatchSnapshot()
+  })
+
+  it('adds leading hashtags when flag is set', () => {
+    const props = {
+      addLeadingHashtag: true,
+      tags: [{name: 'one', id: 1}, {name: 'two', id: 2}]
+    }
+    const wrapper = renderComponent(shallow, props)
+    expect(wrapper).toMatchSnapshot()
+  })
+
   describe('resetInput', () => {
-    const handleInputChange = jest.fn()
-    const wrapper = renderComponent(shallow, { handleInputChange })
-    const input = {value: 'old'}
-    wrapper.instance().input = input
-    wrapper.instance().resetInput()
-    expect(input.value).toEqual('')
-    expect(handleInputChange).toHaveBeenCalledWith('')
+    it("sets input.value to '' and calls handleInputChange", () => {
+      const handleInputChange = jest.fn()
+      const wrapper = renderComponent(shallow, { handleInputChange })
+      const input = {value: 'old'}
+      wrapper.instance().input = input
+      wrapper.instance().resetInput()
+      expect(input.value).toEqual('')
+      expect(handleInputChange).toHaveBeenCalledWith('')
+    })
   })
 })
