9b6565e5ca59b9d05413cbbdf8cc9d0a944e9391
Ray Marceau <razorman8669@users.noreply.github.com>
2018-04-30 17:24:31 -0700

    Web-519 notifications button click target (#313)

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -1,19 +1,18 @@
 import React from 'react'
-import Icon from 'components/Icon'
 import './Select.scss'
+import { defaultTo } from 'lodash'
 
 export default function Select ({ options, selected, onChange, placeholder, fullWidth = false }) {
   const style = fullWidth ? {
     width: '100%'
   } : { }
 
-  return <span styleName='select-wrapper'>
-    <select styleName='select' style={style} onChange={e => onChange(e.target.value)} value={selected}>
+  return <label styleName='select-wrapper' style={style} >
+    <select styleName='select' style={style} onChange={e => onChange(e.target.value)} value={defaultTo(selected, '')}>
       {placeholder && <option value='' disabled>{placeholder}</option>}
       {options.map(({id, label}) => <option value={id} key={id}>
         {label}
       </option>)}
     </select>
-    <Icon name='ArrowDown' styleName='icon' />
-  </span>
+  </label>
 }
