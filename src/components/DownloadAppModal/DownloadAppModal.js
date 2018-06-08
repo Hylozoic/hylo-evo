4194f68a9a9c9c8421c9c7efbe04c741b5842a81
Andy Gimma <andy@hylo.com>
2018-06-08 11:35:11 -0500

    Add ios Universal Link: DEMO ONLY

--- src/components/DownloadAppModal/DownloadAppModal.js
+++ src/components/DownloadAppModal/DownloadAppModal.js
@@ -4,11 +4,17 @@ import Button from '../Button'
 import { axolotlPhone } from 'util/assets'
 
 export default function DownloadAppModal ({url, header = 'Download the app to use Hylo on mobile!'}) {
+  const origin = 'HyloApp://test'
+  const deepLinkPath = origin + window.location.pathname
   return <div styleName='modal'>
     <div styleName='modal-container'>
       <h1 styleName='modal-header'>{header}</h1>
       <a href={url}>
         <Button>Go to the App Store</Button>
+        <img styleName='axolotl-phone-image' src={axolotlPhone} />
+      </a>
+      <a href={deepLinkPath}>
+        <Button>Go to the app</Button>
         <img styleName='axolotl-phone-image' src={axolotlPhone} />
       </a>
     </div>
