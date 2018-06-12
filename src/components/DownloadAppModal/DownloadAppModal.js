85e6dca8dbcf0c7d253791e936ae1977fc9be543
Andy Gimma <andy@hylo.com>
2018-06-12 10:18:07 -0500

    Use a working deep link path

--- src/components/DownloadAppModal/DownloadAppModal.js
+++ src/components/DownloadAppModal/DownloadAppModal.js
@@ -4,8 +4,9 @@ import Button from '../Button'
 import { axolotlPhone } from 'util/assets'
 
 export default function DownloadAppModal ({url, header = 'Download the app to use Hylo on mobile!'}) {
-  const origin = 'HyloApp://test'
-  const deepLinkPath = origin + window.location.pathname
+  // const origin = 'HyloApp://test'
+  // const deepLinkPath = origin + window.location.pathname
+  const deepLinkPath = 'HyloApp://c/hylo'
   return <div styleName='modal'>
     <div styleName='modal-container'>
       <h1 styleName='modal-header'>{header}</h1>
