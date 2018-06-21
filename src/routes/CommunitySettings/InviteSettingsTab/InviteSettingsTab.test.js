0601c7ac5bae873f2f2aca88d7b557dc03101ec6
Andy Gimma <andy@hylo.com>
2018-06-21 14:01:51 -0500

    Add tests

--- src/routes/CommunitySettings/InviteSettingsTab/InviteSettingsTab.test.js
+++ src/routes/CommunitySettings/InviteSettingsTab/InviteSettingsTab.test.js
@@ -15,3 +15,23 @@ it('renders correctly', () => {
     />)
   expect(wrapper).toMatchSnapshot()
 })
+
+it('toggles allowCommunityInvites and calls the function to make the request on the server', () => {
+  const community = {
+    id: 1,
+    name: 'Hylo'
+  }
+  const allMembersCanInvite = false
+  const allowCommunityInvites = jest.fn(() => new Promise(() => {}))
+
+  const wrapper = shallow(<InviteSettingsTab
+    community={community}
+    regenerateAccessCode={() => {}}
+    inviteLink='http://www.hylo.com/c/hylo/join/lalala'
+    allMembersCanInvite={allMembersCanInvite}
+    allowCommunityInvites={allowCommunityInvites}
+    />)
+  wrapper.instance().toggleSwitch()
+  expect(allowCommunityInvites).toBeCalled()
+  expect(wrapper.instance().state.allMembersCanInvite).toBeTruthy()
+})
