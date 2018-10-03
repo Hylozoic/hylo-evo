3d49e3429c43a94277a96b838aafcb32efd197b0
Loren Johnson <lorenjohnson@gmail.com>
2018-10-03 15:06:47 -0700

    Remove cruft

--- src/store/reducers/resetStore.test.js
+++ src/store/reducers/resetStore.test.js
@@ -1,16 +1,3 @@
-// import { rootReducer, initialState } from 'store'
-// import { LOGOUT } from 'store/constants'
-
-// describe('store', () => {
-//   it('resets to initial state on logout', () => {
-//     const fullInitialState = rootReducer(initialState, {})
-//     const state = {foo: 'bar'}
-//     const action = {type: LOGOUT}
-//     const newState = rootReducer(state, action)
-//     expect(newState).toEqual(fullInitialState)
-//   })
-// })
-import { pick } from 'lodash/fp'
 import { getEmptyState } from 'store'
 import resetStore, { PRESERVE_STATE_ON_RESET } from './resetStore'
 import {
