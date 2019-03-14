d56f0a0395444ee76a91a0dcd948cbb64f4f4b36
Robbie Carlton <robbie.carlton@gmail.com>
2019-03-14 15:52:28 -0600

    remove cruft

--- src/components/PostCard/PostBody/PostBody.js
+++ src/components/PostCard/PostBody/PostBody.js
@@ -14,10 +14,6 @@ import cx from 'classnames'
 
 const maxDetailsLength = 144
 
-export const formatDate = date => {
-  return date
-}
-
 export default function PostBody ({
   post,
   slug,
@@ -29,45 +25,4 @@ export default function PostBody ({
     <PostTitle {...post} highlightProp={highlightProps} />
     <PostDetails {...post} slug={slug} highlightProp={highlightProps} hideDetails={!expanded} expanded={expanded} />
   </div>
-}
-
-export function PostBodyText ({
-  title,
-  details,
-  linkPreview,
-  slug,
-  expanded,
-  className,
-  highlightProps,
-  fileAttachments,
-  hideDetails
-}) {
-  title = decode(title)
-  details = present(sanitize(details), {slug})
-  if (!expanded && textLength(details) > maxDetailsLength) {
-    details = truncate(details, maxDetailsLength)
-  }
-
-  return <Highlight {...highlightProps}>
-    <div styleName={cx('body', {smallMargin: hideDetails})} className={className}>
-      <div styleName='title' className='hdr-headline'>{title}</div>
-      {details && !hideDetails &&
-        <ClickCatcher>
-          <div styleName='details' dangerouslySetInnerHTML={{__html: details}} />
-        </ClickCatcher>
-      }
-      {linkPreview &&
-        <LinkPreview {...pick(['title', 'url', 'imageUrl'], linkPreview)} />}
-      {fileAttachments && <div styleName='file-attachments'>
-        {fileAttachments.map(fileAttachment =>
-          <a styleName='file-attachment'
-            href={fileAttachment.url}
-            target='_blank'
-            key={fileAttachment.id}>
-            <Icon name='Document' styleName='file-icon' />
-            <span styleName='file-name'>{decodeURIComponent(path.basename(fileAttachment.url))}</span>
-          </a>)}
-      </div>}
-    </div>
-  </Highlight>
 }
