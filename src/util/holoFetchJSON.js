cb694391b1934d102ed7075040889595385b61a5
Loren Johnson <lorenjohnson@gmail.com>
2019-03-26 22:06:31 -0700

    turn of holo dependency for now

--- src/util/holoFetchJSON.js
+++ src/util/holoFetchJSON.js
@@ -1,12 +1,16 @@
-import { connect } from '@holochain/hc-web-client'
+// FIX: dependency causing troubles so removing for now
+// 
+// "@holochain/hc-web-client": "https://github.com/holochain/hc-web-client#call-raw"
+// 
+// import { connect } from '@holochain/hc-web-client'
 
 var call
 
 export default async function holoFetchJSON (path, params) {
-  if (!call) {
-    const connection = await connect(process.env.HOLO_CHAT_API_HOST)
-    call = connection.call
-  }
+  // if (!call) {
+  //   const connection = await connect(process.env.HOLO_CHAT_API_HOST)
+  //   call = connection.call
+  // }
 
   return call(path)(params)
 }
