f4dc0dfaaeecd20f6021cdea9905d10a81aa6358
Loren Johnson <lorenjohnson@gmail.com>
2019-04-01 22:58:09 -0700

    Remove holo fetch

--- src/util/holoFetchJSON.js
+++ src/util/holoFetchJSON.js
@@ -1,12 +1,12 @@
-import { connect } from '@holochain/hc-web-client'
+// import { connect } from '@holochain/hc-web-client'
 
 var call
 
 export default async function holoFetchJSON (path, params) {
-  if (!call) {
-    const connection = await connect(process.env.HOLO_CHAT_API_HOST)
-    call = connection.call
-  }
+  // if (!call) {
+  //   const connection = await connect(process.env.HOLO_CHAT_API_HOST)
+  //   call = connection.call
+  // }
 
   return call(path)(params)
 }
