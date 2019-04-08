6c2ffe5a9e4b603708c25fd6b66140b700aaac21
Loren Johnson <lorenjohnson@gmail.com>
2019-04-08 11:17:55 -0700

    Fix build issue with CSS modules

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -217,7 +217,9 @@ module.exports = {
     }),
     // Note: this won't work without ExtractTextPlugin.extract(..) in `loaders`.
     new ExtractTextPlugin({
-      filename: cssFilename
+      filename: cssFilename,
+      allChunks: true,
+      ignoreOrder: true
     }),
     // Generate a manifest file which contains a mapping of all asset filenames
     // to their corresponding output file so that tools can pick it up without
