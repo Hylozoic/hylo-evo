c5e2f6fad3d0bbc54acf0fc4de285f415d100e16
Robbie Carlton <robbie.carlton@gmail.com>
2019-04-10 10:16:21 -0600

    coverage

--- src/components/EventInviteDialog/EventInviteDialog.connector.js
+++ src/components/EventInviteDialog/EventInviteDialog.connector.js
@@ -1,26 +1,6 @@
 import { connect } from 'react-redux'
-import { createSelector } from 'redux-orm'
-import orm from 'store/models'
 import fetchPeople from 'store/actions/fetchPeople'
-import { invitePeopleToEvent } from './EventInviteDialog.store'
-import { pick } from 'lodash/fp'
-
-export function presentPerson (person) {
-  return {
-    ...pick([ 'id', 'name', 'avatarUrl' ], person.ref),
-    community: person.memberships.first()
-      ? person.memberships.first().community.name : null
-  }
-}
-
-export const peopleSelector = createSelector(
-  orm,
-  state => state.orm,
-  session => session.Person.all()
-    .orderBy('name')
-    .toModelArray()
-    .map(presentPerson)
-)
+import { invitePeopleToEvent, peopleSelector } from './EventInviteDialog.store'
 
 export function mapStateToProps (state, props) {
   const people = peopleSelector(state, props)
@@ -33,4 +13,5 @@ export function mapStateToProps (state, props) {
 export const mapDispatchToProps = {
   fetchPeople, invitePeopleToEvent
 }
+
 export default connect(mapStateToProps, mapDispatchToProps)
--- src/components/EventInviteDialog/EventInviteDialog.store.js
+++ src/components/EventInviteDialog/EventInviteDialog.store.js
@@ -1,6 +1,27 @@
+import { createSelector } from 'redux-orm'
+import orm from 'store/models'
+import { pick } from 'lodash/fp'
+
 export const MODULE_NAME = 'EventInviteDialog'
 export const INVITE_PEOPLE_TO_EVENT = `${MODULE_NAME}/INVITE_PEOPLE_TO_EVENT`
 export const INVITE_PEOPLE_TO_EVENT_PENDING = `${INVITE_PEOPLE_TO_EVENT}_PENDING`
+
+export function presentPerson (person) {
+  return {
+    ...pick([ 'id', 'name', 'avatarUrl' ], person.ref),
+    community: person.memberships.first()
+      ? person.memberships.first().community.name : null
+  }
+}
+
+export const peopleSelector = createSelector(
+  orm,
+  state => state.orm,
+  session => session.Person.all()
+    .orderBy('name')
+    .toModelArray()
+    .map(presentPerson)
+)
 
 export function invitePeopleToEvent (eventId, inviteeIds) {
   return {
