efa1153b16ba64006180834db6b157857a8a01c0
Loren Johnson <lorenjohnson@gmail.com>
2019-04-10 01:12:07 -0700

    Match more of CRA latest dependencies

--- config/webpack.config.shared.js
+++ config/webpack.config.shared.js
@@ -17,6 +17,13 @@ module.exports = {
     options: {
       ident: 'postcss', // https://webpack.js.org/guides/migrating/#complex-options
       plugins: () => [
+        require('postcss-flexbugs-fixes'),
+        require('postcss-preset-env')({
+          autoprefixer: {
+            flexbox: 'no-2009',
+          },
+          stage: 3,
+        }),
         require('autoprefixer')({
           browsers: [
             '>1%',
