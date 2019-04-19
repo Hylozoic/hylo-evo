bc3ff0ae4d68c8779211763a42ee2913e740c5d7
Loren Johnson <lorenjohnson@gmail.com>
2019-04-19 12:59:56 -0700

    Linting and test fixes

--- src/components/FeedList/FeedList.js
+++ src/components/FeedList/FeedList.js
@@ -47,7 +47,7 @@ export default class FeedList extends React.Component {
   })
 
   componentDidMount () {
-    this.fetchOrShowCached()    
+    this.fetchOrShowCached()
   }
 
   componentDidUpdate (prevProps) {
