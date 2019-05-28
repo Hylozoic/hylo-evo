021c6b3d8b02959580774f21a16a64ad7ec9e17b
Loren Johnson <lorenjohnson@gmail.com>
2019-05-28 06:04:00 +0200

    Add random avatar button and avatar preview to hc reg

--- src/components/Avatar/Avatar.js
+++ src/components/Avatar/Avatar.js
@@ -6,13 +6,17 @@ import { Link } from 'react-router-dom'
 const { string, bool } = PropTypes
 
 export default function Avatar ({ url, avatarUrl, small, medium, className }) {
-  return <Link to={url} className={className}>
-    <RoundImage url={avatarUrl} small={small} medium={medium} />
-  </Link>
+  if (url) {
+    return <Link to={url} className={className}>
+      <RoundImage url={avatarUrl} small={small} medium={medium} />
+    </Link>
+  } else {
+    return <RoundImage className={className} url={avatarUrl} small={small} medium={medium} />
+  }
 }
 Avatar.propTypes = {
-  url: string.isRequired,
-  avatarUrl: string,
+  url: string,
+  avatarUrl: string.isRequired,
   small: bool,
   medium: bool,
   className: string
--- src/routes/NonAuthLayout/HolochainLogin/HolochainLogin.js
+++ src/routes/NonAuthLayout/HolochainLogin/HolochainLogin.js
@@ -4,7 +4,9 @@ import TextInput from 'components/TextInput'
 import Button from 'components/Button'
 import { defaultHolochainCommunityUrl } from 'util/navigation'
 import { formatError } from '../util'
+import fetch from 'isomorphic-fetch'
 import './HolochainLogin.scss'
+import Avatar from 'components/Avatar/Avatar';
 
 export default class HolochainLogin extends React.Component {
   constructor (props) {
@@ -23,6 +25,15 @@ export default class HolochainLogin extends React.Component {
 
   onChangeHandlerForKey = key => event => this.setState({ [key]: event.target.value })
 
+  getRandomAvatarUrl = async () => {    
+    const randomUserResponse = await fetch('https://randomuser.me/api')
+    const randomUserResult = await randomUserResponse.json()
+    const randomUser = randomUserResult.results[0]
+
+    this.setState(() => ({ avatarUrl: randomUser.picture.thumbnail }))
+    this.avatarUrl.value = randomUser.picture.thumbnail
+  }
+
   render () {
     return <div className={this.props.className}>
       <h1 styleName='title'>Register your Holochain agent</h1>
@@ -33,9 +44,25 @@ export default class HolochainLogin extends React.Component {
           inputRef={input => { this.name = input }} autoFocus />
       </div>
       <div styleName='field'>
-        <label htmlFor='avatarUrl' styleName='field-label'>Avatar URL</label>
-        <TextInput aria-label='avatarUrl' label='avatarUrl' type='text' name='avatarUrl' onChange={this.onChangeHandlerForKey('avatarUrl')}
-          inputRef={input => { this.avatarUrl = input }} />
+        <label styleName='field-label avatar-entry-label' htmlFor='avatarUrl'>
+          Avatar URL
+          <a styleName='avatar-entry-label-link' onClick={this.getRandomAvatarUrl}>get random avatar</a>
+        </label>
+        <div styleName='avatar-entry'>
+          <Avatar
+            styleName='avatar-entry-preview'
+            avatarUrl={this.state.avatarUrl}
+          />
+          <TextInput
+            styleName='avatar-entry-url'
+            aria-label='avatarUrl'
+            label='avatarUrl'
+            type='text'
+            name='avatarUrl'
+            onChange={this.onChangeHandlerForKey('avatarUrl')}
+            inputRef={input => { this.avatarUrl = input }}
+          />
+        </div>
       </div>
       <Button styleName='submit' label='Log In' onClick={this.submit} />
     </div>
