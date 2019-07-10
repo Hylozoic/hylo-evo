883647b24f344320d3790c9f01c16f7fb1bd4e64
JhonatanHern <jhonatanhernandez998@gmail.com>
2019-07-10 17:37:47 -0400

    added markdown to PostLabel and deleted story for component thet used redux

--- src/components/PostLabel/PostLabel.stories.js
+++ src/components/PostLabel/PostLabel.stories.js
@@ -2,34 +2,63 @@ import React from 'react'
 import { storiesOf } from '@storybook/react'
 import PostLabel from './PostLabel'
 
+const markdown = `
+Different labels for posts, possible types are:
+
+- label
+- discussion
+- event
+- offer
+- project
+- request
+`
+
 storiesOf('PostLabel', module)
   .add('label',
     () => (
       <PostLabel type={'label'} className={'label'} />
-    )
+    ),
+    {
+      notes: { markdown }
+    }
   )
   .add('discussion',
     () => (
       <PostLabel type={'discussion'} className={'discussion'} />
-    )
+    ),
+    {
+      notes: { markdown }
+    }
   )
   .add('event',
     () => (
       <PostLabel type={'event'} className={'event'} />
-    )
+    ),
+    {
+      notes: { markdown }
+    }
   )
   .add('offer',
     () => (
       <PostLabel type={'offer'} className={'offer'} />
-    )
+    ),
+    {
+      notes: { markdown }
+    }
   )
   .add('project',
     () => (
       <PostLabel type={'project'} className={'project'} />
-    )
+    ),
+    {
+      notes: { markdown }
+    }
   )
   .add('request',
     () => (
       <PostLabel type={'request'} className={'request'} />
-    )
+    ),
+    {
+      notes: { markdown }
+    }
   )
