f1cdce35a00f77edee2e8bb687a0c86f87d83863
Loren Johnson <loren@hellovenado.com>
2019-11-19 03:23:28 -0800

    Update topBar for smaller screens

--- src/routes/PrimaryLayout/components/TopNav/TopNav.js
+++ src/routes/PrimaryLayout/components/TopNav/TopNav.js
@@ -1,6 +1,7 @@
 import React from 'react'
+import { HOLOCHAIN_ACTIVE } from 'util/holochain'
 import { bgImageStyle } from 'util/index'
-import { personUrl } from 'util/navigation'
+import { personUrl, messagesUrl } from 'util/navigation'
 import { Link } from 'react-router-dom'
 import Icon from 'components/Icon'
 import BadgedIcon from 'components/BadgedIcon'
@@ -12,7 +13,6 @@ import { get } from 'lodash/fp'
 import { hyloLogo } from 'util/assets'
 import MessagesDropdown from './MessagesDropdown'
 import NotificationsDropdown from './NotificationsDropdown'
-import { HOLOCHAIN_ACTIVE } from 'util/holochain'
 import './TopNav.scss'
 
 export default function TopNav ({
@@ -31,18 +31,22 @@ export default function TopNav ({
 
   return <div styleName='topNavWrapper' className={className}>
     <div styleName='topNav'>
-      {goBack && <BackButton onClick={goBack} />}
-      <div styleName='logo-hover' onClick={onClick}>
+      {goBack && <Button styleName='backButton' color='gray' small narrow onClick={goBack}>
+        <Logo {...{ communityOrNetwork: community || network, toggleDrawer }} />
+        <Title label='< Back to' community={community} network={network} />
+      </Button>}
+      {!goBack && <div styleName='logo-hover' onClick={onClick}>
         <Logo {...{ communityOrNetwork: community || network, toggleDrawer }} />
         {showLogoBadge && <Badge number='1' styleName='logoBadge' border />}
         <Title community={community} network={network} onClick={toggleDrawer} />
-      </div>
+      </div>}
       <div styleName='navIcons'>
         {!HOLOCHAIN_ACTIVE && <Link to='/search'><Icon name='Search' styleName='icon' /></Link>}
-        <MessagesDropdown
+        {smallScreen && <Link to={messagesUrl()}><Icon name='Messages' styleName='icon' /></Link>}
+        {!smallScreen && <MessagesDropdown
           renderToggleChildren={showBadge =>
             <BadgedIcon name='Messages' styleName='icon' showBadge={showBadge} />}
-          smallScreen={smallScreen} />
+          smallScreen={smallScreen} />}
         {!HOLOCHAIN_ACTIVE && <NotificationsDropdown renderToggleChildren={showBadge =>
           <BadgedIcon name='Notifications' styleName='icon' showBadge={showBadge} />} />}
         <Dropdown styleName='user-menu' alignRight toggleChildren={
@@ -62,26 +66,25 @@ export default function TopNav ({
   </div>
 }
 
-function BackButton ({ onClick }) {
-  return <Button styleName='backButton' color='gray' small narrow onClick={onClick}>&lt; Back</Button>
-}
-
 function Logo ({ communityOrNetwork, toggleDrawer }) {
   const imageStyle = bgImageStyle(get('avatarUrl', communityOrNetwork) || hyloLogo)
   return <span styleName='image' style={imageStyle} onClick={toggleDrawer} />
 }
 
-function Title ({ community, network, onClick }) {
-  var [ label, name ] = ['GLOBAL', 'All Communities']
+function Title ({ community, network, onClick, label }) {
+  var [ labelText, name ] = ['GLOBAL', 'All Communities']
   if (community) {
-    [ label, name ] = ['COMMUNITY', community.name]
+    [ labelText, name ] = ['COMMUNITY', community.name]
   } else if (network) {
-    [ label, name ] = ['NETWORK', network.name]
+    [ labelText, name ] = ['NETWORK', network.name]
+  }
+  if (label) {
+    labelText = label
   }
 
   return <a styleName='title' onClick={onClick}>
     <div styleName='label'>
-      {label}
+      {labelText}
     </div>
     <div styleName='communityName'>{name}</div>
   </a>
