81fd4a0a26ac34cb76c7a16cd71be1c2362c560d
Tom Watson <thomasgabriel.watson@gmail.com>
2020-05-25 14:12:51 -0700

    Added toggle switch implemented to spec in Figma map design

--- src/components/Switch/Switch.stories.js
+++ src/components/Switch/Switch.stories.js
@@ -2,18 +2,30 @@ import React, { useState } from 'react'
 import { storiesOf } from '@storybook/react'
 import { MemoryRouter } from 'react-router'
 import Switch from 'components/Switch'
+import SwitchStyled from 'components/SwitchStyled'
 
 storiesOf('Switch', module)
   .addDecorator(story => (
     <MemoryRouter>{story()}</MemoryRouter>
   ))
   .add('Basic', () => <SwitchDemo />)
+  .add('SwitchStyled', () => <SwitchStyledDemo />)
 
 function SwitchDemo () {
-  const [switchBool, setSwitchFool] = useState(true)
+  const [switchBool, setSwitchBool] = useState(true)
 
   function handleToggleSwitch () {
-    setSwitchFool(!switchBool)
+    setSwitchBool(!switchBool)
   }
   return (<Switch value={switchBool} onClick={handleToggleSwitch} />)
+}
+
+function SwitchStyledDemo () {
+  const [switchBool, setSwitchBool] = useState(false)
+
+  function handleToggleSwitch (checked, name) {
+    console.log(checked, name)
+    setSwitchBool(!switchBool)
+  }
+  return (<SwitchStyled checked={switchBool} name='demo' onChange={handleToggleSwitch} />)
 }
