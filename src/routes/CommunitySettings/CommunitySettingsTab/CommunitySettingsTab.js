3c8f3f9847afb2d9ac39f6e509481e1497d7d771
Clare Politano Hutchings <cpolitano8@gmail.com>
2020-06-16 10:44:32 -0700

    community privacy settings UI

--- src/routes/CommunitySettings/CommunitySettingsTab/CommunitySettingsTab.js
+++ src/routes/CommunitySettings/CommunitySettingsTab/CommunitySettingsTab.js
@@ -1,9 +1,11 @@
 import PropTypes from 'prop-types'
 import React, { Component } from 'react'
+import cx from 'classnames'
 import './CommunitySettingsTab.scss'
 import Button from 'components/Button'
 import ChangeImageButton from 'components/ChangeImageButton'
 import SettingsControl from 'components/SettingsControl'
+import SwitchStyled from 'components/SwitchStyled'
 import Loading from 'components/Loading'
 import { bgImageStyle } from 'util/index'
 import { DEFAULT_BANNER, DEFAULT_AVATAR } from 'store/models/Community'
@@ -39,13 +41,20 @@ export default class CommunitySettingsTab extends Component {
       name, description, location, avatarUrl, bannerUrl
     } = community
 
+    let isDiscoverable = false
+    let isJoinable = false
+    let membersViewable = false
+
     this.setState({
       edits: {
         name: name || '',
         description: description || '',
         location: location || '',
         avatarUrl: avatarUrl || DEFAULT_AVATAR,
-        bannerUrl: bannerUrl || DEFAULT_BANNER
+        bannerUrl: bannerUrl || DEFAULT_BANNER,
+        isDiscoverable: isDiscoverable || false,
+        isJoinable: isJoinable || false,
+        membersViewable: membersViewable || false
       }
     })
   }
@@ -56,6 +65,8 @@ export default class CommunitySettingsTab extends Component {
     if (key === 'location') {
       edits['location'] = event.target.value.fullText
       edits['locationId'] = event.target.value.id
+    } else if (key === 'isDiscoverable' || key === 'isJoinable' || key === 'membersViewable') {
+      edits[key] = !edits[key]
     } else {
       edits[key] = event.target.value
     }
@@ -80,7 +91,7 @@ export default class CommunitySettingsTab extends Component {
 
     const { edits, changed } = this.state
     const {
-      name, description, location, avatarUrl, bannerUrl
+      name, description, location, avatarUrl, bannerUrl, isDiscoverable, isJoinable, membersViewable
     } = edits
 
     const locationObject = community.locationObject || currentUser.locationObject
@@ -107,6 +118,26 @@ export default class CommunitySettingsTab extends Component {
         locationObject={locationObject}
         type='location'
       />
+      <div styleName='privacy-settings'>
+        <div styleName='control-label'>Community Privacy</div>
+        <div styleName='privacy-detail'>By default, your community is not listed in the public directory.
+        Only people invited to your community can join it, and only members of your community can see posts.</div>
+        <div styleName={cx('privacy-option-container', { on: isDiscoverable })}>
+          <SwitchStyled checked={isDiscoverable} onChange={this.updateSetting('isDiscoverable')} backgroundColor={isDiscoverable ? '#40A1DD' : '#808C9B'} />
+          <span styleName='privacy-option'>Allow anyone to see {community.name} and request to become a member</span>
+          <span styleName='privacy-state'>{isDiscoverable ? 'ON' : 'OFF'}</span>
+        </div>
+        <div styleName={cx('privacy-option-container', { on: isJoinable })}>
+          <SwitchStyled checked={isJoinable} onChange={this.updateSetting('isJoinable')} backgroundColor={isJoinable ? '#40A1DD' : '#808C9B'} />
+          <span styleName='privacy-option'>Allow anyone to join {community.name}</span>
+          <span styleName='privacy-state'>{isJoinable ? 'ON' : 'OFF'}</span>
+        </div>
+        <div styleName={cx('privacy-option-container', { on: membersViewable })}>
+          <SwitchStyled checked={membersViewable} onChange={this.updateSetting('membersViewable')} backgroundColor={membersViewable ? '#40A1DD' : '#808C9B'} />
+          <span styleName='privacy-option'>Anyone can see who is a member of {community.name}</span>
+          <span styleName='privacy-state'>{membersViewable ? 'ON' : 'OFF'}</span>
+        </div>
+      </div>
       <div styleName='button-row'>
         <Button label='Save Changes' color={changed ? 'green' : 'gray'} onClick={changed ? this.save : null} styleName='save-button' />
       </div>
