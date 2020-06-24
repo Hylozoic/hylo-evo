2b92bb211fa89a36a62f281a139d88428029b6f2
Loren Johnson <lorenjohnson@gmail.com>
2020-06-24 09:41:21 -1000

    Update Invite button to look more active

--- src/components/PostCard/EventBody/EventBody.js
+++ src/components/PostCard/EventBody/EventBody.js
@@ -36,7 +36,7 @@ export default class EventBody extends Component {
       </div>
       <div styleName='eventRightColumn'>
         <EventRSVP {...event} respondToEvent={respondToEvent} />
-        <Button label='Invite' onClick={this.toggleInviteDialog} narrow small color='gray' styleName='inviteButton' />
+        <Button label='Invite' onClick={this.toggleInviteDialog} narrow small color='green-white' styleName='inviteButton' />
       </div>
       {showInviteDialog && <EventInviteDialog
         eventId={id}
