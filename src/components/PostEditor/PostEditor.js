438de95f79071204e53275f1cacc46517f544c2a
wushenshen <wushenshen@gmail.com>
2020-06-27 14:20:52 -0700

    Add locationId to default state for PostEditor and updatePost mutation vars

--- src/components/PostEditor/PostEditor.js
+++ src/components/PostEditor/PostEditor.js
@@ -93,6 +93,7 @@ export default class PostEditor extends React.Component {
     })
     const currentPost = post
       ? ({ ...post,
+        locationId: post.locationObject.id,
         startTime: Moment(post.startTime),
         endTime: Moment(post.endTime)
       })
