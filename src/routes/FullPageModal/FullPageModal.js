94d5a58844beecb0b55a6fb83eeb3fdf57be5ffb
Aaron Brodeur <aaronb@gmail.com>
2020-06-27 11:43:06 -0700

    Mobile menu for settings and community settings

--- src/routes/FullPageModal/FullPageModal.js
+++ src/routes/FullPageModal/FullPageModal.js
@@ -41,6 +41,7 @@ export default class FullPageModal extends Component {
                 key={tab.path}>
                 {tab.name}
               </NavLink>)}
+              <Icon name='ArrowDown' styleName='arrowDown' />
           </div>
         </div>
         {multipleTabs && <div styleName='center narrow'>
