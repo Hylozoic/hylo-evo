ec2bd4473a8fb6efac5f8fe18e2918750b905469
Loren Johnson <lorenjohnson@gmail.com>
2020-07-14 20:18:45 -1000

    Remove limit on pending invitations list

--- src/routes/CommunitySettings/CommunitySettings.store.js
+++ src/routes/CommunitySettings/CommunitySettings.store.js
@@ -22,7 +22,7 @@ export function fetchCommunitySettings (slug) {
           invitePath
           hidden
           allowCommunityInvites
-          pendingInvitations (first: 100) {
+          pendingInvitations {
             hasMore
             items {
               id
