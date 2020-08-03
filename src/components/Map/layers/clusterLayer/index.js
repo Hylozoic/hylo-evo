0caf2be3492b133746b62f2c93eefbf5677bf6c0
Tibet Sprague <tibet.sprague@gmail.com>
2020-08-03 16:41:24 -0700

    Dont zoom in too far when clicking on a cluster on the map

--- src/components/Map/layers/clusterLayer/index.js
+++ src/components/Map/layers/clusterLayer/index.js
@@ -80,7 +80,7 @@ export default class PostClusterLayer extends CompositeLayer {
           .map(f => f.properties)
       }
       info.object = pickedObject
-      info.expansionZoom = Math.min(this.state.index.getClusterExpansionZoom(pickedObject.cluster_id, this.context.viewport.zoom), 20)
+      info.expansionZoom = Math.min(this.state.index.getClusterExpansionZoom(pickedObject.cluster_id, this.context.viewport.zoom), 14)
     }
     return info
   }
