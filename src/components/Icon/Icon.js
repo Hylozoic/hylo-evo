f04cdc145336f8f8671e5bbd8af29e435a3b96f1
Loren Johnson <lorenjohnson@gmail.com>
2020-10-10 18:38:50 -1000

    Clean-up Icon

--- src/components/Icon/Icon.js
+++ src/components/Icon/Icon.js
@@ -2,10 +2,23 @@ import React from 'react'
 import cx from 'classnames'
 import './Icon.scss'
 
-export default function Icon ({ name, className, green, children, onClick, dataTip, dataTipFor }) {
+export default function Icon ({
+  name,
+  className,
+  green,
+  children,
+  onClick,
+  dataTip,
+  dataTipFor
+}) {
   const iconClassName = `icon-${name}`
-  return <span styleName={cx('icon', { green })} onClick={onClick} data-tip={dataTip} data-for={dataTipFor}
-    className={cx(iconClassName, className)}>
+
+  return <span
+    styleName={cx('icon', { green })}
+    className={cx(iconClassName, className)}
+    onClick={onClick}
+    data-tip={dataTip}
+    data-for={dataTipFor}>
     {children}
   </span>
 }
