daed7281f83321fd889472f8bc6e366084c15c39
wushenshen <wushenshen@gmail.com>
2020-10-28 17:00:35 -0700

    little clean up

--- src/util/navigation.js
+++ src/util/navigation.js
@@ -281,10 +281,6 @@ export function isAllCommunitiesPath (path) {
   return (path.startsWith('/all'))
 }
 
-export function isCommunityPath (path) {
-  return (path.startsWith('/c/'))
-}
-
 export function isNetworkPath (path) {
   return (path.startsWith('/n/'))
 }
