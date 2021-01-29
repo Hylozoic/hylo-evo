2eb19699df9c47e0bd034d884c05ed71578c585d
Loren Johnson <lorenjohnson@gmail.com>
2021-01-28 15:25:00 -1000

    Upgrade to node v15, etc (#752)
    * Upgrade to node v15
    * Upgrade node-sass to node 15 compat
    * Upgrade SASS modules
    * Webpack and Redux (base libraries) upgrades
    * Upgrade connected router, babel-graphql
    * Upgrade react-redux
    * Jest to v26
    * Upgrade to React Router 5.x
    * Fixes #770

--- config/postcss.config.js
+++ config/postcss.config.js
@@ -2,15 +2,6 @@ module.exports = {
   plugins: {
     'postcss-flexbugs-fixes': {},
     'postcss-preset-env': {
-      autoprefixer: {
-        flexbox: 'no-2009',
-        browsers: [
-          '>1%',
-          'last 4 versions',
-          'Firefox ESR',
-          'not ie < 9' // React doesn't support IE8 anyway
-        ]
-      },
       stage: 3
     }
   }
--- src/store/presenters/presentPost.test.js
+++ src/store/presenters/presentPost.test.js
@@ -15,6 +15,6 @@ describe('presentPost', () => {
   it('matches the snapshot', () => {
     const post = session.Post.withId(postId)
     const result = presentPost(post, communityId)
-    expect(result).toMatchSnapshot(true)
+    expect(result).toMatchSnapshot()
   })
 })
