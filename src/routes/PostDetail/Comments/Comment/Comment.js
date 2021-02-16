78eb25ef294a4bf34592955c527471a17d33d191
pospi <pospi@spadgos.com>
2021-02-16 12:05:35 +1000

    swap out comment tooltips for shared standard appearance

--- src/routes/PostDetail/Comments/Comment/Comment.js
+++ src/routes/PostDetail/Comments/Comment/Comment.js
@@ -2,11 +2,11 @@ import PropTypes from 'prop-types'
 import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
 import { Link } from 'react-router-dom'
-import ReactTooltip from 'react-tooltip'
 import { filter, isEmpty, isFunction } from 'lodash/fp'
 import { humanDate, present, sanitize } from 'hylo-utils/text'
 import { personUrl } from 'util/navigation'
 import ShowMore from '../ShowMore'
+import Tooltip from 'components/Tooltip'
 import Avatar from 'components/Avatar'
 import Dropdown from 'components/Dropdown'
 import Icon from 'components/Icon'
@@ -184,18 +184,7 @@ export default class CommentWithReplies extends Component {
           editorContent={this.state.prefillEditor}
           focusOnRender />
       </div>}
-      <ReactTooltip
-        id={`reply-tip-${comment.id}`}
-        effect='solid'
-        style='light'
-        border
-        // :TODO: de-duplicate these colour values
-        textColor='#2A4059'
-        borderColor='#40A1DD'
-        backgroundColor='white'
-        offset={{ 'top': -2 }}
-        delayShow={500}
-        styleName='actionsTooltip' />
+      <Tooltip id={`reply-tip-${comment.id}`} />
     </div>
   }
 }
