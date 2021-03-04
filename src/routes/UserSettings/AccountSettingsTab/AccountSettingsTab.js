04325aee090303554235b46fd6382240b8609fc0
Aaron Brodeur <aaronb@gmail.com>
2021-03-04 15:58:10 -0800

    Added sticky save button to personal settings. Added padding to full page modal center column so you can scroll content into view

--- src/routes/UserSettings/AccountSettingsTab/AccountSettingsTab.js
+++ src/routes/UserSettings/AccountSettingsTab/AccountSettingsTab.js
@@ -122,7 +122,10 @@ export default class AccountSettingsTab extends Component {
       <div styleName='help'>
         Passwords must be at least 9 characters long, and should be a mix of lower and upper case letters, numbers and symbols.
       </div>
-      <div styleName='button-row'>
+
+
+      <div styleName='saveChanges'>
+        <span styleName={canSave ? 'settingChanged' : ''}>{canSave ? 'Changes not saved' : 'Current settings up to date'}</span>
         <Button label='Save Changes' color={canSave ? 'green' : 'gray'} onClick={canSave ? this.save : null} styleName='save-button' />
       </div>
     </div>
