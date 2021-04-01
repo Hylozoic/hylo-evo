50d2b3181a992c57b0375a77857b4a6582c1c42c
Tibet Sprague <tibet.sprague@gmail.com>
2021-03-31 21:44:07 -0700

    Temporarily turn off inline replies, until it is on mobile too

--- src/routes/PostDetail/Comments/Comment/Comment.js
+++ src/routes/PostDetail/Comments/Comment/Comment.js
@@ -74,12 +74,12 @@ export class Comment extends Component {
             {editing && 'Editing now'}
             {!editing && humanDate(createdAt)}
           </span>
-          <div styleName='upperRight'>
+          {/* XXX: turn off replies for now until mobile has them <div styleName='upperRight'>
             <div styleName='commentAction' onClick={onReplyComment} data-tip='Reply' data-for={`reply-tip-${id}`}>
               <Icon name='Replies' />
             </div>
             {dropdownItems.length > 0 && <Dropdown styleName='dropdown' toggleChildren={<Icon name='More' />} items={dropdownItems} />}
-          </div>
+          </div>*/}
         </div>
         <CardImageAttachments attachments={attachments} linked styleName='images' />
         <CardFileAttachments attachments={attachments} styleName='files' />
