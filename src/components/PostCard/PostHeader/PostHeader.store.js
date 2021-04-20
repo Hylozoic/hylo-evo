9a044766213bdc6eee3fe8675f1802f47df85f2a
Tibet Sprague <tibet.sprague@gmail.com>
2021-04-20 11:15:40 -0700

    When deleting or removing a post from a group remove it from landing page

--- src/components/PostCard/PostHeader/PostHeader.store.js
+++ src/components/PostCard/PostHeader/PostHeader.store.js
@@ -14,7 +14,7 @@ export const FULFILL_POST = `${MODULE_NAME}/FULFILL_POST`
 export const FULFILL_POST_PENDING = `${MODULE_NAME}/FULFILL_POST_PENDING`
 
 // Action Creators
-export function deletePost (id) {
+export function deletePost (id, groupId) {
   return {
     type: DELETE_POST,
     graphql: {
@@ -29,7 +29,8 @@ export function deletePost (id) {
     },
     meta: {
       optimistic: true,
-      id
+      id,
+      groupId
     }
   }
 }
@@ -104,11 +105,32 @@ export const getGroup = ormCreateSelector(
   (session, { groupSlug }) => session.Group.safeGet({ slug: groupSlug })
 )
 
+// XXX: this is ugly, would be better to load these posts through redux-orm "queries" so they update automatically
+const removePostFromGroup = (post, group) => {
+  if (post && group) {
+    if (post.announcement) {
+      group.update({ announcements: group.announcements.filter(p => p.id !== post.id).toModelArray() })
+    }
+    if (post.type === 'request' || post.type === 'offer') {
+      group.update({ openOffersAndRequests: group.openOffersAndRequests.filter(p => p.id !== post.id).toModelArray() })
+    } else if (post.type === 'event') {
+      group.update({ upcomingEvents: group.upcomingEvents.filter(p => p.id !== post.id).toModelArray() })
+    } else if (post.type === 'project') {
+      group.update({ activeProjects: group.activeProjects.filter(p => p.id !== post.id).toModelArray() })
+    }
+  }
+}
+
 export function ormSessionReducer ({ Group, Post }, { type, meta }) {
   let post
   switch (type) {
     case DELETE_POST_PENDING:
-      Post.withId(meta.id).delete()
+      post = Post.withId(meta.id)
+      if (meta.groupId) {
+        const group = Group.withId(meta.groupId)
+        removePostFromGroup(post, group)
+      }
+      post.delete()
       break
 
     case REMOVE_POST_PENDING: {
@@ -116,19 +138,8 @@ export function ormSessionReducer ({ Group, Post }, { type, meta }) {
       const groups = post.groups.filter(c =>
         c.slug !== meta.slug).toModelArray()
       post.update({ groups })
-
-      // XXX: this is ugly, would be better to load these posts through redux-orm "queries" so they update automatically
       const group = Group.safeGet({ slug: meta.slug })
-      if (post.announcement) {
-        group.update({ announcements: group.announcements.filter(p => p.id !== post.id).toModelArray() })
-      }
-      if (post.type === 'request' || post.type === 'offer') {
-        group.update({ openOffersAndRequests: group.openOffersAndRequests.filter(p => p.id !== post.id).toModelArray() })
-      } else if (post.type === 'event') {
-        group.update({ upcomingEvents: group.upcomingEvents.filter(p => p.id !== post.id).toModelArray() })
-      } else if (post.type === 'project') {
-        group.update({ activeProjects: group.activeProjects.filter(p => p.id !== post.id).toModelArray() })
-      }
+      removePostFromGroup(post, group)
       break
     }
 
