f546a72cc57c5fd2000701028dc1b7dd864472e3
Clare Politano Hutchings <cpolitano8@gmail.com>
2021-04-21 11:02:48 -0700

    new home msg + network map centering

--- src/components/Widget/WelcomeWidget/WelcomeWidget.js
+++ src/components/Widget/WelcomeWidget/WelcomeWidget.js
@@ -15,7 +15,7 @@ export default class WelcomeWidget extends Component {
     return (
       <div styleName='welcome-widget'>
         <h2>{settings.title || `Welcome to ${group.name}!`}</h2>
-        <p>{settings.text || `We're thrilled to have you here`}</p>
+        <p>{settings.text || `We're happy you're here! Please take a moment to explore this page to see what's alive in our group. Introduce yourself by clicking Create to post a Discussion sharing who you are and what brings you to our group. And don't forget to fill out your profile - so likeminded new friends can connect with you!`}</p>
       </div>
     )
   }
