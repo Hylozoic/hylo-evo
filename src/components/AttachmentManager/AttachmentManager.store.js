5f022abf5de9b997fff0d11ef505bfa4b0ef2b0f
Tibet Sprague <tibet.sprague@gmail.com>
2021-05-11 13:48:06 -0700

    Fix editing a post deleting it's images
    Fixes https://github.com/Hylozoic/hylo-evo/issues/865

--- src/components/AttachmentManager/AttachmentManager.store.js
+++ src/components/AttachmentManager/AttachmentManager.store.js
@@ -80,14 +80,10 @@ export function getAttachments (state, {
 // the provided type and id pair
 export const getAttachmentsFromObject = ormCreateSelector(
   orm,
-  (state, _) => get('orm', state),
   (_, props) => props,
   ({ Attachment }, { id = ID_FOR_NEW, type, attachmentType }) => Attachment
     .all()
-    .filter(({ type: at, ...rest }) =>
-      at === attachmentType &&
-      rest[type.toLowerCase()] === id
-    )
+    .filter(({ type: at, ...rest }) => at === attachmentType && rest[type.toLowerCase()] === id)
     .orderBy('position')
     .toRefArray()
     .map(({ url }) => ({
