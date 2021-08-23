39f0a8324a67f36966b4c14c17de3d48f480e471
Tom Watson <thomasgabriel.watson@gmail.com>
2021-08-23 12:26:09 -0700

    Hylo.com links target _self

--- src/components/ClickCatcher/ClickCatcher.js
+++ src/components/ClickCatcher/ClickCatcher.js
@@ -1,5 +1,6 @@
 import PropTypes from 'prop-types'
 import React from 'react'
+import { hyloUrlRegex } from 'util/navigation'
 
 export default function ClickCatcher ({ tag, handleMouseOver, navigate, ...props }) {
   if (!['div', 'span', 'p'].includes(tag)) {
@@ -18,6 +19,13 @@ export default function ClickCatcher ({ tag, handleMouseOver, navigate, ...props
 
     if (node.getAttribute('target') !== '_blank') {
       node.setAttribute('target', '_blank')
+    }
+
+    const matches = [...node.getAttribute('href').matchAll(hyloUrlRegex)]
+    if (matches[0] && matches[0].length === 2) {
+      node.setAttribute('target', '_self')
+      const urlPath = matches[0][1] === '' ? '/' : matches[0][1]
+      node.setAttribute('href', urlPath)
     }
   }
   return React.createElement(tag, { ...props, onClick: handleClick })
