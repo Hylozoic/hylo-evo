c0969d75e235c479ff1c3b9ab47bca46ce10c02e
Tom Watson <thomasgabriel.watson@gmail.com>
2021-11-11 11:35:00 -0800

    Switch EventInviteDialog to hook-function component

--- src/components/EventInviteDialog/EventInviteDialog.js
+++ src/components/EventInviteDialog/EventInviteDialog.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, {useState, useEffect} from 'react'
 import cx from 'classnames'
 import { bgImageStyle } from 'util/index'
 import ModalDialog from 'components/ModalDialog'
@@ -8,95 +8,82 @@ import styles from './EventInviteDialog.scss'
 import { humanResponse } from 'store/models/EventInvitation'
 import TextInput from 'components/TextInput'
 
-export default class EventInviteDialog extends React.PureComponent {
-  state = {
-    invitedIds: [],
-    searchTerm: ''
-  }
+export default function EventInviteDialog ({
+  fetchPeople,
+  forGroups,
+  eventInvitations,
+  people,
+  eventId,
+  onClose,
+  invitePeopleToEvent
+}) {
+  const [invitedIds, setInvitedIds] = useState([])
+  const [searchTerm, setSearchTerm] = useState('')
 
-  toggleInvite = id => {
-    const { invitedIds } = this.state
+  const toggleInvite = id => (invitedIds.includes(id)) 
+    ? setInvitedIds(invitedIds.filter(invitedId => invitedId !== id))
+    : setInvitedIds(invitedIds.concat([id])) 
 
-    if (invitedIds.includes(id)) {
-      this.setState({
-        invitedIds: invitedIds.filter(invitedId => invitedId !== id)
-      })
-    } else {
-      this.setState({
-        invitedIds: invitedIds.concat([id])
-      })
-    }
-  }
+  const onSearchChange = ({ target: { value } }) => setSearchTerm(value)
 
-  onSearchChange = ({ target: { value } }) => {
-    const { fetchPeople, forGroups } = this.props
-    const forGroupIds = forGroups.map(c => c.id)
-    this.setState({ searchTerm: value })
-    fetchPeople(value, forGroupIds)
-  }
+  useEffect(() => {
+      const forGroupIds = forGroups.map(c => c.id)
+      fetchPeople(searchTerm, forGroupIds)
+  }, [searchTerm])
 
-  getFilteredInviteSuggestions = () => {
-    const { eventInvitations } = this.props
-    const { searchTerm } = this.state
-    return this.props.people.filter(person => {
+  const getFilteredInviteSuggestions = () => {
+    return people.filter(person => {
       return !eventInvitations.map(ei => ei.id).includes(person.id) &&
       person.name.toLowerCase().includes(searchTerm.toLowerCase())
     })
   }
 
-  submit = () => {
-    const { eventId, onClose } = this.props
-    const { invitedIds } = this.state
-    this.props.invitePeopleToEvent(eventId, invitedIds)
+  const submit = () => {
+    invitePeopleToEvent(eventId, invitedIds)
     onClose()
   }
 
-  render () {
-    const { invitedIds } = this.state
-    const { onClose, eventInvitations } = this.props
+  const filteredInviteSuggestions = getFilteredInviteSuggestions()
 
-    const filteredInviteSuggestions = this.getFilteredInviteSuggestions()
+  const inviteButtonLabel = invitedIds.length === 0
+    ? 'Select people to invite'
+    : invitedIds.length === 1
+      ? 'Invite 1 person'
+      : `Invite ${invitedIds.length} people`
 
-    const inviteButtonLabel = invitedIds.length === 0
-      ? 'Select people to invite'
-      : invitedIds.length === 1
-        ? 'Invite 1 person'
-        : `Invite ${invitedIds.length} people`
-
-    return <ModalDialog key='event-invite-dialog'
-      closeModal={onClose}
-      modalTitle={`Invite`}
-      showCancelButton={false}
-      showSubmitButton={false}
-      style={{ width: '100%', maxWidth: '620px' }}>
-      <div styleName='container'>
-        <Search onChange={this.onSearchChange} />
-        <div styleName='inviteSuggestions'>
-          {filteredInviteSuggestions.map(invitee => <InviteeRow
-            key={invitee.id}
-            person={invitee}
-            selected={invitedIds.includes(invitee.id)}
-            onClick={() => this.toggleInvite(invitee.id)}
-          />)}
-        </div>
-        <div styleName='alreadyInvitedLabel'>Already Invited</div>
-        <div styleName='alreadyInvited'>
-          {eventInvitations.map(eventInvitation =>
-            <InviteeRow
-              person={eventInvitation}
-              showResponse
-              key={eventInvitation.id}
-            />)}
-        </div>
-        <Button
-          small
-          styleName='inviteButton'
-          label={inviteButtonLabel}
-          onClick={this.submit}
-          disabled={invitedIds.length === 0} />
+  return <ModalDialog key='event-invite-dialog'
+    closeModal={onClose}
+    modalTitle={`Invite`}
+    showCancelButton={false}
+    showSubmitButton={false}
+    style={{ width: '100%', maxWidth: '620px' }}>
+    <div styleName='container'>
+      <Search onChange={onSearchChange} />
+      <div styleName='inviteSuggestions'>
+        {filteredInviteSuggestions.map(invitee => <InviteeRow
+          key={invitee.id}
+          person={invitee}
+          selected={invitedIds.includes(invitee.id)}
+          onClick={() => toggleInvite(invitee.id)}
+        />)}
       </div>
-    </ModalDialog>
-  }
+      <div styleName='alreadyInvitedLabel'>Already Invited</div>
+      <div styleName='alreadyInvited'>
+        {eventInvitations.map(eventInvitation =>
+          <InviteeRow
+            person={eventInvitation}
+            showResponse
+            key={eventInvitation.id}
+          />)}
+      </div>
+      <Button
+        small
+        styleName='inviteButton'
+        label={inviteButtonLabel}
+        onClick={submit}
+        disabled={invitedIds.length === 0} />
+    </div>
+  </ModalDialog>
 }
 
 export function InviteeRow ({ person, selected, showResponse, onClick }) {
@@ -118,15 +105,11 @@ export function InviteeRow ({ person, selected, showResponse, onClick }) {
   </div>
 }
 
-export class Search extends Component {
-  render () {
-    const { onChange } = this.props
-
-    return <div styleName='search'>
-      <TextInput theme={styles}
-        inputRef={x => x && x.focus()}
-        placeholder='Search members'
-        onChange={onChange} />
-    </div>
-  }
+export function Search ({ onChange }) {
+  return <div styleName='search'>
+    <TextInput theme={styles}
+      inputRef={x => x && x.focus()}
+      placeholder='Search members'
+      onChange={onChange} />
+  </div>
 }
