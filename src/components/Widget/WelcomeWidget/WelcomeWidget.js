550c6f5462029aa7cc9f57e8eaf47533871b8f5e
Tibet Sprague <tibet.sprague@gmail.com>
2021-12-15 11:39:57 -0800

    Fix issue where editing welcome widget message wouldn't update immediately Also make it so links work in the welcome message, and you can use markdown to style the text!
    Fixes https://github.com/Hylozoic/hylo-evo/issues/921

--- src/components/Widget/WelcomeWidget/WelcomeWidget.js
+++ src/components/Widget/WelcomeWidget/WelcomeWidget.js
@@ -1,3 +1,4 @@
+import { markdown } from 'hylo-utils/text'
 import PropTypes from 'prop-types'
 import React, { Component } from 'react'
 
@@ -12,10 +13,11 @@ export default class WelcomeWidget extends Component {
 
   render () {
     const { group, settings = {} } = this.props
+    const message = settings.text || `We're happy you're here! Please take a moment to explore this page to see what's alive in our group. Introduce yourself by clicking Create to post a Discussion sharing who you are and what brings you to our group. And don't forget to fill out your profile - so likeminded new friends can connect with you!`
     return (
       <div styleName='welcome-widget'>
         <h2>{settings.title || `Welcome to ${group.name}!`}</h2>
-        <p>{settings.text || `We're happy you're here! Please take a moment to explore this page to see what's alive in our group. Introduce yourself by clicking Create to post a Discussion sharing who you are and what brings you to our group. And don't forget to fill out your profile - so likeminded new friends can connect with you!`}</p>
+        <p dangerouslySetInnerHTML={{ __html: markdown(message) }} />
       </div>
     )
   }
