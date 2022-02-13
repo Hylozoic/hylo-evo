05de75f4b037779583affca447947b7ad723600d
Loren Johnson <lorenjohnson@gmail.com>
2022-02-13 13:21:43 -0800

    Remove test related to client-side santization

--- src/routes/PostDetail/Comments/Comment/Comment.test.js
+++ src/routes/PostDetail/Comments/Comment/Comment.test.js
@@ -49,17 +49,6 @@ describe('Comment', () => {
     expect(wrapper).toMatchSnapshot()
   })
 
-  it('sanitizes text', () => {
-    const comment = {
-      ...props.comment,
-      text: '<p>Nice text<script>a sneaky script</script></p>'
-    }
-    const wrapper = shallow(<Comment {...props} comment={comment} />)
-    expect(wrapper.find('div #text').prop('dangerouslySetInnerHTML')).toEqual({
-      __html: '<p>Nice text</p>'
-    })
-  })
-
   it('displays the delete menu when deleteComment is defined', () => {
     const wrapper = shallow(<Comment {...props} deleteComment={() => {}} />)
     expect(wrapper).toMatchSnapshot()
