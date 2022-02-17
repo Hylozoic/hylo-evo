3dafe9d2653819b1b1e74135016efa1b9b00ae57
Tom Watson <thomasgabriel.watson@gmail.com>
2022-02-17 08:35:55 -0800

    Added integration tests for GroupSearch

--- src/util/reactTestingLibraryExtended.js
+++ src/util/reactTestingLibraryExtended.js
@@ -1,17 +1,28 @@
 import React from 'react'
 import { render } from '@testing-library/react'
 import { MemoryRouter } from 'react-router'
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+import rootReducer from '../store/reducers'
+import createMiddleware from '../store/middleware'
+import '@testing-library/jest-dom/extend-expect' // Added many additional test helpers https://github.com/testing-library/jest-dom
 
-const AllTheProviders = ({ children }) => {
+export function generateStore (history, initialState) {
+  return createStore(rootReducer, initialState, createMiddleware(history))
+}
+
+export const AllTheProviders = (store) => ({ children }) => {
   return (
-    <MemoryRouter>
-      {children}
-    </MemoryRouter>
+    <Provider store={store || createStore(() => {})}>
+      <MemoryRouter>
+        {children}
+      </MemoryRouter>
+    </Provider>
   )
 }
 
-const customRender = (ui, options) =>
-  render(ui, { wrapper: AllTheProviders, ...options })
+const customRender = (ui, options, providersFunc) =>
+  render(ui, { wrapper: providersFunc, ...options })
 
 // re-export everything
 export * from '@testing-library/react'
