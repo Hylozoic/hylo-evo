73b4b4834945d7048e1e9c5b49743f2a49d792df
Tom Watson <thomasgabriel.watson@gmail.com>
2022-05-11 17:11:13 -0700

    Add url sanitization to at a glance links

--- src/components/FancyLink/FancyLink.js
+++ src/components/FancyLink/FancyLink.js
@@ -2,9 +2,9 @@ import React from 'react'
 import Icon from 'components/Icon'
 import './FancyLink.scss'
 
-export default function FancyLink ({ linkUrl, title = '', description = '', iconName }) {
+export default function FancyLink ({ linkUrl, title = '', description = '', iconName, target = null }) {
   return (
-    <a href={linkUrl} styleName='fancy-link-container'>
+    <a href={linkUrl} target={target} styleName='fancy-link-container'>
       <Icon styleName='icon' name={iconName} />
       <div styleName='text-container'>
         <h4>{title}</h4>
--- src/components/Widget/AtAGlanceWidget/AtAGlanceWidget.js
+++ src/components/Widget/AtAGlanceWidget/AtAGlanceWidget.js
@@ -4,6 +4,7 @@ import FancyLink from 'components/FancyLink'
 import { getSocialMedia, getWebsite, getAtAGlance } from 'store/selectors/farmExtensionSelectors'
 import { FarmDetailSection } from '../FarmDetailsWidget/FarmDetailsWidget'
 import './AtAGlanceWidget.scss'
+import { sanitizeURL } from 'util/url'
 
 export default function AtAGlanceWidget ({ group }) {
   /*
@@ -11,15 +12,15 @@ export default function AtAGlanceWidget ({ group }) {
     - website
     - social media
   */
-  const socialMedia = getSocialMedia(group)
-  const website = getWebsite(group)
+  const socialMedia = sanitizeURL(getSocialMedia(group))
+  const website = sanitizeURL(getWebsite(group))
   const atAGlance = getAtAGlance(group)
 
   return (
     <div styleName='at-a-glance-container'>
       {atAGlance.length > 0 && <FarmDetailSection items={atAGlance.map((item) => capitalize(item))} />}
-      {website && <FancyLink iconName='Website' linkUrl={website} title={`Website for ${group.name}`} />}
-      {socialMedia && <FancyLink iconName='Socials' linkUrl={socialMedia} title={`Social media link for ${group.name}`} />}
+      {website && <FancyLink iconName='Website' target='_blank' linkUrl={`https://${website}`} title={`Website for ${group.name}`} />}
+      {socialMedia && <FancyLink iconName='Socials' target='_blank' linkUrl={`https://${socialMedia}`} title={`Social media link for ${group.name}`} />}
     </div>
   )
 }
