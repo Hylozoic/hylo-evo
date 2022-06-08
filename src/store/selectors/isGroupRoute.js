ad6178fe3fbcba1c061d436c23a86e5d5271c1ea
Tom Watson <thomasgabriel.watson@gmail.com>
2022-06-08 16:47:58 -0700

    Ensure the correct version of a groups data is selected when there is both a groupSlug and a detailSlug

--- src/store/selectors/isGroupRoute.js
+++ src/store/selectors/isGroupRoute.js
@@ -9,5 +9,5 @@ const isGroupRoute = createSelector(
 export default isGroupRoute
 
 export function getSlugFromLocation (state, props) {
-  return getRouteParam('groupSlug', state, props, false) || getRouteParam('detailGroupSlug', state, props, false) || props.groupSlug
+  return getRouteParam('detailGroupSlug', state, props, false) || getRouteParam('groupSlug', state, props, false) || props.groupSlug
 }
