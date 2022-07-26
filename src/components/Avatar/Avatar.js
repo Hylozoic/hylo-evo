7818691998d09bfc678aa20c329c9d6693f0808e
Loren Johnson <lorenjohnson@gmail.com>
2022-07-26 09:36:36 +0200

    Minor refactors to remove console warnings

--- src/components/Avatar/Avatar.js
+++ src/components/Avatar/Avatar.js
@@ -3,18 +3,24 @@ import React from 'react'
 import RoundImage from '../RoundImage'
 import { Link } from 'react-router-dom'
 
-const { string, bool } = PropTypes
-
 export default function Avatar ({ url, avatarUrl, tiny, small, medium, className }) {
-  return url ? <Link to={url} className={className}>
-    <RoundImage url={avatarUrl} small={small} medium={medium} tiny={tiny} />
-  </Link>
-    : <span className={className}><RoundImage url={avatarUrl} small={small} medium={medium} tiny={tiny} /></span>
+  if (url) {
+    return (
+      <Link to={url} className={className}>
+        <RoundImage url={avatarUrl} small={small} medium={medium} tiny={tiny} />
+      </Link>
+    )
+  } else {
+    return (
+      <span className={className}><RoundImage url={avatarUrl} small={small} medium={medium} tiny={tiny} /></span>
+    )
+  }
 }
+
 Avatar.propTypes = {
-  url: string.isRequired,
-  avatarUrl: string.isRequired,
-  small: bool,
-  medium: bool,
-  className: string
+  url: PropTypes.string,
+  avatarUrl: PropTypes.string,
+  small: PropTypes.bool,
+  medium: PropTypes.bool,
+  className: PropTypes.string
 }
