e20456fbabd05e746346176bc04145ff9e239ad4
Loren Johnson <lorenjohnson@gmail.com>
2022-10-04 09:40:33 -0700

    Fix test

--- src/components/FeedList/TabBar/TabBar.test.js
+++ src/components/FeedList/TabBar/TabBar.test.js
@@ -1,7 +1,5 @@
 import { shallow } from 'enzyme'
 import React from 'react'
-import { render } from 'util/testing/reactTestingLibraryExtended'
-
 import TabBar from './TabBar'
 
 it('renders sort options', () => {
