2e03db18909b6d597d81a6a1329f0258a1cb5d1b
Loren Johnson <lorenjohnson@gmail.com>
2022-10-10 15:53:14 -0700

    Remove commented test code. Add note

--- src/components/HyloTipTapEditor/HyloTipTapRender.js
+++ src/components/HyloTipTapEditor/HyloTipTapRender.js
@@ -7,6 +7,8 @@ import PeopleMentions from './extensions/PeopleMentions'
 import Legacy from './extensions/Legacy'
 import Link from '@tiptap/extension-link'
 
+// TipTap can be used to render HTML with low overhead, and this is mostly what that would look like.
+// This is experimental and not used. Can be removed if proves not useful or informative.
 export default function HyloTipTapRender ({ className, contentHTML }) {
   const editor = useEditor({
     content: contentHTML,
