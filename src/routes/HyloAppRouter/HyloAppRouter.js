0596544e53f9cea4a82f297a823412548cc53fc6
Loren Johnson <lorenjohnson@gmail.com>
2022-10-20 00:48:16 -0700

    Revert "Move HyloMobileEditor into a code splitting block"
    This reverts commit fb20608cb1c598be0352463da557b8299cc0f18c.

--- src/routes/HyloAppRouter/HyloAppRouter.js
+++ src/routes/HyloAppRouter/HyloAppRouter.js
@@ -1,19 +1,16 @@
-import React, { Suspense } from 'react'
+import React from 'react'
 import { Route, Switch } from 'react-router-dom'
-
-const HyloEditorMobile = React.lazy(() => import('components/HyloEditor/HyloEditorMobile'))
+import HyloEditorMobile from 'components/HyloEditor/HyloEditorMobile'
 
 export default function HyloAppRouter () {
   return (
-    <Suspense fallback={<div>Loading...</div>}>
-      <Switch>
-        <Route
-          path='/hyloApp/editor'
-          component={routeProps => (
-            <HyloEditorMobile {...routeProps} />
-          )}
-        />
-      </Switch>
-    </Suspense>
+    <Switch>
+      <Route
+        path='/hyloApp/editor'
+        component={routeProps => (
+          <HyloEditorMobile {...routeProps} />
+        )}
+      />
+    </Switch>
   )
 }
