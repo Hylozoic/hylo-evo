fb20608cb1c598be0352463da557b8299cc0f18c
Loren Johnson <lorenjohnson@gmail.com>
2022-10-20 00:14:07 -0700

    Move HyloMobileEditor into a code splitting block

--- src/routes/HyloAppRouter/HyloAppRouter.js
+++ src/routes/HyloAppRouter/HyloAppRouter.js
@@ -1,16 +1,19 @@
-import React from 'react'
+import React, { Suspense } from 'react'
 import { Route, Switch } from 'react-router-dom'
-import HyloEditorMobile from 'components/HyloEditor/HyloEditorMobile'
+
+const HyloEditorMobile = React.lazy(() => import('components/HyloEditor/HyloEditorMobile'))
 
 export default function HyloAppRouter () {
   return (
-    <Switch>
-      <Route
-        path='/hyloApp/editor'
-        component={routeProps => (
-          <HyloEditorMobile {...routeProps} />
-        )}
-      />
-    </Switch>
+    <Suspense fallback={<div>Loading...</div>}>
+      <Switch>
+        <Route
+          path='/hyloApp/editor'
+          component={routeProps => (
+            <HyloEditorMobile {...routeProps} />
+          )}
+        />
+      </Switch>
+    </Suspense>
   )
 }
