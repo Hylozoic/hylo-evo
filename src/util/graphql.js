3596b85091dd587a7e67de55fd5c274aadb4341b
Loren Johnson <lorenjohnson@gmail.com>
2022-10-31 09:02:22 -0700

    Removes HyloEditor dependency on store for Mobile use

--- src/util/graphql.js
+++ src/util/graphql.js
@@ -1,5 +1,8 @@
+import fetch from 'isomorphic-fetch'
 import gql from 'graphql-tag'
 import { print } from 'graphql/language/printer'
+
+export const HYLO_GRAPHQL_ENDPOINT_PATH = '/noo/graphql'
 
 export function stringToGraphql (graphqlString) {
   return (typeof graphqlString === 'string' || graphqlString instanceof String)
@@ -12,4 +15,37 @@ export function graphqlToString (unknownGraphql) {
   return (typeof unknownGraphql === 'object' && !(unknownGraphql instanceof String))
     ? print(unknownGraphql)
     : unknownGraphql
+}
+
+export function getHyloAPIEndpointURL () {
+  return typeof window === 'undefined'
+    ? `${process.env.API_HOST}${HYLO_GRAPHQL_ENDPOINT_PATH}`
+    : `${window.location.origin}${HYLO_GRAPHQL_ENDPOINT_PATH}`
+}
+
+// For directly querying our API outside of the Redux store.
+// Currently only used in the WebView HyloEditor
+export async function queryHyloAPI ({ query: unknownGraphql, variables }) {
+  const params = { query: graphqlToString(unknownGraphql), variables }
+  const response = await fetch(getHyloAPIEndpointURL(), {
+    body: JSON.stringify(params),
+    credentials: 'same-origin',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    },
+    method: 'POST'
+  })
+
+  if (response.status === 200) {
+    return response.json()
+  } else {
+    const { status, statusText, url } = response
+    const body = await response.text()
+    const error = new Error(body)
+
+    error.response = { status, statusText, url, body }
+
+    throw error
+  }
 }
