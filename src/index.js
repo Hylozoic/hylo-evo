2b86996de926451a8a3ad638b5a8ccf3e0fcc899
Loren Johnson <lorenjohnson@gmail.com>
2022-10-31 09:24:46 -0700

    Adds an isolated Feature / ReactPlayer route for HyloApp WebView use

--- src/index.js
+++ src/index.js
@@ -2,23 +2,41 @@ import React, { Suspense } from 'react'
 import ReactDOM from 'react-dom'
 import { rootDomId } from 'client/util'
 import Loading from 'components/Loading'
-
 import './client/websockets'
 import './css/global/index.scss'
 
 const App = React.lazy(() => import('./router'))
 const HyloEditorMobile = React.lazy(() => import('components/HyloEditor/HyloEditorMobile'))
+const Feature = React.lazy(() => import('components/PostCard/Feature'))
 
-ReactDOM.render(
-  window.location.pathname === '/hyloApp/editor'
-    ? (
-      <Suspense fallback={() => null}>
-        <HyloEditorMobile />
-      </Suspense>
-    ) : (
-      <Suspense fallback={() => <Loading type='fullscreen' />}>
-        <App />
-      </Suspense>
-    ),
-  document.getElementById(rootDomId)
-)
+const renderRoot = () => {
+  switch (window.location.pathname) {
+    case '/hyloApp/editor': {
+      return (
+        <Suspense fallback={() => null}>
+          <HyloEditorMobile />
+        </Suspense>
+      )
+    }
+
+    case '/hyloApp/videoPlayer': {
+      const querystringParams = new URLSearchParams(window.location.search)
+
+      return (
+        <Suspense fallback={() => null}>
+          <Feature url={querystringParams.get('url')} />
+        </Suspense>
+      )
+    }
+
+    default: {
+      return (
+        <Suspense fallback={() => <Loading type='fullscreen' />}>
+          <App />
+        </Suspense>
+      )
+    }
+  }
+}
+
+ReactDOM.render(renderRoot(), document.getElementById(rootDomId))
