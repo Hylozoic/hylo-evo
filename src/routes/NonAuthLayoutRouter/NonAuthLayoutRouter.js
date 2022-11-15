65db001b692d3ec69b35973e698c58b6eeac66ca
Tibet Sprague <tibet.sprague@gmail.com>
2022-11-15 15:46:59 -0800

    Use https links

--- src/routes/NonAuthLayoutRouter/NonAuthLayoutRouter.js
+++ src/routes/NonAuthLayoutRouter/NonAuthLayoutRouter.js
@@ -162,8 +162,8 @@ export default function NonAuthLayoutRouter (props) {
           />
         </Switch>
         <div styleName='below-container'>
-          <a href='http://hylo.com/terms/' target='_blank' rel='noreferrer'>Terms of Service</a> +&nbsp;
-          <a href='http://hylo.com/terms/privacy' target='_blank' rel='noreferrer'>Privacy Policy</a>
+          <a href='https://hylo.com/terms/' target='_blank' rel='noreferrer'>Terms of Service</a> +&nbsp;
+          <a href='https://hylo.com/terms/privacy' target='_blank' rel='noreferrer'>Privacy Policy</a>
         </div>
       </div>
       <HyloCookieConsent />
