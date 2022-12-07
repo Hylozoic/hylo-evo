6f26f97df3512edca4d3a4b0a86e829e50a3940e
Sofia Acosta <sofiaacostarascon@gmail.com>
2022-12-07 16:02:48 -0600

    additional translations

--- src/components/Widget/Widget.js
+++ src/components/Widget/Widget.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import { useTranslation } from 'react-i18next'
 import Icon from 'components/Icon'
 import AtAGlanceWidget from 'components/Widget/AtAGlanceWidget'
 import AnnouncementWidget from 'components/Widget/AnnouncementWidget'
@@ -28,103 +29,113 @@ import getMe from 'store/selectors/getMe'
 import { updateWidget } from './Widget.store'
 import useRouter from 'hooks/useRouter'
 
-const WIDGETS = {
-  text_block: {
-    title: '',
-    moderatorTitle: 'Welcome Message',
-    component: WelcomeWidget
-  },
-  announcements: {
-    title: 'Announcements',
-    component: AnnouncementWidget
-  },
-  active_members: {
-    title: 'Recently Active Members',
-    component: MembersWidget
-  },
-  requests_offers: {
-    title: 'Open Requests & Offers',
-    component: OffersAndRequestsWidget
-  },
-  posts: {
-    title: 'Recent Posts',
-    component: RecentPostsWidget
-  },
-  community_topics: {
-    title: 'Community Topics',
-    component: GroupTopicsWidget
-  },
-  events: {
-    title: 'Upcoming Events',
-    component: EventsWidget
-  },
-  project_activity: {
-    title: 'Recently Active Projects',
-    component: ProjectsWidget
-  },
-  group_affiliations: {
-    title: 'Subgroups',
-    component: GroupsWidget
-  },
-  relevant_project_activity: {
-    title: 'Recently Active Projects',
-    component: ProjectsWidget
-  },
-  relevant_groups: {
-    title: 'Nearby Relevant Groups', // TODO: ensure there is a way to customize/overwrite this
-    component: GroupsWidget
-  },
-  relevant_events: {
-    title: 'Nearby Relevant Events', // TODO: ensure there is a way to customize/overwrite this
-    component: EventsWidget
-  },
-  relevant_requests_offers: {
-    title: 'Nearby Relevant Offers and Requests', // TODO: ensure there is a way to customize/overwrite this
-    component: OffersAndRequestsWidget
-  },
-  farm_at_a_glance: {
-    title: 'At A Glance',
-    component: AtAGlanceWidget
-  },
-  farm_details: {
-    title: 'Farm Details',
-    component: FarmDetailsWidget
-  },
-  farm_open_to_public: {
-    title: 'Location & Hours',
-    component: FarmOpenToPublic
-  },
-  farm_map: {
-    title: 'Farm Surrounds & Posts',
-    component: FarmMapWidget
-  },
-  moderators: {
-    title: 'Moderators', // TODO: ensure there is a way to customize/overwrite this
-    component: ModeratorsWidget
-  },
-  opportunities_to_collaborate: {
-    title: 'Opportunities to Collaborate',
-    component: OpportunitiesToCollaborateWidget
-  },
-  privacy_settings: {
-    title: 'Privacy',
-    component: PrivacyWidget
-  },
-  mission: {
-    title: null,
-    component: RichTextWidget
-  },
-  join: {
-    title: null,
-    component: JoinWidget
-  },
-  topics: {
-    title: null,
-    component: TopicsWidget
-  }
-}
-
 export default function Widget (props) {
+  const { t } = useTranslation()
+  const WIDGETS = {
+    text_block: {
+      title: '',
+      moderatorTitle: t('Welcome Message'),
+      component: WelcomeWidget
+    },
+    announcements: {
+      title: t('Announcements'),
+      component: AnnouncementWidget
+    },
+    active_members: {
+      title: t('Recently Active Members'),
+      component: MembersWidget
+    },
+    requests_offers: {
+      title: t('Open Requests & Offers'),
+      component: OffersAndRequestsWidget
+    },
+    posts: {
+      title: t('Recent Posts'),
+      component: RecentPostsWidget
+    },
+    community_topics: {
+      title: t('Community Topics'),
+      component: GroupTopicsWidget
+    },
+    events: {
+      title: t('Upcoming Events'),
+      component: EventsWidget
+    },
+    project_activity: {
+      title: t('Recently Active Projects'),
+      component: ProjectsWidget
+    },
+    group_affiliations: {
+      title: t('Subgroups'),
+      component: GroupsWidget
+    },
+    relevant_project_activity: {
+      title: t('Recently Active Projects'),
+      component: ProjectsWidget
+    },
+    relevant_groups: {
+      title: t('Nearby Relevant Groups'), // TODO: ensure there is a way to customize/overwrite this
+      component: GroupsWidget
+    },
+    relevant_events: {
+      title: t('Nearby Relevant Events'), // TODO: ensure there is a way to customize/overwrite this
+      component: EventsWidget
+    },
+    relevant_requests_offers: {
+      title: t('Nearby Relevant Offers and Requests'), // TODO: ensure there is a way to customize/overwrite this
+      component: OffersAndRequestsWidget
+    },
+    farm_at_a_glance: {
+      title: t('At A Glance'),
+      component: AtAGlanceWidget
+    },
+    farm_details: {
+      title: t('Farm Details'),
+      component: FarmDetailsWidget
+    },
+    farm_open_to_public: {
+      title: t('Location & Hours'),
+      component: FarmOpenToPublic
+    },
+    farm_map: {
+      title: t('Farm Surrounds & Posts'),
+      component: FarmMapWidget
+    },
+    moderators: {
+      title: t('Moderators'), // TODO: ensure there is a way to customize/overwrite this
+      component: ModeratorsWidget
+    },
+    opportunities_to_collaborate: {
+      title: t('Opportunities to Collaborate'),
+      component: OpportunitiesToCollaborateWidget
+    },
+    privacy_settings: {
+      title: t('Privacy'),
+      component: PrivacyWidget
+    },
+    mission: {
+      title: null,
+      component: RichTextWidget
+    },
+    join: {
+      title: null,
+      component: JoinWidget
+    },
+    topics: {
+      title: null,
+      component: TopicsWidget
+    }
+  }
+  const HiddenWidget = ({ name }) => {
+    const { t } = useTranslation()
+    return (
+      <div styleName='hidden-description'>
+        <h4><Icon name='Hidden' styleName='hidden-icon' /> {t('Hidden')}</h4>
+        <p>{t('The {{title}} section is not visible to members of this group. Click the three dots', { title: WIDGETS[name].moderatorTitle || WIDGETS[name].title })} (<Icon name='More' styleName='more-icon' />) {t('above this box to change the visibility settings. Only moderators can see this message')}.</p>
+      </div>
+    )
+  }
+
   const dispatch = useDispatch()
   const { childGroups, id, isModerator, isVisible, name, posts, isMember, settings = {} } = props
   const router = useRouter()
@@ -154,14 +165,14 @@ export default function Widget (props) {
           <div styleName={`edit-menu ${isMenuOpen ? 'visible' : ''}`}>
             {!isEditingSettings && <div styleName='edit-section'>
               <span styleName='triangle'>&nbsp;</span>
-              {name === 'text_block' && <div styleName='edit-settings'><span onClick={() => setIsEditingSettings(!isEditingSettings)}><Icon name='Edit' /> Edit welcome message</span></div>}
+              {name === 'text_block' && <div styleName='edit-settings'><span onClick={() => setIsEditingSettings(!isEditingSettings)}><Icon name='Edit' /> {t('Edit welcome message')}</span></div>}
               <div styleName='visibility-settings'>
                 <VisibilityToggle
                   id={id}
                   checked={isVisible}
                   onChange={() => handleUpdateWidget(id, { isVisible: !isVisible })}
                   styleName='widget-visibility'
-                  backgroundColor={isVisible ? 'gray' : 'black'} /> <span styleName='visibility-label'>Visibility:</span> {isVisible ? 'Visible' : 'Hidden'}
+                  backgroundColor={isVisible ? 'gray' : 'black'} /> <span styleName='visibility-label'>{t('Visibility')}:</span> {isVisible ? t('Visible') : t('Hidden')}
               </div>
             </div>}
           </div>
@@ -185,16 +196,8 @@ export default function Widget (props) {
   )
 }
 
-const HiddenWidget = ({ isVisible, name }) => {
-  return (
-    <div styleName='hidden-description'>
-      <h4><Icon name='Hidden' styleName='hidden-icon' /> Hidden</h4>
-      <p>The {WIDGETS[name].moderatorTitle || WIDGETS[name].title} section is not visible to members of this group. Click the three dots (<Icon name='More' styleName='more-icon' />) above this box to change the visibility settings. Only moderators can see this message.</p>
-    </div>
-  )
-}
-
 const EditForm = ({ id, setIsEditingSettings, setIsMenuOpen, newSettings, updateSettings, save }) => {
+  const { t } = useTranslation()
   return (
     <div styleName='edit-form'>
       <div>
@@ -202,7 +205,7 @@ const EditForm = ({ id, setIsEditingSettings, setIsMenuOpen, newSettings, update
           type='text'
           autoFocus
           onChange={e => updateSettings({ ...newSettings, title: e.target.value.substring(0, 50) })}
-          placeholder='Enter a title'
+          placeholder={t('Enter a title')}
           value={newSettings.title}
         />
         <div styleName='chars'>{(newSettings.title && newSettings.title.length) || 0}/{50}</div>
@@ -212,7 +215,7 @@ const EditForm = ({ id, setIsEditingSettings, setIsMenuOpen, newSettings, update
         <textarea
           type='text'
           onChange={e => updateSettings({ ...newSettings, text: e.target.value.substring(0, 500) })}
-          placeholder='Enter your message here'
+          placeholder={t('Enter your message here')}
           value={newSettings.text}
         />
         <div styleName='chars'>{(newSettings.text && newSettings.text.length) || 0}/{500}</div>
@@ -226,9 +229,9 @@ const EditForm = ({ id, setIsEditingSettings, setIsMenuOpen, newSettings, update
             setIsMenuOpen(true)
           }}
         >
-          Cancel
+          {t('Cancel')}
         </span>
-        <span styleName='save' onClick={() => { save(id, { settings: newSettings }); setIsEditingSettings(false); setIsMenuOpen(false) }}>Save</span>
+        <span styleName='save' onClick={() => { save(id, { settings: newSettings }); setIsEditingSettings(false); setIsMenuOpen(false) }}>{t('Save')}</span>
       </div>
 
     </div>
