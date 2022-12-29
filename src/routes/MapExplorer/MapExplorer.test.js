7457cbda78b38343006fcee5321af1fa17a2c5a5
Sofia Acosta <sofiaacostarascon@gmail.com>
2022-12-29 00:01:02 -0600

    update test

--- src/routes/MapExplorer/MapExplorer.test.js
+++ src/routes/MapExplorer/MapExplorer.test.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { MemoryRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 import { shallow } from 'enzyme'
 import MapExplorer from './MapExplorer'
 
