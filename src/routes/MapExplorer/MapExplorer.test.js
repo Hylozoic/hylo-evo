15310aa445864ba2c6925e1fede9177f9b0f1995
Sofia Acosta <sofiaacostarascon@gmail.com>
2022-12-29 00:01:02 -0600

    update test

--- src/routes/MapExplorer/MapExplorer.test.js
+++ src/routes/MapExplorer/MapExplorer.test.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { MemoryRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 import { shallow } from 'enzyme'
 import MapExplorer from './MapExplorer'
 
@@ -13,33 +13,41 @@ jest.mock('react-i18next', () => ({
 
 describe('MapExplorer', () => {
   it('Matches Snapshot', () => {
-    const wrapper = shallow(<MemoryRouter><MapExplorer
-      centerLocation={{ lat: 35.442845, lng: 7.916598 }}
-      fetchPosts={jest.fn()}
-      fetchSavedSearches={jest.fn()}
-      filters={{ featureTypes: { request: true, offer: true } }}
-      groups={[]}
-      hideDrawer={false}
-      match={{ params: {} }}
-      members={[]}
-      postsForDrawer={[]}
-      postsForMap={[]}
-      routeParams={{}}
-      storeFetchPostsParam={jest.fn()}
-      topics={[]}
-      zoom={0}
-    /></MemoryRouter>)
+    const wrapper = shallow(
+      <BrowserRouter>
+        <MapExplorer
+          centerLocation={{ lat: 35.442845, lng: 7.916598 }}
+          fetchPosts={jest.fn()}
+          fetchSavedSearches={jest.fn()}
+          filters={{ featureTypes: { request: true, offer: true } }}
+          groups={[]}
+          hideDrawer={false}
+          match={{ params: {} }}
+          members={[]}
+          postsForDrawer={[]}
+          postsForMap={[]}
+          routeParams={{}}
+          storeFetchPostsParam={jest.fn()}
+          topics={[]}
+          zoom={0}
+        />
+      </BrowserRouter>
+    )
     expect(wrapper).toMatchSnapshot()
   })
 
   it('has a TabBar', () => {
-    const wrapper = shallow(<MemoryRouter><MapExplorer
-      fetchPosts={jest.fn()}
-      fetchSavedSearches={jest.fn()}
-      filters={{ featureTypes: { request: true, offer: true } }}
-      match={{ params: {} }}
-      storeFetchPostsParam={jest.fn()}
-    /></MemoryRouter>)
+    const wrapper = shallow(
+      <BrowserRouter>
+        <MapExplorer
+          fetchPosts={jest.fn()}
+          fetchSavedSearches={jest.fn()}
+          filters={{ featureTypes: { request: true, offer: true } }}
+          match={{ params: {} }}
+          storeFetchPostsParam={jest.fn()}
+        />
+      </BrowserRouter>
+    )
     expect(wrapper.find('TabBar')).toBeTruthy()
   })
 })
