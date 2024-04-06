a326b41bba79bbb761da99b83bcec96fbceb4fe7
Kevin Triplett <kt@kevintriplett.com>
2024-04-05 21:25:37 -0500

    1585 add duplicate post option w/tests

--- src/store/selectors/getPost.test.js
+++ src/store/selectors/getPost.test.js
@@ -1,9 +1,36 @@
+import { Model } from 'redux-orm'
 import orm from 'store/models'
 import getPost from 'store/selectors/getPost'
 
-describe('getPost', () => {
-  it("returns null if post doesn't exist", () => {
-    const session = orm.session(orm.getEmptyState())
+let session = null
+
+beforeAll(() => {
+  session = orm.session(orm.getEmptyState())
+})
+
+describe('getPost for non-existant post', () => {
+  it('returns null', () => {
     expect(getPost({ orm: session.state }, { match: { params: { postId: '1' } } })).toEqual(null)
+  })
+})
+
+describe('getPost for existing post', () => {
+  it('returns post for editing (and ignores duplication param)', () => {
+    const props = {
+      match: { params: { postId: '2' } }
+    }
+    Model.withId = jest.fn(id => id)
+    expect(getPost({ orm: session.state }, props)).toBe('2')
+    expect(Model.withId.mock.results[0].value).toBe('2')
+  })
+
+  it('returns post for duplicating', () => {
+    const props = {
+      match: { params: {} },
+      location: { search: '?fromPostId=3' }
+    }
+    Model.withId = jest.fn(id => id)
+    expect(getPost({ orm: session.state }, props)).toBe('3')
+    expect(Model.withId.mock.results[0].value).toBe('3')
   })
 })
