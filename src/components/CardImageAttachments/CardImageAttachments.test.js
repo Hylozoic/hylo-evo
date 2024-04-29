e547275d6058d807fc77a8499ada43036154ad8b
Kevin Triplett <kt@kevintriplett.com>
2024-04-28 19:15:29 -0600

    add test for no attached images case

--- src/components/CardImageAttachments/CardImageAttachments.test.js
+++ src/components/CardImageAttachments/CardImageAttachments.test.js
@@ -2,6 +2,14 @@ import React from 'react'
 import CardImageAttachments from './CardImageAttachments'
 import { shallow } from 'enzyme'
 
+it('renders no images', () => {
+  expect(shallow(<CardImageAttachments attachments={[
+    { url: 'bonkerz', type: 'file' },
+    { url: 'bonkers', type: 'file' },
+    { url: 'bonkerzztop', type: 'file' }
+  ]} />)).toEqual({})
+})
+
 it('renders a single image', () => {
   expect(shallow(<CardImageAttachments attachments={[
     { url: 'foo', type: 'image' },
