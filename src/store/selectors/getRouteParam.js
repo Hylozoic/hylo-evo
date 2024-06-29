f1b392c00f874fc2794e37f5979be010b74c4142
Kevin Triplett <kt@kevintriplett.com>
2024-06-29 08:45:52 -0500

    remove curry from getRouteParam

--- src/store/selectors/getRouteParam.js
+++ src/store/selectors/getRouteParam.js
@@ -1,8 +1,8 @@
-import { curry, get } from 'lodash/fp'
+import { get } from 'lodash/fp'
 
-const getRouteParam = curry((key, state, props, warn = true) => {
+const getRouteParam = (key, state, props, warn = true) => {
   if (warn && !props.match) console.warn(`getRouteParam('${key}') missing props.match`)
   return get(['match', 'params', key], props)
-})
+}
 
 export default getRouteParam
