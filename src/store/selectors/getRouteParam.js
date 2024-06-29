4a2dc591df13af945add96b432b2006a9ffaef48
Kevin Triplett <kt@kevintriplett.com>
2024-06-29 09:07:32 -0500

    remove get from getRouteParam

--- src/store/selectors/getRouteParam.js
+++ src/store/selectors/getRouteParam.js
@@ -1,8 +1,6 @@
-import { get } from 'lodash/fp'
-
 const getRouteParam = (key, state, props, warn = true) => {
   if (warn && !props.match) console.warn(`getRouteParam('${key}') missing props.match`)
-  return get(['match', 'params', key], props)
+  return props?.match?.params?.[key]
 }
 
 export default getRouteParam
