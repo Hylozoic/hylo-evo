d50aef2bc8f86819b7e6d2ddf590b9a009881634
Tibet Sprague <tibet.sprague@gmail.com>
2024-07-10 18:10:09 -0700

    Display correct color and responsibilities for role badges

--- src/components/BadgeEmoji/BadgeEmoji.js
+++ src/components/BadgeEmoji/BadgeEmoji.js
@@ -5,11 +5,19 @@ import './badgeEmoji.scss'
 
 export default function Badge ({ emoji, expanded, className, common, border, onClick, name, id, responsibilities = [] }) {
   if (!emoji) return null
+
+  // TODO: why is this items?
+  responsibilities = responsibilities?.items ? responsibilities.items : responsibilities
+
+  // XXX: hacky way to determine if this is an important system role, having a responsibilit of Administration, Manage Content, or Remove Members
+  common = common || responsibilities.find(r => ['1', '3', '4'].includes(r.id))
+
   return (
     <>
       <span
-        className={className} onClick={onClick}
-        data-tip={responsibilities.length > 0 ? `${name}: ${responsibilities.map(r => r.title).join(', ')}` : name}
+        className={className}
+        onClick={onClick}
+        data-tip='hello'
         data-for={`${id}-${name}-badge-tt`}
         styleName={cx(expanded ? 'badge' : 'badge-collapsed', { border, common })}
       >
@@ -20,6 +28,18 @@ export default function Badge ({ emoji, expanded, className, common, border, onC
         position='bottom'
         offset={{ top: 0, right: 3 }}
         id={`${id}-${name}-badge-tt`}
+        getContent={() => (
+          <div styleName='tipContent'>
+            <span>{name}</span>
+            {responsibilities.length > 0 && (
+              <ul>
+                {responsibilities.map(r => (
+                  <li key={r.id}>{r.title}</li>
+                ))}
+              </ul>
+            )}
+          </div>
+        )}
       />
     </>
   )
