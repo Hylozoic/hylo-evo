d596d3e70acd6f6f5d4faa362e7dc02afbbe0a03
Kevin Triplett <kt@kevintriplett.com>
2024-08-19 13:36:00 -0400

    add console debug msg for hylo-node call in tests

--- config/jest/__mocks__/fileMock.js
+++ config/jest/__mocks__/fileMock.js
@@ -1,1 +1,11 @@
+import { http, Response } from 'msw'
+
 module.exports = 'test-file-stub'
+
+export const handlers = [
+  http.get('http://localhost/socket.io/', () => {
+    console.log('http://localhost/socket.io/ called, ignoring')
+    console.trace()
+    return new Response('no hylo-node server in mocked tests')
+  })
+]
