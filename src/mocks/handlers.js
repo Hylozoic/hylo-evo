5f0c1a720d70428b1c5000eb09b52270e773a81d
Kevin Triplett <kt@kevintriplett.com>
2024-08-20 21:02:33 -0400

    correct url for msw mock handlers

--- src/mocks/handlers.js
+++ src/mocks/handlers.js
@@ -1,7 +1,7 @@
 import { rest } from 'msw'
 
 export const handlers = [
-  rest.get('http://localhost/socket.io/', (req, res, ctx) => {
+  rest.get('/socket.io', (req, res, ctx) => {
     console.log('http://localhost/socket.io/ called in mocked test')
     console.trace()
     return res(
