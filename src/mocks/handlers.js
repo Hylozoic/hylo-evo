c4f18b6ac3d8514aa4c56d855dcad2a5aa4fe5fd
Kevin Triplett <kt@kevintriplett.com>
2024-08-21 06:35:48 -0400

    add msw http handler

--- src/mocks/handlers.js
+++ src/mocks/handlers.js
@@ -1,11 +1,19 @@
-import { rest } from 'msw'
+import { rest, http } from 'msw'
 
 export const handlers = [
   rest.get('/socket.io', (req, res, ctx) => {
     console.log('http://localhost/socket.io/ called in mocked test')
     console.trace()
     return res(
-      ctx.status(200),
+      ctx.status(404),
+      ctx.json({})
+    )
+  }),
+  http.get('/socket.io', (req, res, ctx) => {
+    console.log('http://localhost/socket.io/ called in mocked test')
+    console.trace()
+    return res(
+      ctx.status(404),
       ctx.json({})
     )
   })
