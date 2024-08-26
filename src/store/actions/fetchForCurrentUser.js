ae64a34fe89c6725985afde16092411875891bc1
Kevin Triplett <kt@kevintriplett.com>
2024-08-26 08:25:15 -0300

    integrate MeCompleteFragment into Me

--- src/store/actions/fetchForCurrentUser.js
+++ src/store/actions/fetchForCurrentUser.js
@@ -8,150 +8,146 @@ export default function fetchForCurrentUser () {
     graphql: {
       query: `query MeQuery {
         me {
-          ...MeCompleteFragment
-        }
-      }
-
-      fragment MeCompleteFragment on Me {
-        id
-        isAdmin
-        name
-        avatarUrl
-        hasRegistered
-        newNotificationCount
-        unseenThreadCount
-        location
-        locationObject {
           id
-          addressNumber
-          addressStreet
-          bbox {
-            lat
-            lng
-          }
-          center {
-            lat
-            lng
-          }
-          city
-          country
-          fullText
-          locality
-          neighborhood
-          region
-        }
-        email
-        emailValidated
-        bannerUrl
-        bio
-        contactEmail
-        contactPhone
-        tagline
-        twitterName
-        linkedinUrl
-        facebookUrl
-        url
-        hasDevice
-        intercomHash
-        hasStripeAccount
-        blockedUsers {
-          id
+          isAdmin
           name
-        }
-        settings {
-          alreadySeenTour
-          digestFrequency
-          dmNotifications
-          commentNotifications
-          postNotifications
-          locale
-          mapBaseLayer
-          signupInProgress
-          streamChildPosts
-          streamViewMode
-          streamSortBy
-          streamPostType
-        }
-        affiliations {
-          items {
+          avatarUrl
+          hasRegistered
+          newNotificationCount
+          unseenThreadCount
+          location
+          locationObject {
             id
-            role
-            preposition
-            orgName
-            url
-            createdAt
-            updatedAt
-            isActive
-          }
-        }
-        joinRequests(status: 0) {
-          items {
-            id
-            status
-            createdAt
-            group {
-              id
+            addressNumber
+            addressStreet
+            bbox {
+              lat
+              lng
             }
+            center {
+              lat
+              lng
+            }
+            city
+            country
+            fullText
+            locality
+            neighborhood
+            region
           }
-        }
-        groupRoles {
-          items {
+          email
+          emailValidated
+          bannerUrl
+          bio
+          contactEmail
+          contactPhone
+          tagline
+          twitterName
+          linkedinUrl
+          facebookUrl
+          url
+          hasDevice
+          intercomHash
+          hasStripeAccount
+          blockedUsers {
             id
             name
-            emoji
-            active
-            groupId
-            responsibilities {
-              items {
+          }
+          settings {
+            alreadySeenTour
+            digestFrequency
+            dmNotifications
+            commentNotifications
+            postNotifications
+            locale
+            mapBaseLayer
+            signupInProgress
+            streamChildPosts
+            streamViewMode
+            streamSortBy
+            streamPostType
+          }
+          affiliations {
+            items {
+              id
+              role
+              preposition
+              orgName
+              url
+              createdAt
+              updatedAt
+              isActive
+            }
+          }
+          joinRequests(status: 0) {
+            items {
+              id
+              status
+              createdAt
+              group {
                 id
-                title
-                description
               }
             }
           }
-        }
-        # For memberships only including only what is needed
-        # for initial load in AuthLayoutRouter
-        memberships {
-          id
-          lastViewedAt
-          newPostCount
-          person {
-            id
-          }
-          settings {
-            agreementsAcceptedAt
-            joinQuestionsAnsweredAt
-            sendEmail
-            sendPushNotifications
-            showJoinForm
-          }
-          group {
-            id
-            avatarUrl
-            bannerUrl
-            name
-            memberCount
-            stewardDescriptor
-            stewardDescriptorPlural
-            settings {
-              showSuggestedSkills
+          groupRoles {
+            items {
+              id
+              name
+              emoji
+              active
+              groupId
+              responsibilities {
+                items {
+                  id
+                  title
+                  description
+                }
+              }
             }
-            slug
           }
-        }
-        membershipCommonRoles {
-          items {
+          # For memberships only including only what is needed
+          # for initial load in AuthLayoutRouter
+          memberships {
             id
-            commonRoleId
-            groupId
-            userId
+            lastViewedAt
+            newPostCount
+            person {
+              id
+            }
+            settings {
+              agreementsAcceptedAt
+              joinQuestionsAnsweredAt
+              sendEmail
+              sendPushNotifications
+              showJoinForm
+            }
+            group {
+              id
+              avatarUrl
+              bannerUrl
+              name
+              memberCount
+              stewardDescriptor
+              stewardDescriptorPlural
+              settings {
+                showSuggestedSkills
+              }
+              slug
+            }
           }
-        }
-        skills {
-          items {
-            id
-            name
+          membershipCommonRoles {
+            items {
+              id
+              commonRoleId
+              groupId
+              userId
+            }
+          }
+          skills {
+            items {
+              id
+              name
+            }
           }
         }
       }`
