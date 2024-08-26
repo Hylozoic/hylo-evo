035be12217653b0ed96a2bab01b638171f45bb39
Kevin Triplett <kt@kevintriplett.com>
2024-08-26 08:31:43 -0300

    comment out MeQuery in mockGraphqlServer

--- src/util/testing/mockGraphqlServer.js
+++ src/util/testing/mockGraphqlServer.js
@@ -1,6 +1,6 @@
 import { setupServer } from 'msw/node'
 import { handlers } from './handlers'
-import { graphql } from 'msw'
+// import { graphql } from 'msw'
 
 export const mockGraphqlServer = setupServer(...handlers)
 
@@ -8,47 +8,47 @@ export default mockGraphqlServer
 
 // AuthLayoutRouter empty handlers for reference; to reduce boilerplace
 // possibly pre-polulate these on each test load
-mockGraphqlServer.resetHandlers(
-  graphql.query('MeQuery', (req, res, ctx) => {
-    return res(
-      ctx.data({
-        me: null
-      })
-    )
-  }),
-  graphql.query('FetchForGroup', (req, res, ctx) => {
-    return res(
-      ctx.data({
-        group: null
-      })
-    )
-  }),
-  graphql.query('GroupDetailsQuery', (req, res, ctx) => {
-    return res(
-      ctx.data({
-        group: null
-      })
-    )
-  }),
-  graphql.query('MessageThreadsQuery', (req, res, ctx) => {
-    return res(
-      ctx.data({
-        me: null
-      })
-    )
-  }),
-  graphql.query('MyPendingJoinRequestsQuery', (req, res, ctx) => {
-    return res(
-      ctx.data({
-        joinRequests: null
-      })
-    )
-  }),
-  graphql.query('NotificationsQuery', (req, res, ctx) => {
-    return res(
-      ctx.data({
-        notifications: null
-      })
-    )
-  })
-)
+// mockGraphqlServer.resetHandlers(
+//   graphql.query('MeQuery', (req, res, ctx) => {
+//     return res(
+//       ctx.data({
+//         me: null
+//       })
+//     )
+//   }),
+//   graphql.query('FetchForGroup', (req, res, ctx) => {
+//     return res(
+//       ctx.data({
+//         group: null
+//       })
+//     )
+//   }),
+//   graphql.query('GroupDetailsQuery', (req, res, ctx) => {
+//     return res(
+//       ctx.data({
+//         group: null
+//       })
+//     )
+//   }),
+//   graphql.query('MessageThreadsQuery', (req, res, ctx) => {
+//     return res(
+//       ctx.data({
+//         me: null
+//       })
+//     )
+//   }),
+//   graphql.query('MyPendingJoinRequestsQuery', (req, res, ctx) => {
+//     return res(
+//       ctx.data({
+//         joinRequests: null
+//       })
+//     )
+//   }),
+//   graphql.query('NotificationsQuery', (req, res, ctx) => {
+//     return res(
+//       ctx.data({
+//         notifications: null
+//       })
+//     )
+//   })
+// )
